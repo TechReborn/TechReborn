0e2ad2637fa04b74b0d5a506a92e21c5d5842cd9
Gig <thegigabit101@gmail.com>
2015-04-11 00:12:59 +0100

    Added basic Item for all the dusts

--- src/main/java/techreborn/Core.java
+++ src/main/java/techreborn/Core.java
@@ -1,19 +1,22 @@
 package techreborn;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import techreborn.blocks.BlockThermalGenerator;
+import techreborn.client.GuiHandler;
+import techreborn.client.TechRebornCreativeTab;
+import techreborn.items.ItemDusts;
+import techreborn.tiles.TileThermalGenerator;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
-import net.minecraft.block.Block;
-import techreborn.blocks.BlockThermalGenerator;
-import techreborn.client.GuiHandler;
-import techreborn.client.TechRebornCreativeTab;
-import techreborn.tiles.TileThermalGenerator;
 
 @Mod(modid = "techreborn", name = "TechReborn", version = "@MODVERSION@")
 public class Core {
 
     public static Block thermalGenerator;
+    public static Item dusts;
 
     @Mod.Instance
     public static Core INSTANCE;
@@ -23,6 +26,8 @@ public class Core {
         thermalGenerator = new BlockThermalGenerator().setBlockName("techreborn.thermalGenerator").setBlockTextureName("techreborn:ThermalGenerator_other").setCreativeTab(TechRebornCreativeTab.instance);
         GameRegistry.registerBlock(thermalGenerator, "techreborn.thermalGenerator");
         GameRegistry.registerTileEntity(TileThermalGenerator.class, "TileThermalGenerator");
+        dusts = new ItemDusts();
+        GameRegistry.registerItem(dusts, "dust");
 
         NetworkRegistry.INSTANCE.registerGuiHandler(INSTANCE, new GuiHandler());
     }
