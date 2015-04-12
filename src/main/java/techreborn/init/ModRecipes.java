0a14a36ef435b1209e59f5b29e9ca2362139f760
Gig <thegigabit101@gmail.com>
2015-04-12 16:43:51 +0100

    Added craftingHelper and basic recipes

--- src/main/java/techreborn/init/ModRecipes.java
+++ src/main/java/techreborn/init/ModRecipes.java
@@ -1,11 +1,19 @@
 package techreborn.init;
 
+import cpw.mods.fml.common.registry.GameRegistry;
+import ic2.api.item.IC2Items;
+import ic2.core.Ic2Items;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import techreborn.api.CentrifugeRecipie;
 import techreborn.api.TechRebornAPI;
+import techreborn.util.CraftingHelper;
 import techreborn.util.LogHelper;
 
 public class ModRecipes {
+	
+	public static Item dustIron;
 	
 	public static void init()
 	{
@@ -23,6 +31,16 @@ public class ModRecipes {
 	
 	public static void addShappedRecipes()
 	{
+		CraftingHelper.addShapedOreRecipe(new ItemStack(ModBlocks.thermalGenerator),
+				new Object[]{"III", "IHI", "CGC", 
+				'I', "ingotInvar", 
+				'H', IC2Items.getItem("reinforcedGlass"),
+				'C', IC2Items.getItem("electronicCircuit"), 
+				'G', IC2Items.getItem("geothermalGenerator")});
+		
+		
+		
+		
 		LogHelper.info("Shapped Recipes Added");
 	}
 	
@@ -33,6 +51,14 @@ public class ModRecipes {
 	
 	public static void addSmeltingRecipes()
 	{
+		GameRegistry.addSmelting(new ItemStack(ModItems.dusts,1 ,27), new ItemStack(Items.iron_ingot), 1F);
+		GameRegistry.addSmelting(new ItemStack(ModItems.dusts,1 ,23), new ItemStack(Items.gold_ingot), 1F);
+		GameRegistry.addSmelting(new ItemStack(ModItems.dusts,1 ,14), IC2Items.getItem("copperIngot"), 1F);
+		GameRegistry.addSmelting(new ItemStack(ModItems.dusts,1 ,51), IC2Items.getItem("tinIngot"), 1F);
+		GameRegistry.addSmelting(new ItemStack(ModItems.dusts,1 ,7), IC2Items.getItem("bronzeIngot"), 1F);
+		GameRegistry.addSmelting(new ItemStack(ModItems.dusts,1 ,29), IC2Items.getItem("leadIngot"), 1F);
+		GameRegistry.addSmelting(new ItemStack(ModItems.dusts,1 ,45), IC2Items.getItem("silverIngot"), 1F);
+
 		LogHelper.info("Smelting Recipes Added");
 	}
 	
