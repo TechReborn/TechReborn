16ab2cfa066508a7265d631f5a3556e897567d15
Gig <thegigabit101@gmail.com>
2015-05-01 20:05:34 +0100

    Added new recipe to buildcraft quarry

--- src/main/java/techreborn/compat/recipes/RecipesBuildcraft.java
+++ src/main/java/techreborn/compat/recipes/RecipesBuildcraft.java
@@ -1,6 +1,9 @@
 package techreborn.compat.recipes;
 
+import ic2.api.item.IC2Items;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
+import techreborn.util.CraftingHelper;
 import techreborn.util.RecipeRemover;
 import buildcraft.BuildCraftFactory;
 
@@ -20,7 +23,19 @@ public class RecipesBuildcraft {
 
 	public static void addRecipies()
 	{
-
+		//Quarry
+		CraftingHelper.addShapedOreRecipe(
+				new ItemStack (BuildCraftFactory.quarryBlock),
+				new Object[]
+				{ 
+					"IAI", "GIG", "DED",
+					'I', "gearIron", 
+					'G', "gearGold",
+					'D', "gearDiamond",
+					'A', IC2Items.getItem("advancedCircuit"),
+					'E', IC2Items.getItem("diamondDrill") 
+				}
+	       );
 	}
 
 }
