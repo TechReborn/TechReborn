2901466288d4c4ae49d9f2541d3841858dbebcba
modmuss50 <m123world@gmail.com>
2015-05-04 08:44:44 +0100

    Started work on the multiBlock

--- src/main/java/techreborn/tiles/TileMachineCasing.java
+++ src/main/java/techreborn/tiles/TileMachineCasing.java
@@ -1,17 +1,23 @@
 package techreborn.tiles;
 
+import codechicken.multipart.MultiPartRegistry;
+import erogenousbeef.coreTR.multiblock.MultiblockRegistry;
+import erogenousbeef.coreTR.multiblock.MultiblockWorldRegistry;
 import techreborn.multiblocks.MultiBlockCasing;
 import erogenousbeef.coreTR.multiblock.MultiblockControllerBase;
 import erogenousbeef.coreTR.multiblock.MultiblockValidationException;
 import erogenousbeef.coreTR.multiblock.rectangular.RectangularMultiblockTileEntityBase;
+import techreborn.multiblocks.MultiblockImplosionCompressor;
 
 public class TileMachineCasing extends RectangularMultiblockTileEntityBase {
+
+	int ticks;
+	boolean isStarShape = false;
 
 	@Override
 	public boolean canUpdate()
 	{
-		// No need to update this.
-		return false;
+		return true;
 	}
 
 	@Override
@@ -29,12 +35,20 @@ public class TileMachineCasing extends RectangularMultiblockTileEntityBase {
 	@Override
 	public MultiblockControllerBase createNewMultiblock()
 	{
+		if(isStarShape)
+		{
+			return new MultiblockImplosionCompressor(worldObj);
+		}
 		return new MultiBlockCasing(worldObj);
 	}
 
 	@Override
 	public Class<? extends MultiblockControllerBase> getMultiblockControllerType()
 	{
+		if(isStarShape)
+		{
+			return MultiblockImplosionCompressor.class;
+		}
 		return MultiBlockCasing.class;
 	}
 
@@ -71,5 +85,18 @@ public class TileMachineCasing extends RectangularMultiblockTileEntityBase {
 	public MultiBlockCasing getMultiblockController()
 	{
 		return (MultiBlockCasing) super.getMultiblockController();
+	}
+
+	@Override
+	public void updateEntity() {
+		super.updateEntity();
+		if(getMultiblockController() == null || !getMultiblockController().isAssembled()){
+			ticks ++;
+			if(ticks == 20){
+				isStarShape = !isStarShape;
+				ticks = 0;
+				//TODO recalculate multiblock
+			}
+		}
 	}
 }
