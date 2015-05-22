e9b1280a13665c7e61df4ae9df6e2922851ab92d
Tntrololol <mj.welch@comcast.net>
2015-05-22 15:02:02 -0500

    Added 5 additional types of machine hulls and their respective textures

--- src/main/java/techreborn/blocks/BlockMachineFrame.java
+++ src/main/java/techreborn/blocks/BlockMachineFrame.java
@@ -1,47 +1,80 @@
 package techreborn.blocks;
 
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.util.IIcon;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.util.MathHelper;
+import net.minecraftforge.common.util.ForgeDirection;
+import techreborn.client.TechRebornCreativeTab;
+import techreborn.client.TechRebornCreativeTabMisc;
 
-public class BlockMachineFrame extends BlockMachineBase{
+import java.util.List;
+import java.util.Random;
 
-	@SideOnly(Side.CLIENT)
-	private IIcon iconFront;
+public class BlockMachineFrame extends Block {
 
-	@SideOnly(Side.CLIENT)
-	private IIcon iconTop;
+    public static final String[] types = new String[]
+            { "aluminum", "iron", "bronze", "brass", "steel", "titanium" };
 
-	@SideOnly(Side.CLIENT)
-	private IIcon iconBottom;
+	private IIcon[] textures;
 	
 	public BlockMachineFrame(Material material)
 	{
-		super(material);
-		setBlockName("techreborn.machineframe");
-	}
-	
-	@Override
-	@SideOnly(Side.CLIENT)
-	public void registerBlockIcons(IIconRegister icon)
-	{
-		this.blockIcon = icon.registerIcon("techreborn:machine/machine_side");
-		this.iconFront = icon.registerIcon("techreborn:machine/machine_side");
-		this.iconTop = icon.registerIcon("techreborn:machine/machine_side");
-		this.iconBottom = icon.registerIcon("techreborn:machine/machine_side");
+        super(material);
+        setBlockName("techreborn.machineFrame");
+        setCreativeTab(TechRebornCreativeTab.instance);
+        setHardness(1f);
 	}
 
-	@SideOnly(Side.CLIENT)
-	public IIcon getIcon(int side, int metadata)
-	{
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void getSubBlocks(Item item, CreativeTabs creativeTabs, List list)
+    {
+        for (int meta = 0; meta < types.length; meta++)
+        {
+            list.add(new ItemStack(item, 1, meta));
+        }
+    }
 
-		return metadata == 0 && side == 3 ? this.iconFront
-				: side == 1 ? this.iconTop : 
-					side == 0 ? this.iconBottom: (side == 0 ? this.iconTop
-						: (side == metadata ? this.iconFront : this.blockIcon));
+    @Override
+    public int damageDropped(int metaData)
+    {
+        return metaData;
+    }
 
-	}
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister iconRegister)
+    {
+        this.textures = new IIcon[types.length];
+
+        for (int i = 0; i < types.length; i++)
+        {
+            textures[i] = iconRegister.registerIcon("techreborn:" + "machine/"
+                    + types[i] + "_machine_block");
+        }
+    }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public IIcon getIcon(int side, int metaData)
+    {
+        metaData = MathHelper.clamp_int(metaData, 0, types.length - 1);
+
+        if (ForgeDirection.getOrientation(side) == ForgeDirection.UP
+                || ForgeDirection.getOrientation(side) == ForgeDirection.DOWN)
+        {
+            return textures[metaData];
+        } else
+        {
+            return textures[metaData];
+        }
+    }
 
 }
