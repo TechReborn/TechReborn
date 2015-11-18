276bf8355fb5e5c7c7b0bbd527feca85046bd2cc
Modmuss50 <modmuss50@gmail.com>
2015-11-18 19:51:41 +0000

    Computer Cube now shows PDA gui

--- src/main/java/techreborn/blocks/BlockComputerCube.java
+++ src/main/java/techreborn/blocks/BlockComputerCube.java
@@ -4,8 +4,12 @@ import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import techreborn.Core;
+import techreborn.client.GuiHandler;
 
 public class BlockComputerCube extends BlockMachineBase {
 
@@ -25,6 +29,15 @@ public class BlockComputerCube extends BlockMachineBase {
     @SideOnly(Side.CLIENT)
     public IIcon getIcon(IBlockAccess blockAccess, int x, int y, int z, int side) {
         return this.blockIcon;
+    }
+
+    @Override
+    public boolean onBlockActivated(World world, int x, int y, int z,
+                                    EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
+        if (!player.isSneaking())
+            player.openGui(Core.INSTANCE, GuiHandler.pdaID, world, x,
+                    y, z);
+        return true;
     }
 
 }
