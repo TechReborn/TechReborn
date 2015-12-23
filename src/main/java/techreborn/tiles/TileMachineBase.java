2320ceb2c3fdb273d9135b321e0ea694b40aa423
modmuss50 <modmuss50@gmail.com>
2015-12-23 12:40:48 +0000

    Fixed machines loosing state

--- src/main/java/techreborn/tiles/TileMachineBase.java
+++ src/main/java/techreborn/tiles/TileMachineBase.java
@@ -1,13 +1,16 @@
 package techreborn.tiles;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.ITickable;
+import net.minecraft.world.World;
 import reborncore.common.packets.PacketHandler;
 import techreborn.blocks.BlockMachineBase;
 
@@ -67,4 +70,13 @@ public class TileMachineBase extends TileEntity implements ITickable {
         }
     }
 
+
+    //This stops the tile from getting cleared when the state is updated(rotation and on/off)
+    @Override
+    public boolean shouldRefresh(World world, BlockPos pos, IBlockState oldState, IBlockState newSate) {
+        if(oldState.getBlock() != newSate.getBlock()){
+            return true;
+        }
+        return false;
+    }
 }
