692c89a6a06e7257e76a6cf61532c185e950f867
modmuss50 <modmuss50@gmail.com>
2016-01-13 17:17:53 +0000

    Fixes #327

--- src/main/java/techreborn/blocks/fluid/TechRebornFluid.java
+++ src/main/java/techreborn/blocks/fluid/TechRebornFluid.java
@@ -1,12 +1,10 @@
 package techreborn.blocks.fluid;
 
-import net.minecraft.client.resources.model.ModelResourceLocation;
-import net.minecraft.item.ItemStack;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fluids.Fluid;
 
 public class TechRebornFluid extends Fluid {
     public TechRebornFluid(String fluidName) {
-        super(fluidName,  new ModelResourceLocation("techreborn:textures/block/" + fluidName + "_flowing"), new ModelResourceLocation("techreborn:textures/block/" + fluidName + "_flowing"));
+        super(fluidName,  new ResourceLocation("techreborn:textures/block/" + fluidName + "_flowing"), new ResourceLocation("techreborn:textures/block/" + fluidName + "_flowing"));
     }
 }
