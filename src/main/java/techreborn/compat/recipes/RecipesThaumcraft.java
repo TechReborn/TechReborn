6de20dcba10a817b6b565a93b1caf3553c9e4743
modmuss50 <modmuss50@gmail.com>
2016-02-29 13:30:14 +0000

    Fix crash with temp thaumcraft integration

--- src/main/java/techreborn/compat/recipes/RecipesThaumcraft.java
+++ src/main/java/techreborn/compat/recipes/RecipesThaumcraft.java
@@ -28,14 +28,14 @@ public class RecipesThaumcraft implements ICompatModule {
         //ItemStack greatWoodStack = BlocksTC.getBlock("blockWoodenDevice", 6);
         ItemStack greatWoodStack = null;
         //SOMEONE WHO KNOWS TC FIX THIS
-        greatWoodStack.stackSize = 6;
+//        greatWoodStack.stackSize = 6;
         RecipeHandler.addRecipe(new IndustrialSawmillRecipe(new ItemStack(BlocksTC.log), null, new FluidStack(FluidRegistry.WATER, 1000), greatWoodStack, pulpStack, null, 200, 30, false));
         RecipeHandler.addRecipe(new IndustrialSawmillRecipe(new ItemStack(BlocksTC.log), IC2Items.getItem("waterCell"), null, greatWoodStack, pulpStack, IC2Items.getItem("cell"), 200, 30, false));
         RecipeHandler.addRecipe(new IndustrialSawmillRecipe(new ItemStack(BlocksTC.log), new ItemStack(Items.water_bucket), null, greatWoodStack, pulpStack, new ItemStack(Items.bucket), 200, 30, false));
 
         //ItemStack silverWoodStack = ItemApi.getBlock("blockWoodenDevice", 7);
         ItemStack silverWoodStack = null;
-        silverWoodStack.stackSize = 6;
+        //silverWoodStack.stackSize = 6;
         RecipeHandler.addRecipe(new IndustrialSawmillRecipe(new ItemStack(BlocksTC.log, 1), null, new FluidStack(FluidRegistry.WATER, 1000), silverWoodStack, pulpStack, null, 200, 30, false));
         RecipeHandler.addRecipe(new IndustrialSawmillRecipe(new ItemStack(BlocksTC.log, 1), IC2Items.getItem("waterCell"), null, silverWoodStack, pulpStack, IC2Items.getItem("cell"), 200, 30, false));
         RecipeHandler.addRecipe(new IndustrialSawmillRecipe(new ItemStack(BlocksTC.log, 1), new ItemStack(Items.water_bucket), null, silverWoodStack, pulpStack, new ItemStack(Items.bucket), 200, 30, false));
