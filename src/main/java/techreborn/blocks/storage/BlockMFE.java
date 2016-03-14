bb16153e77f5fd874c907d07f8b1a7811e851013
TheDoctorSoda <thedoctorsoda@gmail.com>
2016-03-14 11:25:12 -0700

    Fix MFE and MFSU paths

--- src/main/java/techreborn/blocks/storage/BlockMFE.java
+++ src/main/java/techreborn/blocks/storage/BlockMFE.java
@@ -23,26 +23,26 @@ public class BlockMFE extends BlockBatBox {
 
     @Override
     public String getFrontOff() {
-        return prefix + "batbox_front";
+        return prefix + "mfe_front";
     }
 
     @Override
     public String getFrontOn() {
-        return prefix + "batbox_front";
+        return prefix + "mfe_front";
     }
 
     @Override
     public String getSide() {
-        return prefix + "batbox_side";
+        return prefix + "mfe_side";
     }
 
     @Override
     public String getTop() {
-        return prefix + "batbox_side";
+        return prefix + "mfe_top";
     }
 
     @Override
     public String getBottom() {
-        return prefix + "batbox_side";
+        return prefix + "mfe_bottom";
     }
 }
