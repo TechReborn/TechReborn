929d2f4b7f4385a1dd71e8620c5fd381ede87a81
modmuss50 <modmuss50@gmail.com>
2016-03-16 15:52:27 +0000

    Improvements to the nuke, rendering needs fixing

--- src/main/java/techreborn/blocks/BlockNuke.java
+++ src/main/java/techreborn/blocks/BlockNuke.java
@@ -1,37 +1,46 @@
 package techreborn.blocks;
 
 import me.modmuss50.jsonDestroyer.api.ITexturedBlock;
-import net.minecraft.block.BlockTNT;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyBool;
+import net.minecraft.block.state.BlockState;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.projectile.EntityArrow;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumWorldBlockLayer;
 import net.minecraft.world.Explosion;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import reborncore.RebornCore;
+import reborncore.common.BaseBlock;
 import techreborn.client.TechRebornCreativeTabMisc;
 import techreborn.entitys.EntityNukePrimed;
 
 /**
  * Created by Mark on 13/03/2016.
  */
-public class BlockNuke extends BlockTNT implements ITexturedBlock {
+public class BlockNuke extends BaseBlock implements ITexturedBlock {
+    public static final PropertyBool OVERLAY = PropertyBool.create("overlay");
 
     public BlockNuke() {
+        super(Material.tnt);
         setUnlocalizedName("techreborn.nuke");
         setCreativeTab(TechRebornCreativeTabMisc.instance);
         RebornCore.jsonDestroyer.registerObject(this);
+        this.setDefaultState(this.blockState.getBaseState().withProperty(OVERLAY, false));
     }
 
-    @Override
     public void explode(World worldIn, BlockPos pos, IBlockState state, EntityLivingBase igniter) {
         if (!worldIn.isRemote) {
-            if (state.getValue(EXPLODE).booleanValue()) {
-                EntityNukePrimed entitynukeprimed = new EntityNukePrimed(worldIn, (double) ((float) pos.getX() + 0.5F), (double) pos.getY(), (double) ((float) pos.getZ() + 0.5F), igniter);
-                worldIn.spawnEntityInWorld(entitynukeprimed);
-                worldIn.playSoundAtEntity(entitynukeprimed, "game.tnt.primed", 1.0F, 1.0F);
-            }
+            EntityNukePrimed entitynukeprimed = new EntityNukePrimed(worldIn, (double) ((float) pos.getX() + 0.5F), (double) pos.getY(), (double) ((float) pos.getZ() + 0.5F), igniter);
+            worldIn.spawnEntityInWorld(entitynukeprimed);
+            worldIn.playSoundAtEntity(entitynukeprimed, "game.tnt.primed", 1.0F, 1.0F);
         }
     }
 
@@ -44,6 +53,40 @@ public class BlockNuke extends BlockTNT implements ITexturedBlock {
         }
     }
 
+    public void onEntityCollidedWithBlock(World worldIn, BlockPos pos, IBlockState state, Entity entityIn) {
+        if (!worldIn.isRemote && entityIn instanceof EntityArrow) {
+            EntityArrow entityarrow = (EntityArrow) entityIn;
+
+            if (entityarrow.isBurning()) {
+                this.explode(worldIn, pos, state, entityarrow.shootingEntity instanceof EntityLivingBase ? (EntityLivingBase) entityarrow.shootingEntity : null);
+                worldIn.setBlockToAir(pos);
+            }
+        }
+    }
+
+    public void onBlockAdded(World worldIn, BlockPos pos, IBlockState state) {
+        super.onBlockAdded(worldIn, pos, state);
+        if (worldIn.isBlockPowered(pos)) {
+            this.onBlockDestroyedByPlayer(worldIn, pos, state);
+            worldIn.setBlockToAir(pos);
+        }
+    }
+
+    /**
+     * Called when a neighboring block changes.
+     */
+    public void onNeighborBlockChange(World worldIn, BlockPos pos, IBlockState state, Block neighborBlock) {
+        if (worldIn.isBlockPowered(pos)) {
+            this.onBlockDestroyedByPlayer(worldIn, pos, state);
+            worldIn.setBlockToAir(pos);
+        }
+    }
+
+    public void onBlockDestroyedByPlayer(World worldIn, BlockPos pos, IBlockState state)
+    {
+        this.explode(worldIn, pos, state, (EntityLivingBase)null);
+    }
+
     @Override
     public boolean onBlockActivated(World worldIn, BlockPos pos, IBlockState state, EntityPlayer playerIn, EnumFacing side, float hitX, float hitY, float hitZ) {
         return false; //No flint and steel
@@ -51,11 +94,28 @@ public class BlockNuke extends BlockTNT implements ITexturedBlock {
 
     @Override
     public String getTextureNameFromState(IBlockState iBlockState, EnumFacing enumFacing) {
-        return "techreborn:blocks/machine/greg_machine/nuke";
+        if (iBlockState.getValue(OVERLAY)) {
+            return "techreborn:blocks/nuke_overlay";
+        }
+        return "techreborn:blocks/nuke";
     }
 
     @Override
     public int amountOfStates() {
-        return 1;
+        return 2;
     }
+
+    public int getMetaFromState(IBlockState state) {
+        return state.getValue(OVERLAY) ? 1 : 0;
+    }
+
+    public IBlockState getStateFromMeta(int meta)
+    {
+        return this.getDefaultState().withProperty(OVERLAY, Boolean.valueOf((meta & 1) > 0));
+    }
+
+    protected BlockState createBlockState() {
+        return new BlockState(this, OVERLAY);
+    }
+
 }
