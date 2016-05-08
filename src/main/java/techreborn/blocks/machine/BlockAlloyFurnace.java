ba5d400c88dc6718b3246e52ae9119ba9c9235bd
gigabit101 <thegigabit101@gmail.com>
2016-05-08 18:59:30 +0100

    alloy furnace no longer needs a wrench

--- src/main/java/techreborn/blocks/machine/BlockAlloyFurnace.java
+++ src/main/java/techreborn/blocks/machine/BlockAlloyFurnace.java
@@ -1,10 +1,13 @@
 package techreborn.blocks.machine;
 
+import java.util.ArrayList;
+
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.IIcon;
 import net.minecraft.world.IBlockAccess;
@@ -80,5 +83,12 @@ public class BlockAlloyFurnace extends BlockMachineBase {
             return this.blockIcon;
         }
     }
-
+    
+    @Override
+    public ArrayList<ItemStack> getDrops(World world, int x, int y, int z, int metadata, int fortune) 
+    {
+    	ArrayList<ItemStack> items = new ArrayList<>();
+		items.add(new ItemStack(this));
+		return items;
+    }
 }
