1efa303bad850a47e33fa4dee22c2c7834d8be7f
modmuss50 <modmuss50@gmail.com>
2016-05-21 20:01:03 +0100

    Fixes #593

--- src/main/java/techreborn/blocks/BlockRubberSapling.java
+++ src/main/java/techreborn/blocks/BlockRubberSapling.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Random;
 
 import net.minecraft.block.BlockSapling;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
@@ -24,6 +25,7 @@ public class BlockRubberSapling extends BlockSapling
 		setUnlocalizedName("techreborn.rubbersapling");
 		setCreativeTab(TechRebornCreativeTabMisc.instance);
 		this.setDefaultState(this.getDefaultState().withProperty(STAGE, 0));
+		setSoundType(SoundType.PLANT)
 	}
 
 	@Override
