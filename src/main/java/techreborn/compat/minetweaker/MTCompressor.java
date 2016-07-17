ea74721945cac0bea34cb406baaeebd927b90cab
al132 <apertram1@gmail.com>
2016-07-17 15:19:31 -0500

    Add CraftTweaker support for the Compressor

---
+++ src/main/java/techreborn/compat/minetweaker/MTCompressor.java
@@ -0,0 +1,43 @@
+package techreborn.compat.minetweaker;
+
+import minetweaker.MineTweakerAPI;
+import minetweaker.api.item.IIngredient;
+import minetweaker.api.item.IItemStack;
+import net.minecraft.item.ItemStack;
+import stanhebben.zenscript.annotations.ZenClass;
+import stanhebben.zenscript.annotations.ZenMethod;
+import techreborn.api.recipe.machines.CompressorRecipe;
+import techreborn.api.Reference;
+
+/*
+ * mods.techreborn.compressor.addRecipe(<minecraft:clay>,<minecraft:clay_ball>, 40,100);
+ */
+
+@ZenClass("mods.techreborn.compressor")
+public class MTCompressor extends MTGeneric {
+
+    @ZenMethod
+    public static void addRecipe(IItemStack output1, IIngredient input1, int ticktime, int euTick) {
+        ItemStack oInput1 = (ItemStack) MinetweakerCompat.toObject(input1);
+
+        
+        //public CompressorRecipe(ItemStack input1, ItemStack output1, int tickTime, int euPerTick) {
+        CompressorRecipe r = new CompressorRecipe(oInput1, MinetweakerCompat.toStack(output1), ticktime, euTick);
+
+        addRecipe(r);
+    }
+
+    @ZenMethod
+    public static void removeInputRecipe(IIngredient iIngredient) {
+        MineTweakerAPI.apply(new RemoveInput(iIngredient, getMachineName()));
+    }
+
+    @ZenMethod
+    public static void removeRecipe(IItemStack output) {
+        MineTweakerAPI.apply(new Remove(MinetweakerCompat.toStack(output), getMachineName()));
+    }
+
+    public static String getMachineName() {
+        return Reference.compressorRecipe;
+    }
+}
