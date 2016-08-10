658b974ab95f6724521c794cfc9e703529800962
unknown <zygimantas.magelinskas@vileisiumokykla.lt>
2016-08-10 21:42:19 +0300

    3 Pyrite dusts in an Industrial Electrolyzer gives 1 Iron dust and 2 Sulfur dust

--- src/main/java/techreborn/init/ModRecipes.java
+++ src/main/java/techreborn/init/ModRecipes.java
@@ -2046,7 +2046,7 @@ public class ModRecipes
 				ItemCells.getCellByName("nitrogen"), ItemCells.getCellByName("carbon"), null, null, 80, 60));
 
 		RecipeHandler.addRecipe(new IndustrialElectrolyzerRecipe(ItemDusts.getDustByName("pyrite", 3), null,
-				ItemDusts.getDustByName("iron"), ItemDusts.getDustByName("sulfur"), null, null, 120, 128));
+				ItemDusts.getDustByName("iron"), ItemDusts.getDustByName("sulfur", 2), null, null, 120, 128));
 
 		RecipeHandler.addRecipe(new IndustrialElectrolyzerRecipe(ItemDusts.getDustByName("sphalerite", 2), null,
 				ItemDusts.getDustByName("zinc"), ItemDusts.getDustByName("sulfur"), null, null, 150, 100));
