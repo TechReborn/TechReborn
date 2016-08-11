c24cf545ec27860445905a9d882950671a08c4d8
unknown <zygimantas.magelinskas@vileisiumokykla.lt>
2016-08-11 23:15:16 +0300

    Fixed #715 by removing Iridium Ore from Industrial Grinder recipes

--- src/main/java/techreborn/init/ModRecipes.java
+++ src/main/java/techreborn/init/ModRecipes.java
@@ -3064,28 +3064,6 @@ public class ModRecipes
                 ItemDustsSmall.getSmallDustByName("Silver", 1),
 				null, 100, 120));
 
-		// Iridium Ore
-		RecipeHandler.addRecipe(new IndustrialGrinderRecipe(
-                new ItemStack(ModBlocks.ore, 1, 1),
-				new FluidStack(FluidRegistry.WATER, 1000), 
-                OreDictionary.getOres("oreIridium").get(0),
-				ItemDustsSmall.getSmallDustByName("Platinum", 2),
-                null, null, 100, 120));
-        
-		RecipeHandler.addRecipe(new IndustrialGrinderRecipe(
-                new ItemStack(ModBlocks.ore, 1, 1), 
-                new FluidStack(ModFluids.fluidMercury, 1000), 
-                OreDictionary.getOres("oreIridium").get(0),
-				ItemDustsSmall.getSmallDustByName("Platinum", 2),
-                null, null, 100, 120));
-        
-		RecipeHandler.addRecipe(new IndustrialGrinderRecipe(
-                new ItemStack(ModBlocks.ore, 1, 1),
-                new FluidStack(ModFluids.fluidMercury, 1000), 
-                OreDictionary.getOres("oreIridium").get(0),
-				ItemDustsSmall.getSmallDustByName("Platinum", 2),
-                null, null, 100, 120));
-
 		RecipeHandler.addRecipe(new IndustrialGrinderRecipe(
                 new ItemStack(ModBlocks.ore, 1, 2),
                 new FluidStack(FluidRegistry.WATER, 1000), 
