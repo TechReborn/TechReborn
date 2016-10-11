c1d733f0801bdf61f385a2d8878dfb802f7a3bad
ProfessorProspector <thedoctorsoda@gmail.com>
2016-10-10 17:43:09 -0700

    Simplify a load of jsons, fix hopefully all particles to use front texture, and fix solar panel model (was previously using Jason D. before)

--- src/main/java/techreborn/init/ModBlocks.java
+++ src/main/java/techreborn/init/ModBlocks.java
@@ -352,6 +352,7 @@ public class ModBlocks {
 		solarPanel = new BlockSolarPanel();
 		registerBlock(solarPanel, "techreborn.solarpanel");
 		GameRegistry.registerTileEntity(TileSolarPanel.class, "TileSolarPanel");
+		Core.proxy.registerCustomBlockStateLocation(ElectricFurnace, "machines/generators/solar_panel");
 
 		waterMill = new BlockWaterMill();
 		registerBlock(waterMill, "techreborn.watermill");
