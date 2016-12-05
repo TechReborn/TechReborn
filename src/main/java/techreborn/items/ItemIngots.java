f07bae19bd94fa652d8f4ee52f148b4337e846eb
modmuss50 <modmuss50@gmail.com>
2016-12-05 13:00:54 +0000

    Fix iridium stone, remove silicon ingot.

--- src/main/java/techreborn/items/ItemIngots.java
+++ src/main/java/techreborn/items/ItemIngots.java
@@ -14,7 +14,7 @@ public class ItemIngots extends ItemTextureBase {
 	public static final String[] types = new String[] { "aluminum", "brass", "bronze", "chrome", "copper", "electrum",
 		"invar", "iridium", "lead", "nickel", "platinum", "silver", "steel", "tin", "titanium", "tungsten",
 		"hotTungstensteel", "tungstensteel", "zinc", "refinedIron", "advancedAlloy", "mixedMetal",
-		"iridiumAlloy", "silicon" };
+		"iridiumAlloy"};
 
 	public ItemIngots() {
 		setCreativeTab(TechRebornCreativeTabMisc.instance);
