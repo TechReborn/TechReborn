1566953f005d22330d5101a14d52bdf270126c09
Thog <thog92@hotmail.fr>
2016-12-13 17:00:23 +0100

    Make treetap working with the IC2 rubber tree (Fix #786)
    Changes:
    - The dependency of IC2 have been updated to the last version
    - This implementation follow the original treetap logic but use
    Techreborn original electrical cost.

--- src/main/java/techreborn/items/tools/ItemTreeTap.java
+++ src/main/java/techreborn/items/tools/ItemTreeTap.java
@@ -1,14 +1,23 @@
 package techreborn.items.tools;
 
+import ic2.core.item.tool.ItemTreetap;
 import me.modmuss50.jsonDestroyer.api.ITexturedItem;
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumActionResult;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.EnumHand;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import reborncore.RebornCore;
 import techreborn.client.TechRebornCreativeTab;
+import techreborn.compat.CompatManager;
 import techreborn.lib.ModInfo;
 
 public class ItemTreeTap extends Item implements ITexturedItem {
@@ -19,6 +28,20 @@ public class ItemTreeTap extends Item implements ITexturedItem {
 		setUnlocalizedName("techreborn.treetap");
 		RebornCore.jsonDestroyer.registerObject(this);
 		setCreativeTab(TechRebornCreativeTab.instance);
+	}
+
+
+	@Override
+	public EnumActionResult onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumHand hand, EnumFacing side, float hitX, float hitY, float hitZ) {
+		IBlockState state = worldIn.getBlockState(pos);
+		Block block = state.getBlock();
+		if(CompatManager.isIC2Loaded && block == Block.getBlockFromName("ic2:rubber_wood"))  {
+			ItemTreetap.attemptExtract(playerIn, worldIn, pos, side, state, null);
+			if (!worldIn.isRemote)
+				stack.damageItem(1, playerIn);
+			return EnumActionResult.SUCCESS;
+		}
+		return EnumActionResult.PASS;
 	}
 
 	@Override
