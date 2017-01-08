a700d80e0fa44c597fc9d39165e678ca5a340228
Ourten <ourtencoop@gmail.com>
2017-01-08 22:33:50 +0100

    Fix icon of SpriteSlot

--- src/main/java/techreborn/client/container/builder/slot/SpriteSlot.java
+++ src/main/java/techreborn/client/container/builder/slot/SpriteSlot.java
@@ -1,18 +1,21 @@
 package techreborn.client.container.builder.slot;
 
+import javax.annotation.Nullable;
+
 import net.minecraft.inventory.IInventory;
-import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import techreborn.lib.ModInfo;
 
 public class SpriteSlot extends FilteredSlot {
 
+	private final String sprite;
 	int stacksize;
 
 	public SpriteSlot(final IInventory inventory, final int index, final int xPosition, final int yPosition, final String sprite, final int stacksize) {
 		super(inventory, index, xPosition, yPosition);
-		this.setBackgroundLocation(
-				new ResourceLocation(ModInfo.MOD_ID + ":textures/gui/slot_sprites/" + sprite + ".png"));
+		this.sprite = ModInfo.MOD_ID + ":textures/gui/slot_sprites/" + sprite;
 		this.stacksize = stacksize;
 	}
 
@@ -23,5 +26,12 @@ public class SpriteSlot extends FilteredSlot {
 	@Override
 	public int getSlotStackLimit() {
 		return this.stacksize;
+	}
+
+	@Override
+	@Nullable
+	@SideOnly(Side.CLIENT)
+	public String getSlotTexture() {
+		return this.sprite;
 	}
 }
