543879f3a47b8bc28d71252f17bb74183a7cdf00
modmuss50 <modmuss50@gmail.com>
2017-01-16 11:10:06 +0000

    Attempt to fix crash again.

--- src/main/java/techreborn/events/BlockBreakHandler.java
+++ src/main/java/techreborn/events/BlockBreakHandler.java
@@ -3,6 +3,7 @@ package techreborn.events;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.oredict.OreDictionary;
+import reborncore.common.util.OreUtil;
 import techreborn.items.ItemGems;
 import techreborn.utils.OreDictUtils;
 
@@ -13,7 +14,7 @@ public class BlockBreakHandler {
 
 	@SubscribeEvent
 	public void onBlockHarvest(BlockEvent.HarvestDropsEvent event) {
-		if (event.getState() != null && OreDictionary.doesOreNameExist("oreRuby") && OreDictUtils.isOre(event.getState(), "oreRuby")) {
+		if (event.getState() != null && OreUtil.doesOreExistAndValid("oreRuby") && OreDictUtils.isOre(event.getState(), "oreRuby")) {
 			event.getDrops().add(ItemGems.getGemByName("red_garnet").copy());
 		}
 	}
