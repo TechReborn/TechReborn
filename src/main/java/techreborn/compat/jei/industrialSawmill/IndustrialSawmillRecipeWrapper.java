1bd86924b6cf0a75c2381c3430e7433482278aa2
drcrazy <hcb@wowhcb.ru>
2017-04-07 00:53:09 +0100

    Initial commit for Insdustrial Sawmill JEI support
    (cherry picked from commit 8b4ce46)

---
+++ src/main/java/techreborn/compat/jei/industrialSawmill/IndustrialSawmillRecipeWrapper.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package techreborn.compat.jei.industrialSawmill;
+
+import mezz.jei.api.ingredients.IIngredients;
+import mezz.jei.api.recipe.BlankRecipeWrapper;
+
+/**
+ * @author drcrazy
+ *
+ */
+public class IndustrialSawmillRecipeWrapper extends BlankRecipeWrapper {
+
+	/**
+	 * 
+	 */
+	public IndustrialSawmillRecipeWrapper() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/* (non-Javadoc)
+	 * @see mezz.jei.api.recipe.IRecipeWrapper#getIngredients(mezz.jei.api.ingredients.IIngredients)
+	 */
+	@Override
+	public void getIngredients(IIngredients ingredients) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
