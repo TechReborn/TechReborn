3d55b435d48a0e7775e806867485f3403896fada
modmuss50 <modmuss50@gmail.com>
2017-04-21 09:09:28 +0100

    Ic2 1.11.2 support

--- src/main/java/techreborn/init/IC2Dict.java
+++ src/main/java/techreborn/init/IC2Dict.java
@@ -37,6 +37,7 @@ import ic2.core.ref.ItemName;
 import ic2.core.ref.TeBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraftforge.oredict.OreDictionary;
+import reborncore.api.recipe.RecipeHandler;
 import techreborn.Core;
 import techreborn.api.recipe.machines.ExtractorRecipe;
 import techreborn.items.ItemParts;
@@ -86,53 +87,50 @@ public class IC2Dict {
 		//Rubber tree blocks, hidden when deduplication is on, and rubber tress are not set to gen, includes tree taps
 		//Circuits are hidden in JEI
 
+		RecipeHandler.addRecipe(
+			new ExtractorRecipe(ItemName.misc_resource.getItemStack("resin"),
+				ItemParts.getPartByName("rubber", 3), 400, 2));
+
 		try {
-			//			CraftingItemType.circuit.getName();
-			//
-			//			OreDictionary.registerOre("reBattery", ItemName.re_battery.getItemStack());
-			//
-			//			OreDictionary.registerOre("circuitBasic", ItemName.crafting.getItemStack(CraftingItemType.circuit));
-			//			OreDictionary.registerOre("circuitAdvanced", ItemName.crafting.getItemStack(CraftingItemType.advanced_circuit));
-			//
-			//			OreDictionary.registerOre("machineBlockBasic", BlockName.resource.getItemStack(ResourceBlock.machine));
-			//			OreDictionary.registerOre("machineBlockAdvanced", BlockName.resource.getItemStack(ResourceBlock.advanced_machine));
-			//
-			//			OreDictionary.registerOre("lapotronCrystal", ItemName.lapotron_crystal.getItemStack());
-			//			OreDictionary.registerOre("energyCrystal", ItemName.lapotron_crystal.getItemStack());
-			//
-			//			OreDictionary.registerOre("drillBasic", ItemName.drill.getItemStack());
-			//			OreDictionary.registerOre("drillDiamond", ItemName.diamond_drill.getItemStack());
-			//			OreDictionary.registerOre("drillAdvanced", ItemName.iridium_drill.getItemStack());
-			//
-			//			ItemStack industrialTnt = BlockName.te.getItemStack(TeBlock.itnt);
-			//			industrialTnt.setItemDamage(1);
-			//			OreDictionary.registerOre("industrialTnt", industrialTnt);
-			//
-			//			OreDictionary.registerOre("craftingIndustrialDiamond", ItemName.crafting.getItemStack(CraftingItemType.industrial_diamond));
-			//			OreDictionary.registerOre("fertilizer", ItemName.crafting.getItemStack(CraftingItemType.bio_chaff));
-			//			OreDictionary.registerOre("hvTransformer", BlockName.te.getItemStack(TeBlock.hv_transformer));
-			//
-			//			//TODO:
-			//
-			//			//OreDictionary.registerOre("insulatedGoldCableItem", BlockName.te.getItemStack(CableType.gold));
-			//
-			//			//OreDictionary.registerOre("ic2Generator", ModBlocks.Generator);
-			//			//OreDictionary.registerOre("ic2SolarPanel", ModBlocks.solarPanel);
-			//			//OreDictionary.registerOre("ic2Macerator", ModBlocks.Grinder);
-			//			//OreDictionary.registerOre("ic2Extractor", ModBlocks.Extractor);
-			//			//OreDictionary.registerOre("ic2Windmill", ModBlocks.windMill);
-			//			//OreDictionary.registerOre("ic2Watermill", ModBlocks.waterMill);
-			//
-			//			OreDictionary.registerOre("uran235", ItemName.nuclear.getItemStack(NuclearResourceType.uranium_235));
-			//			OreDictionary.registerOre("uran238", ItemName.nuclear.getItemStack(NuclearResourceType.uranium_238));
-			//			OreDictionary.registerOre("smallUran238", ItemName.nuclear.getItemStack(NuclearResourceType.small_uranium_238));
-			//			OreDictionary.registerOre("smallUran235", ItemName.nuclear.getItemStack(NuclearResourceType.small_uranium_235));
-			//
-			//			OreDictionary.registerOre("fenceIron", BlockName.fence.getItemStack(BlockIC2Fence.IC2FenceType.iron));
-			//			OreDictionary.registerOre("rubberWood", BlockName.rubber_wood.getItemStack());
-			//			OreDictionary.registerOre("glassReinforced", BlockName.glass.getItemStack(BlockTexGlass.GlassType.reinforced));
-			//
-			//			OreDictionary.registerOre("oreIridium", ItemName.misc_resource.getItemStack(MiscResourceType.iridium_ore));
+			CraftingItemType.circuit.getName();
+
+			OreDictionary.registerOre("reBattery", ItemName.re_battery.getItemStack());
+
+			OreDictionary.registerOre("circuitBasic", ItemName.crafting.getItemStack(CraftingItemType.circuit));
+			OreDictionary.registerOre("circuitAdvanced", ItemName.crafting.getItemStack(CraftingItemType.advanced_circuit));
+
+			OreDictionary.registerOre("machineBlockBasic", BlockName.resource.getItemStack(ResourceBlock.machine));
+			OreDictionary.registerOre("machineBlockAdvanced", BlockName.resource.getItemStack(ResourceBlock.advanced_machine));
+
+			OreDictionary.registerOre("lapotronCrystal", ItemName.lapotron_crystal.getItemStack());
+			OreDictionary.registerOre("energyCrystal", ItemName.lapotron_crystal.getItemStack());
+
+			OreDictionary.registerOre("drillBasic", ItemName.drill.getItemStack());
+			OreDictionary.registerOre("drillDiamond", ItemName.diamond_drill.getItemStack());
+			OreDictionary.registerOre("drillAdvanced", ItemName.iridium_drill.getItemStack());
+
+			ItemStack industrialTnt = BlockName.te.getItemStack(TeBlock.itnt);
+			industrialTnt.setItemDamage(1);
+			OreDictionary.registerOre("industrialTnt", industrialTnt);
+
+			OreDictionary.registerOre("craftingIndustrialDiamond", ItemName.crafting.getItemStack(CraftingItemType.industrial_diamond));
+			OreDictionary.registerOre("fertilizer", ItemName.crafting.getItemStack(CraftingItemType.bio_chaff));
+			OreDictionary.registerOre("hvTransformer", BlockName.te.getItemStack(TeBlock.hv_transformer));
+
+			OreDictionary.registerOre("uran235", ItemName.nuclear.getItemStack(NuclearResourceType.uranium_235));
+			OreDictionary.registerOre("uran238", ItemName.nuclear.getItemStack(NuclearResourceType.uranium_238));
+			OreDictionary.registerOre("smallUran238", ItemName.nuclear.getItemStack(NuclearResourceType.small_uranium_238));
+			OreDictionary.registerOre("smallUran235", ItemName.nuclear.getItemStack(NuclearResourceType.small_uranium_235));
+
+			OreDictionary.registerOre("fenceIron", BlockName.fence.getItemStack(BlockIC2Fence.IC2FenceType.iron));
+			OreDictionary.registerOre("rubberWood", BlockName.rubber_wood.getItemStack());
+			OreDictionary.registerOre("glassReinforced", BlockName.glass.getItemStack(BlockTexGlass.GlassType.reinforced));
+
+			OreDictionary.registerOre("oreIridium", ItemName.misc_resource.getItemStack(MiscResourceType.iridium_ore));
+
+			OreDictionary.registerOre("plateIridium", ItemName.crafting.getItemStack(CraftingItemType.iridium));
+
+
 		} catch (NoClassDefFoundError notFound) {
 			Core.logHelper.warn(
 				"Can't enable integration: IC2 installed but cannot be hooked\n" +
