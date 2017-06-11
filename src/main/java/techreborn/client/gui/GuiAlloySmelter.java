7def0ecc597d779fe8aabee31a6689de57798fd1
Wanted_b14 <cross.wanted.1.4@gmail.com>
2017-06-11 11:32:30 +0300

    Fixes #347, #510

--- src/main/java/techreborn/client/gui/GuiAlloySmelter.java
+++ src/main/java/techreborn/client/gui/GuiAlloySmelter.java
@@ -7,6 +7,7 @@ import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.StatCollector;
 import techreborn.client.container.ContainerAlloySmelter;
 import techreborn.tiles.TileAlloySmelter;
+import org.lwjgl.opengl.GL11;
 
 public class GuiAlloySmelter extends GuiContainer {
 
@@ -30,6 +31,7 @@ public class GuiAlloySmelter extends GuiContainer {
 
     @Override
     protected void drawGuiContainerBackgroundLayer(float p_146976_1_, int p_146976_2_, int p_146976_3_) {
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
         this.mc.getTextureManager().bindTexture(texture);
         int k = (this.width - this.xSize) / 2;
         int l = (this.height - this.ySize) / 2;
