f1706f02bb8724393ef59a5db62f86b25290ebf4
Wanted_b14 <cross.wanted.1.4@gmail.com>
2017-06-11 11:42:52 +0300

    Fixes #347, #510

--- src/main/java/techreborn/client/gui/GuiFusionReactor.java
+++ src/main/java/techreborn/client/gui/GuiFusionReactor.java
@@ -16,6 +16,7 @@ import techreborn.client.container.ContainerFusionReactor;
 import techreborn.init.ModBlocks;
 import techreborn.proxies.ClientProxy;
 import techreborn.tiles.fusionReactor.TileEntityFusionController;
+import org.lwjgl.opengl.GL11;
 
 
 public class GuiFusionReactor extends GuiContainer {
@@ -62,6 +63,7 @@ public class GuiFusionReactor extends GuiContainer {
 
     @Override
     protected void drawGuiContainerBackgroundLayer(float p_146976_1_, int p_146976_2_, int p_146976_3_) {
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
         this.mc.getTextureManager().bindTexture(texture);
         int k = (this.width - this.xSize) / 2;
         int l = (this.height - this.ySize) / 2;
