3400f42962e9fc625f9d06536dab2c8a94a3de8f
modmuss50 <modmuss50@gmail.com>
2017-06-13 21:27:06 +0100

    Lighten the oil texture, might revert it im not sure?

--- src/main/java/techreborn/blocks/advanced_machine/BlockDistillationTower.java
+++ src/main/java/techreborn/blocks/advanced_machine/BlockDistillationTower.java
@@ -26,10 +26,9 @@ package techreborn.blocks.advanced_machine;
 
 import net.minecraft.block.material.Material;
 import reborncore.common.blocks.BlockMachineBase;
-import reborncore.common.blocks.IRotationTexture;
 import techreborn.client.TechRebornCreativeTab;
 
-public class BlockDistillationTower extends BlockMachineBase implements IRotationTexture {
+public class BlockDistillationTower extends BlockMachineBase {
 
 	private final String prefix = "techreborn:blocks/machine/advanced_machines/";
 
@@ -37,30 +36,5 @@ public class BlockDistillationTower extends BlockMachineBase implements IRotatio
 		super();
 		setUnlocalizedName("techreborn.distillationtower");
 		setCreativeTab(TechRebornCreativeTab.instance);
-	}
-
-	@Override
-	public String getFrontOff() {
-		return prefix + "distillation_tower_front_off";
-	}
-
-	@Override
-	public String getFrontOn() {
-		return prefix + "distillation_tower_front_off";
-	}
-
-	@Override
-	public String getSide() {
-		return prefix + "advanced_machine_side";
-	}
-
-	@Override
-	public String getTop() {
-		return prefix + "industrial_centrifuge_top_off";
-	}
-
-	@Override
-	public String getBottom() {
-		return prefix + "industrial_centrifuge_bottom";
 	}
 }
