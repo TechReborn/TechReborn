ae71797d2879e0ad3c3734fa20bfa3dfe49dcdbd
modmuss50 <modmuss50@gmail.com>
2017-06-29 13:11:59 +0100

    Draw default background on GUI's might help fix some issues

--- src/main/java/techreborn/client/gui/GuiBase.java
+++ src/main/java/techreborn/client/gui/GuiBase.java
@@ -139,6 +139,7 @@ public class GuiBase extends GuiContainer {
 
 	@Override
 	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+		this.drawDefaultBackground();
 		super.drawScreen(mouseX, mouseY, partialTicks);
 		this.renderHoveredToolTip(mouseX, mouseY);
 	}
