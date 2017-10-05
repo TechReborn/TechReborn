fed439307e4efedec18eb88e8879cfd9a5741679
Dimmerworld <Quadcorenerd@gmail.com>
2017-10-05 23:55:56 +1100

    Tile entity work

--- src/main/java/techreborn/blocks/generator/solarpanel/EnumPanelType.java
+++ src/main/java/techreborn/blocks/generator/solarpanel/EnumPanelType.java
@@ -4,27 +4,38 @@ import net.minecraft.util.IStringSerializable;
 import reborncore.api.power.EnumPowerTier;
 
 public enum EnumPanelType implements IStringSerializable {
-	Basic("basic", 128, EnumPowerTier.LOW, 0),
-	Hybrid("hybrid", 32, EnumPowerTier.MEDIUM, 1),
-	Advanced("advanced", 512, EnumPowerTier.MEDIUM, 2),
-	Ultimate("ultimate", 2048, EnumPowerTier.HIGH, 3),
-	Quantum("quantum", 2048, EnumPowerTier.EXTREME, 4);
+	Basic("basic", 128, 1000, 50000, EnumPowerTier.LOW, 0),
+	Hybrid("hybrid", 32, 2000, 100000, EnumPowerTier.MEDIUM, 1),
+	Advanced("advanced", 512, 3000, 200000, EnumPowerTier.MEDIUM, 2),
+	Ultimate("ultimate", 2048, 4000, 300000,  EnumPowerTier.HIGH, 3),
+	Quantum("quantum", 2048, 5000, 400000, EnumPowerTier.EXTREME, 4);
 
 	private int ID;
-	public int generationRate = 128;
 	private String friendlyName;
 
-	EnumPanelType(String friendlyName, int generationRate,  EnumPowerTier tier, int ID) {
+	public int generationRateD = 128;
+	// Generation of FE during Day
+
+	public int generationRateN = 64;
+	// Generation of FE during Night
+
+	public int internalCapacity = 0;
+	// Internal FE storage of solar panel
+
+
+	EnumPanelType(String friendlyName, int generationRateD, int generationRateN, int internalCapacity,  EnumPowerTier tier, int ID) {
 		this.friendlyName = friendlyName;
-		this.generationRate = generationRate;
 		this.ID = ID;
+		this.generationRateD = generationRateD;
+		this.generationRateN = generationRateN;
+		this.internalCapacity = internalCapacity;
 	}
 
 
 
 	@Override
 	public String getName() {
-		return friendlyName.toLowerCase();
+		return friendlyName;
 	}
 
 
