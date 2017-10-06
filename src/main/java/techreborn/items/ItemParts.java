5d0b5c3ea3171fa5ce73946c89d626de66af87a9
Dimmerworld <Quadcorenerd@gmail.com>
2017-10-06 23:27:57 +1100

    Balance, textures and items

--- src/main/java/techreborn/items/ItemParts.java
+++ src/main/java/techreborn/items/ItemParts.java
@@ -45,7 +45,7 @@ public class ItemParts extends ItemTR {
 		"thorium_cell", "double_thorium_cell", "quad_thorium_cell", "plutonium_cell", "double_plutonium_cell",
 		"quad_plutonium_cell", "computer_monitor", "machine_parts", "neutron_reflector", "iridium_neutron_reflector",
 		"thick_neutron_reflector", "electronic_circuit", "advanced_circuit", "sap", "rubber", "scrap",
-		"carbon_mesh", "carbon_fiber", "coolant_simple", "coolant_triple", "coolant_six" };
+		"carbon_mesh", "carbon_fiber", "coolant_simple", "coolant_triple", "coolant_six", "solar_cell","compact_solar_cell","quantum_singularity" };
 
 	public ItemParts() {
 		this.setCreativeTab(TechRebornCreativeTab.instance);
