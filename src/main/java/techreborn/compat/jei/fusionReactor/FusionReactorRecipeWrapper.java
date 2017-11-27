9a8a7455ff85c0db8f1a9ad6068a177c82b464c7
drcrazy <hcb@wowhcb.ru>
2017-11-27 17:58:37 +0300

    Fixed warning for FusionReactorRecipeWrapper

--- src/main/java/techreborn/compat/jei/fusionReactor/FusionReactorRecipeWrapper.java
+++ src/main/java/techreborn/compat/jei/fusionReactor/FusionReactorRecipeWrapper.java
@@ -25,7 +25,7 @@
 package techreborn.compat.jei.fusionReactor;
 
 import mezz.jei.api.ingredients.IIngredients;
-import mezz.jei.api.recipe.BlankRecipeWrapper;
+import mezz.jei.api.recipe.IRecipeWrapper;
 import net.minecraft.client.Minecraft;
 import net.minecraft.item.ItemStack;
 import techreborn.api.reactor.FusionReactorRecipe;
@@ -34,7 +34,7 @@ import techreborn.compat.jei.RecipeUtil;
 import javax.annotation.Nonnull;
 import java.util.Arrays;
 
-public class FusionReactorRecipeWrapper extends BlankRecipeWrapper {
+public class FusionReactorRecipeWrapper implements IRecipeWrapper {
 	private final FusionReactorRecipe baseRecipe;
 
 	public FusionReactorRecipeWrapper(FusionReactorRecipe baseRecipe) {
@@ -42,9 +42,7 @@ public class FusionReactorRecipeWrapper extends BlankRecipeWrapper {
 	}
 
 	@Override
-	public void getIngredients(
-		@Nonnull
-			IIngredients ingredients) {
+	public void getIngredients(@Nonnull IIngredients ingredients) {
 		ingredients.setInputs(ItemStack.class, Arrays.asList(baseRecipe.getTopInput(), baseRecipe.getBottomInput()));
 		ingredients.setOutput(ItemStack.class, baseRecipe.getOutput());
 	}
@@ -58,10 +56,7 @@ public class FusionReactorRecipeWrapper extends BlankRecipeWrapper {
 	}
 
 	@Override
-	public void drawInfo(
-		@Nonnull
-			Minecraft minecraft, int recipeWidth, int recipeHeight, int mouseX, int mouseY) {
-		RecipeUtil.drawInfo(minecraft, 0, 67, baseRecipe.getStartEU(), baseRecipe.getEuTick(),
-			baseRecipe.getTickTime());
+	public void drawInfo(@Nonnull Minecraft minecraft, int recipeWidth, int recipeHeight, int mouseX, int mouseY) {
+		RecipeUtil.drawInfo(minecraft, 0, 67, baseRecipe.getStartEU(), baseRecipe.getEuTick(), baseRecipe.getTickTime());
 	}
 }
