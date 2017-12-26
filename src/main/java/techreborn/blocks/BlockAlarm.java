e93451b5951358c19d55228842290ad6aa640d6b
Dimmerworld <Quadcorenerd@gmail.com>
2017-12-26 12:30:36 +1100

    Woop woop - Alarm work (#1380)
    * Alarm makeover
    
    Model,
    boundrybox, 
    textures,
    name  localization for the alarm
    
    * Actual translations for Alarm
    
    * Texture resize
    
    As requested
    (Didn't notice dimensions when I was grabbing off Prospector)

--- src/main/java/techreborn/blocks/BlockAlarm.java
+++ src/main/java/techreborn/blocks/BlockAlarm.java
@@ -2,19 +2,30 @@ package techreborn.blocks;
 
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.EnumBlockRenderType;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
+import prospector.shootingstar.ShootingStar;
+import prospector.shootingstar.model.ModelCompound;
 import techreborn.client.TechRebornCreativeTab;
+import techreborn.lib.ModInfo;
 import techreborn.tiles.TileAlarm;
 
 import javax.annotation.Nullable;
 
+//TODO: On/off variants, Placeable on walls/roofs(faces), Visible when holding in hand, and recipe.
+
 public class BlockAlarm extends BlockContainer {
+
 	public BlockAlarm() {
 		super(Material.ROCK);
 		setUnlocalizedName("techreborn.alarm");
 		setCreativeTab(TechRebornCreativeTab.instance);
-
+		ShootingStar.registerModel(new ModelCompound(ModInfo.MOD_ID, this, "machines/lighting"));
 	}
 
 	@Nullable
@@ -22,4 +33,30 @@ public class BlockAlarm extends BlockContainer {
 	public TileEntity createNewTileEntity(World worldIn, int meta) {
 		return new TileAlarm();
 	}
+
+	@Override
+	public EnumBlockRenderType getRenderType(IBlockState state) {
+		return EnumBlockRenderType.MODEL;
+	}
+
+	@Override
+	public boolean isOpaqueCube(IBlockState state) {
+		return false;
+	}
+
+	@Override
+	public boolean isFullBlock(IBlockState state) {
+		return false;
+	}
+
+	@Override
+	public boolean isFullCube(IBlockState state) {
+		return false;
+	}
+
+	@Override
+	public AxisAlignedBB getBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
+		return new AxisAlignedBB(0.19, 0.0, 0.19, 0.81, 0.18, 0.81);
+	}
+
 }
