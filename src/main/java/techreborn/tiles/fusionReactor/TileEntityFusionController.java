9908e847641ccda5c0351a5cb042afe7a7dfc41e
drcrazy <hcb@wowhcb.ru>
2018-01-22 15:37:19 +0300

    Backport of 0ae99ebae8b6b30e25db859e1b8c0416b60ce95d to 1.10.2

--- src/main/java/techreborn/tiles/fusionReactor/TileEntityFusionController.java
+++ src/main/java/techreborn/tiles/fusionReactor/TileEntityFusionController.java
@@ -37,7 +37,7 @@ import reborncore.common.IWrenchable;
 import reborncore.common.powerSystem.TilePowerAcceptor;
 import reborncore.common.util.Inventory;
 import reborncore.common.util.ItemUtils;
-
+import slimeknights.tconstruct.plugin.jei.CraftingStationRecipeTransferInfo;
 import techreborn.api.reactor.FusionReactorRecipe;
 import techreborn.api.reactor.FusionReactorRecipeHelper;
 import techreborn.client.container.IContainerProvider;
@@ -45,7 +45,8 @@ import techreborn.client.container.builder.BuiltContainer;
 import techreborn.client.container.builder.ContainerBuilder;
 import techreborn.init.ModBlocks;
 
-public class TileEntityFusionController extends TilePowerAcceptor implements IInventoryProvider, IContainerProvider, IWrenchable {
+public class TileEntityFusionController extends TilePowerAcceptor
+		implements IInventoryProvider, IContainerProvider, IWrenchable {
 
 	public Inventory inventory = new Inventory(3, "TileEntityFusionController", 64, this);
 
@@ -63,7 +64,98 @@ public class TileEntityFusionController extends TilePowerAcceptor implements IIn
 	public TileEntityFusionController() {
 		super(4);
 	}
+	
+	public boolean checkCoils() {
+		int posX = this.getPos().getX();
+		int posY = this.getPos().getY();
+		int posZ = this.getPos().getZ();
 
+		if (isCoil(posX + 3, posY, posZ + 1) && isCoil(posX + 3, posY, posZ) && isCoil(posX + 3, posY, posZ - 1)
+				&& isCoil(posX - 3, posY, posZ + 1) && isCoil(posX - 3, posY, posZ) && isCoil(posX - 3, posY, posZ - 1)
+				&& isCoil(posX + 2, posY, posZ + 2) && isCoil(posX + 2, posY, posZ + 1)
+				&& isCoil(posX + 2, posY, posZ - 1) && isCoil(posX + 2, posY, posZ - 2)
+				&& isCoil(posX - 2, posY, posZ + 2) && isCoil(posX - 2, posY, posZ + 1)
+				&& isCoil(posX - 2, posY, posZ - 1) && isCoil(posX - 2, posY, posZ - 2)
+				&& isCoil(posX + 1, posY, posZ + 3) && isCoil(posX + 1, posY, posZ + 2)
+				&& isCoil(posX + 1, posY, posZ - 2) && isCoil(posX + 1, posY, posZ - 3)
+				&& isCoil(posX - 1, posY, posZ + 3) && isCoil(posX - 1, posY, posZ + 2)
+				&& isCoil(posX - 1, posY, posZ - 2) && isCoil(posX - 1, posY, posZ - 3) && isCoil(posX, posY, posZ + 3)
+				&& isCoil(posX, posY, posZ - 3)) {
+			coilStatus = 1;
+			return true;
+		}
+		coilStatus = 0;
+		return false;
+	}
+
+	private boolean isCoil(final int x, final int y, final int z) {
+		return this.world.getBlockState(new BlockPos(x, y, z)).getBlock() == ModBlocks.FUSION_COIL;
+	}
+	
+	private void resetCrafter() {
+		this.currentRecipe = null;
+		this.crafingTickTime = 0;
+		this.finalTickTime = 0;
+		this.neededPower = 0;
+		this.hasStartedCrafting = false;
+	}
+
+	/**
+	 * Checks to see if it can fit given stack into given slot
+	 * 
+	 * @param stack Itemstack to fit
+	 * @param slot Int slot ID to fit into
+	 * @param oreDic boolean Use oreDictionary
+	 * @return boolean true if stack will fit into slot
+	 */
+	public boolean canFitStack(final ItemStack stack, final int slot, final boolean oreDic) {
+		if (stack == null) {
+			return true;
+		}
+		if (this.inventory.getStackInSlot(slot) == null) {
+			return true;
+		}
+		if (ItemUtils.isItemEqual(this.inventory.getStackInSlot(slot), stack, true, true, oreDic)) {
+			if (stack.stackSize + this.inventory.getStackInSlot(slot).stackSize <= stack.getMaxStackSize()) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	public int getProgressScaled() {
+		return Math.max(0, Math.min(24, (this.getCrafingTickTime() > 0 ? 1 : 0)
+				+ this.getCrafingTickTime() * 24 / (this.finalTickTime < 1 ? 1 : this.finalTickTime)));
+	}
+	
+	private void updateCurrentRecipe() {
+		for (final FusionReactorRecipe reactorRecipe : FusionReactorRecipeHelper.reactorRecipes) {
+			if (validateReactorRecipe(reactorRecipe)) {
+				this.currentRecipe = reactorRecipe;
+				this.crafingTickTime = 0;
+				this.finalTickTime = this.currentRecipe.getTickTime();
+				this.neededPower = (int) this.currentRecipe.getStartEU();
+				this.hasStartedCrafting = false;
+				break;				
+			}
+		}
+	}
+	
+	private boolean validateReactorRecipe(FusionReactorRecipe recipe) {
+		if (ItemUtils.isItemEqual(this.getStackInSlot(topStackSlot), recipe.getTopInput(), true, true, true)) {
+			if (recipe.getBottomInput() != null) {
+				if (!ItemUtils.isItemEqual(this.getStackInSlot(bottomStackSlot), recipe.getBottomInput(), true, true, true)) {
+					return false;
+				}
+			}
+			if (this.canFitStack(recipe.getOutput(), outputStackSlot, true)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	// TilePowerAcceptor
 	@Override
 	public double getMaxPower() {
 		return 100000000;
@@ -129,174 +221,79 @@ public class TileEntityFusionController extends TilePowerAcceptor implements IIn
 		return tagCompound;
 	}
 
-	public boolean checkCoils() {
-		if (this.isCoil(this.getPos().getX() + 3, this.getPos().getY(), this.getPos().getZ() + 1)
-				&& this.isCoil(this.getPos().getX() + 3, this.getPos().getY(), this.getPos().getZ())
-				&& this.isCoil(this.getPos().getX() + 3, this.getPos().getY(), this.getPos().getZ() - 1)
-				&& this.isCoil(this.getPos().getX() - 3, this.getPos().getY(), this.getPos().getZ() + 1)
-				&& this.isCoil(this.getPos().getX() - 3, this.getPos().getY(), this.getPos().getZ())
-				&& this.isCoil(this.getPos().getX() - 3, this.getPos().getY(), this.getPos().getZ() - 1)
-				&& this.isCoil(this.getPos().getX() + 2, this.getPos().getY(), this.getPos().getZ() + 2)
-				&& this.isCoil(this.getPos().getX() + 2, this.getPos().getY(), this.getPos().getZ() + 1)
-				&& this.isCoil(this.getPos().getX() + 2, this.getPos().getY(), this.getPos().getZ() - 1)
-				&& this.isCoil(this.getPos().getX() + 2, this.getPos().getY(), this.getPos().getZ() - 2)
-				&& this.isCoil(this.getPos().getX() - 2, this.getPos().getY(), this.getPos().getZ() + 2)
-				&& this.isCoil(this.getPos().getX() - 2, this.getPos().getY(), this.getPos().getZ() + 1)
-				&& this.isCoil(this.getPos().getX() - 2, this.getPos().getY(), this.getPos().getZ() - 1)
-				&& this.isCoil(this.getPos().getX() - 2, this.getPos().getY(), this.getPos().getZ() - 2)
-				&& this.isCoil(this.getPos().getX() + 1, this.getPos().getY(), this.getPos().getZ() + 3)
-				&& this.isCoil(this.getPos().getX() + 1, this.getPos().getY(), this.getPos().getZ() + 2)
-				&& this.isCoil(this.getPos().getX() + 1, this.getPos().getY(), this.getPos().getZ() - 2)
-				&& this.isCoil(this.getPos().getX() + 1, this.getPos().getY(), this.getPos().getZ() - 3)
-				&& this.isCoil(this.getPos().getX() - 1, this.getPos().getY(), this.getPos().getZ() + 3)
-				&& this.isCoil(this.getPos().getX() - 1, this.getPos().getY(), this.getPos().getZ() + 2)
-				&& this.isCoil(this.getPos().getX() - 1, this.getPos().getY(), this.getPos().getZ() - 2)
-				&& this.isCoil(this.getPos().getX() - 1, this.getPos().getY(), this.getPos().getZ() - 3)
-				&& this.isCoil(this.getPos().getX(), this.getPos().getY(), this.getPos().getZ() + 3)
-				&& this.isCoil(this.getPos().getX(), this.getPos().getY(), this.getPos().getZ() - 3)) {
-			this.coilStatus = 1;
-			return true;
-		}
-		this.coilStatus = 0;
-		return false;
-	}
-
-	private boolean isCoil(final int x, final int y, final int z) {
-		return this.world.getBlockState(new BlockPos(x, y, z)).getBlock() == ModBlocks.FUSION_COIL;
-	}
-
 	@Override
 	public void updateEntity() {
 		super.updateEntity();
-		// TODO improve this code a lot
 
-		if (this.world.getTotalWorldTime() % 20 == 0) {
-			this.checkCoils();
+		if (this.world.isRemote) {
+			return;
 		}
 
-		if (!this.world.isRemote) {
-			if (this.coilStatus == 1) {
-				if (this.currentRecipe == null) {
-					if (this.inventory.hasChanged || this.crafingTickTime != 0) {
-						for (final FusionReactorRecipe reactorRecipe : FusionReactorRecipeHelper.reactorRecipes) {
-							if (ItemUtils.isItemEqual(this.getStackInSlot(this.topStackSlot), reactorRecipe.getTopInput(), true,
-									true, true)) {
-								if (reactorRecipe.getBottomInput() != null) {
-									if (!ItemUtils.isItemEqual(this.getStackInSlot(this.bottomStackSlot),
-											reactorRecipe.getBottomInput(), true, true, true)) {
-										break;
-									}
-								}
-								if (this.canFitStack(reactorRecipe.getOutput(), this.outputStackSlot, true)) {
-									this.currentRecipe = reactorRecipe;
-									if (this.crafingTickTime != 0) {
-										this.finalTickTime = this.currentRecipe.getTickTime();
-										this.neededPower = (int) this.currentRecipe.getStartEU();
-									}
-									this.crafingTickTime = 0;
-									this.finalTickTime = this.currentRecipe.getTickTime();
-									this.neededPower = (int) this.currentRecipe.getStartEU();
-									break;
-								}
-							}
-						}
-					}
-				} else {
-					if (this.inventory.hasChanged) {
-						if (!this.validateRecipe()) {
-							this.resetCrafter();
-							return;
-						}
-					}
-					if (!this.hasStartedCrafting) {
-						if (this.canUseEnergy(this.currentRecipe.getStartEU() + 64)) {
-							this.useEnergy(this.currentRecipe.getStartEU());
-							this.hasStartedCrafting = true;
-						}
-					} else {
-						if (this.crafingTickTime < this.currentRecipe.getTickTime()) {
-							if (this.currentRecipe.getEuTick() > 0) { // Power gen
-								this.addEnergy(this.currentRecipe.getEuTick()); // Waste
-								// power
-								// if it
-								// has
-								// no
-								// where
-								// to go
-								this.crafingTickTime++;
-							} else { // Power user
-								if (this.canUseEnergy(this.currentRecipe.getEuTick() * -1)) {
-									this.setEnergy(this.getEnergy() - this.currentRecipe.getEuTick() * -1);
-									this.crafingTickTime++;
-								}
-							}
-						} else {
-							if (this.canFitStack(this.currentRecipe.getOutput(), this.outputStackSlot, true)) {
-								if (this.getStackInSlot(this.outputStackSlot) == null) {
-									this.setInventorySlotContents(this.outputStackSlot, this.currentRecipe.getOutput().copy());
-								} else {
-									this.decrStackSize(this.outputStackSlot, -this.currentRecipe.getOutput().stackSize);
-								}
-								this.decrStackSize(this.topStackSlot, this.currentRecipe.getTopInput().stackSize);
-								if (this.currentRecipe.getBottomInput() != null) {
-									this.decrStackSize(this.bottomStackSlot, this.currentRecipe.getBottomInput().stackSize);
-								}
-								this.resetCrafter();
-							}
-						}
-					}
-                    this.markDirty();
-				}
-			} else {
-				if (this.currentRecipe != null) {
-					this.resetCrafter();
+		// Force check every second
+		if (this.world.getTotalWorldTime() % 20 == 0) {
+			checkCoils();
+			this.inventory.hasChanged = true;
+		}
+		
+		if (this.coilStatus == 0) {
+			resetCrafter();
+			return;
+		}
+		
+		if (this.currentRecipe == null && this.inventory.hasChanged == true) {
+			updateCurrentRecipe();
+		}
+		
+		if (this.currentRecipe != null) {
+			if (!this.hasStartedCrafting) {
+				if (this.canUseEnergy(this.currentRecipe.getStartEU())) {
+					this.useEnergy(this.currentRecipe.getStartEU());
+					this.hasStartedCrafting = true;
 				}
 			}
-		}
 
+			if (this.hasStartedCrafting && this.crafingTickTime < this.finalTickTime) {
+				// Power gen
+				if (this.currentRecipe.getEuTick() > 0) {
+					this.addEnergy(this.currentRecipe.getEuTick());
+					this.crafingTickTime++;
+				}
+				// Power user
+				else {
+					if (this.canUseEnergy(this.currentRecipe.getEuTick() * -1)) {
+						this.setEnergy(this.getEnergy() - this.currentRecipe.getEuTick() * -1);
+						this.crafingTickTime++;
+					}
+				}
+			} else if (this.crafingTickTime >= this.finalTickTime) {
+				if (this.canFitStack(this.currentRecipe.getOutput(), outputStackSlot, true)) {
+					if (this.getStackInSlot(outputStackSlot) == null) {
+						this.setInventorySlotContents(outputStackSlot, this.currentRecipe.getOutput().copy());
+					} else {
+						this.decrStackSize(outputStackSlot, -this.currentRecipe.getOutput().stackSize);
+					}
+					this.decrStackSize(topStackSlot, this.currentRecipe.getTopInput().stackSize);
+					if (this.currentRecipe.getBottomInput() != null) {
+						this.decrStackSize(bottomStackSlot, this.currentRecipe.getBottomInput().stackSize);
+					}
+					if (validateReactorRecipe(this.currentRecipe)) {
+						this.crafingTickTime = 0;
+					} else {
+						resetCrafter();
+					}
+				} else {
+					resetCrafter();
+				}
+			}
+			this.markDirty();
+		}
+		
 		if (this.inventory.hasChanged) {
 			this.inventory.hasChanged = false;
 		}
 	}
-
-	private boolean validateRecipe() {
-		if (ItemUtils.isItemEqual(this.getStackInSlot(this.topStackSlot), this.currentRecipe.getTopInput(), true, true, true)) {
-			if (this.currentRecipe.getBottomInput() != null) {
-				if (!ItemUtils.isItemEqual(this.getStackInSlot(this.bottomStackSlot), this.currentRecipe.getBottomInput(), true, true,
-						true)) {
-					return false;
-				}
-			}
-			if (this.canFitStack(this.currentRecipe.getOutput(), this.outputStackSlot, true)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	private void resetCrafter() {
-		this.currentRecipe = null;
-		this.crafingTickTime = -1;
-		this.finalTickTime = -1;
-		this.neededPower = -1;
-		this.hasStartedCrafting = false;
-	}
-
-	public boolean canFitStack(final ItemStack stack, final int slot, final boolean oreDic) {// Checks to see if it can fit the stack
-		if (stack == null) {
-			return true;
-		}
-		if (this.inventory.getStackInSlot(slot) == null) {
-			return true;
-		}
-		if (ItemUtils.isItemEqual(this.inventory.getStackInSlot(slot), stack, true, true, oreDic)) {
-			if (stack.stackSize + this.inventory.getStackInSlot(slot).stackSize <= stack.getMaxStackSize()) {
-				return true;
-			}
-		}
-		return false;
-	}
-
+	
+	// TileLegacyMachineBase
 	@Override
 	public String getName() {
 		return null;
@@ -336,11 +333,23 @@ public class TileEntityFusionController extends TilePowerAcceptor implements IIn
 		return index == 2;
 	}
 
+	// IInventoryProvider
 	@Override
 	public Inventory getInventory() {
 		return this.inventory;
 	}
 
+	// IContainerProvider
+	@Override
+	public BuiltContainer createContainer(final EntityPlayer player) {
+		return new ContainerBuilder("fusionreactor").player(player.inventory).inventory(8, 84).hotbar(8, 142)
+				.addInventory().tile(this).slot(0, 88, 17).slot(1, 88, 53).outputSlot(2, 148, 35).syncEnergyValue()
+				.syncIntegerValue(this::getCoilStatus, this::setCoilStatus)
+				.syncIntegerValue(this::getCrafingTickTime, this::setCrafingTickTime)
+				.syncIntegerValue(this::getFinalTickTime, this::setFinalTickTime)
+				.syncIntegerValue(this::getNeededPower, this::setNeededPower).addInventory().create();
+	}
+	
 	public int getCoilStatus() {
 		return this.coilStatus;
 	}
@@ -373,21 +382,7 @@ public class TileEntityFusionController extends TilePowerAcceptor implements IIn
 		this.neededPower = neededPower;
 	}
 
-	public int getProgressScaled() {
-		return Math.max(0, Math.min(24, (this.getCrafingTickTime() > 0 ? 1 : 0)
-				+ this.getCrafingTickTime() * 24 / (this.finalTickTime < 1 ? 1 : this.finalTickTime)));
-	}
-
-	@Override
-	public BuiltContainer createContainer(final EntityPlayer player) {
-		return new ContainerBuilder("fusionreactor").player(player.inventory).inventory(8, 84).hotbar(8, 142)
-				.addInventory().tile(this).slot(0, 88, 17).slot(1, 88, 53).outputSlot(2, 148, 35).syncEnergyValue()
-				.syncIntegerValue(this::getCoilStatus, this::setCoilStatus)
-				.syncIntegerValue(this::getCrafingTickTime, this::setCrafingTickTime)
-				.syncIntegerValue(this::getFinalTickTime, this::setFinalTickTime)
-				.syncIntegerValue(this::getNeededPower, this::setNeededPower).addInventory().create();
-	}
-
+	// IWrenchable
 	@Override
 	public boolean wrenchCanSetFacing(EntityPlayer entityPlayer, EnumFacing enumFacing) {
 		return !entityPlayer.isSneaking();
