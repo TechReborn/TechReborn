5e18a9ee2d8127fbe138c6dbc153dcf6f266592b
modmuss50 <modmuss50@gmail.com>
2018-04-08 21:01:44 +0100

    Fix extractor recipes using ore dict when they aren't supposed to.

--- src/main/java/techreborn/init/recipes/ExtractorRecipes.java
+++ src/main/java/techreborn/init/recipes/ExtractorRecipes.java
@@ -39,41 +39,41 @@ import techreborn.items.DynamicCell;
  */
 public class ExtractorRecipes extends RecipeMethods {
 	public static void init() {
-		register(getStack(ModBlocks.RUBBER_SAPLING), false, getMaterial("rubber", Type.PART));
-		register(getStack(ModBlocks.RUBBER_LOG), false, getMaterial("rubber", Type.PART));
+		register(getStack(ModBlocks.RUBBER_SAPLING), getMaterial("rubber", Type.PART), false);
+		register(getStack(ModBlocks.RUBBER_LOG),  getMaterial("rubber", Type.PART), false);
 		register(getStack(Items.SLIME_BALL), getMaterial("rubber", 2, Type.PART));
 		register(getMaterial("sap", Type.PART), getMaterial("rubber", 3, Type.PART));
-		register(getStack(Blocks.RED_FLOWER), getStack(Items.DYE, 2, 1));
-		register(getStack(Blocks.YELLOW_FLOWER), getStack(Items.DYE, 2, 11));
-		register(getStack(Blocks.RED_FLOWER, 1, 1), getStack(Items.DYE, 2, 12));
-		register(getStack(Blocks.RED_FLOWER, 1, 2), getStack(Items.DYE, 2, 13));
-		register(getStack(Blocks.RED_FLOWER, 1, 3), getStack(Items.DYE, 2, 7));
-		register(getStack(Blocks.RED_FLOWER, 1, 4), getStack(Items.DYE, 2, 1));
-		register(getStack(Blocks.RED_FLOWER, 1, 5), getStack(Items.DYE, 2, 14));
-		register(getStack(Blocks.RED_FLOWER, 1, 6), getStack(Items.DYE, 2, 7));
-		register(getStack(Blocks.RED_FLOWER, 1, 7), getStack(Items.DYE, 2, 9));
-		register(getStack(Blocks.RED_FLOWER, 1, 8), getStack(Items.DYE, 2, 7));
-		register(getStack(Blocks.DOUBLE_PLANT), getStack(Items.DYE, 4, 11));
-		register(getStack(Blocks.DOUBLE_PLANT, 1, 1), getStack(Items.DYE, 4, 13));
-		register(getStack(Blocks.DOUBLE_PLANT, 1, 4), getStack(Items.DYE, 4, 1));
-		register(getStack(Blocks.DOUBLE_PLANT, 1, 5), getStack(Items.DYE, 4, 9));
-		register(getStack(Blocks.TALLGRASS, 1, 1), getStack(Items.WHEAT_SEEDS));
-		register(getStack(Blocks.TALLGRASS, 1, 2), getStack(Items.WHEAT_SEEDS));
-		register(getStack(Blocks.DOUBLE_PLANT, 1, 2), getStack(Items.WHEAT_SEEDS, 2));
-		register(getStack(Blocks.DOUBLE_PLANT, 1, 3), getStack(Items.WHEAT_SEEDS, 2));
+		register(getStack(Blocks.RED_FLOWER), getStack(Items.DYE, 2, 1), false);
+		register(getStack(Blocks.YELLOW_FLOWER), getStack(Items.DYE, 2, 11), false);
+		register(getStack(Blocks.RED_FLOWER, 1, 1), getStack(Items.DYE, 2, 12), false);
+		register(getStack(Blocks.RED_FLOWER, 1, 2), getStack(Items.DYE, 2, 13), false);
+		register(getStack(Blocks.RED_FLOWER, 1, 3), getStack(Items.DYE, 2, 7), false);
+		register(getStack(Blocks.RED_FLOWER, 1, 4), getStack(Items.DYE, 2, 1), false);
+		register(getStack(Blocks.RED_FLOWER, 1, 5), getStack(Items.DYE, 2, 14), false);
+		register(getStack(Blocks.RED_FLOWER, 1, 6), getStack(Items.DYE, 2, 7), false);
+		register(getStack(Blocks.RED_FLOWER, 1, 7), getStack(Items.DYE, 2, 9), false);
+		register(getStack(Blocks.RED_FLOWER, 1, 8), getStack(Items.DYE, 2, 7), false);
+		register(getStack(Blocks.DOUBLE_PLANT), getStack(Items.DYE, 4, 11), false);
+		register(getStack(Blocks.DOUBLE_PLANT, 1, 1), getStack(Items.DYE, 4, 13), false);
+		register(getStack(Blocks.DOUBLE_PLANT, 1, 4), getStack(Items.DYE, 4, 1), false);
+		register(getStack(Blocks.DOUBLE_PLANT, 1, 5), getStack(Items.DYE, 4, 9), false);
+		register(getStack(Blocks.TALLGRASS, 1, 1), getStack(Items.WHEAT_SEEDS), false);
+		register(getStack(Blocks.TALLGRASS, 1, 2), getStack(Items.WHEAT_SEEDS), false);
+		register(getStack(Blocks.DOUBLE_PLANT, 1, 2), getStack(Items.WHEAT_SEEDS, 2), false);
+		register(getStack(Blocks.DOUBLE_PLANT, 1, 3), getStack(Items.WHEAT_SEEDS, 2), false);
 		register(getStack(Blocks.DEADBUSH, 1, 0), getStack(Items.STICK));
 		for (int i = 1; i < 15; i++)
-			register(getStack(Blocks.WOOL, 1, i), getStack(Blocks.WOOL, 1, 0));
+			register(getStack(Blocks.WOOL, 1, i), getStack(Blocks.WOOL, 1, 0), false);
 		for (Fluid fluid : FluidRegistry.getRegisteredFluids().values()) {
-			register(DynamicCell.getCellWithFluid(fluid), DynamicCell.getEmptyCell(1));
+			register(DynamicCell.getCellWithFluid(fluid), DynamicCell.getEmptyCell(1), false);
 		}
 	}
 
 	static void register(ItemStack input, ItemStack output) {
-		register(input, true, output);
+		register(input,  output, true);
 	}
 
-	static void register(ItemStack input, boolean oreDict, ItemStack output) {
+	static void register(ItemStack input, ItemStack output, boolean oreDict) {
 		RecipeHandler.addRecipe(new ExtractorRecipe(input, output, 400, 2, oreDict));
 	}
 }
