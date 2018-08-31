3190185a54cf4d8a648dd317cd420cea424e7d48
drcrazy <hcb@wowhcb.ru>
2018-09-01 02:46:25 +0300

    Cleaned fluid translation keys.

--- src/main/java/techreborn/init/ModTileEntities.java
+++ src/main/java/techreborn/init/ModTileEntities.java
@@ -58,98 +58,65 @@ import java.util.Optional;
 
 public enum ModTileEntities {
 
-	THERMAL_GEN(TileThermalGenerator.class,  "thermal_generator", "TileThermalGeneratorTR"),
-	QUANTUM_TANK(TileQuantumTank.class,  "quantum_tank", "TileQuantumTankTR"),
-	CREATIVE_QUANTUM_TANK(TileCreativeQuantumTank.class,  "creative_quantum_tank"),
-	QUANTUM_CHEST(TileQuantumChest.class,  "quantum_chest", "TileQuantumChestTR"),
-	CREATIVE_QUANTUM_CHEST(TileCreativeQuantumChest.class,  "creative_quantum_chest"),
-	DIGITAL_CHEST(TileDigitalChest.class,  "digital_chest", "TileDigitalChestTR"),
-	INDUSTRIAL_CENTRIFUGE(TileIndustrialCentrifuge.class,  "industrial_centrifuge", "TileIndustrialCentrifugeTR"),
-	ROLLING_MACHINE(TileRollingMachine.class,  "rolling_machine", "TileRollingMachineTR"),
-	INDUSTRIAL_BLAST_FURNACE(TileIndustrialBlastFurnace.class,  "industrial_blast_furnace", "TileIndustrialBlastFurnaceTR"),
-	ALLOY_SMELTER(TileAlloySmelter.class,  "alloy_smelter", "TileAlloySmalterTR"),
-	INDUSTRIAL_GRINDER(TileIndustrialGrinder.class,  "industrial_grinder", "TileIndustrialGrinderTR"),
-	IMPLOSION_COMPRESSOR(TileImplosionCompressor.class,  "implosion_compressor", "TileImplosionCompressorTR"),
-	MATTER_FABRICATOR(TileMatterFabricator.class,  "matter_fabricator", "TileMatterFabricatorTR"),
-	CHUNK_LOADER(TileChunkLoader.class,  "chunk_loader", "TileChunkLoaderTR"),
-	CHARGE_O_MAT(TileChargeOMat.class,  "charge_o_mat", "TileChargeOMatTR"),
-	PLAYER_DETECTOR(TilePlayerDectector.class,  "player_detector", "TilePlayerDectectorTR"),
-	CABLE(TileCable.class,  "cable", "TileCableTR"),
-	MACHINE_CASINGS(TileMachineCasing.class,  "machine_casing", "TileMachineCasingTR"),
-	DRAGON_EGG_SYPHON(TileDragonEggSyphon.class,  "dragon_egg_syphon", "TileDragonEggSyphonTR"),
-	ASSEMBLY_MACHINE(TileAssemblingMachine.class,  "assembly_machine", "TileAssemblyMachineTR"),
-	DIESEL_GENERATOR(TileDieselGenerator.class,  "diesel_generator", "TileDieselGeneratorTR"),
-	INDUSTRIAL_ELECTROLYZER(TileIndustrialElectrolyzer.class,  "industrial_electrolyzer", "TileIndustrialElectrolyzerTR"),
-	SEMI_FLUID_GENERATOR(TileSemiFluidGenerator.class,  "semi_fluid_generator", "TileSemiFluidGeneratorTR"),
-	GAS_TURBINE(TileGasTurbine.class,  "gas_turbine", "TileGasTurbineTR"),
-	IRON_ALLOY_FURNACE(TileIronAlloyFurnace.class,  "iron_alloy_furnace", "TileIronAlloyFurnaceTR"),
-	CHEMICAL_REACTOR(TileChemicalReactor.class,  "chemical_reactor", "TileChemicalReactorTR"),
-	INTERDIMENSIONAL_SU(TileInterdimensionalSU.class,  "interdimensional_su", "TileInterdimensionalSUTR"),
 	ADJUSTABLE_SU(TileAdjustableSU.class,  "adjustable_su", "TileAdjustableSUTR"),
-	LAPOTRONIC_SU(TileLapotronicSU.class,  "lapotronic_su", "TileLapotronicSUTR"),
-	LSU_STORAGE(TileLSUStorage.class,  "lsu_storage", "TileLSUStorageTR"),
-	DISTILLATION_TOWER(TileDistillationTower.class,  "distillation_tower", "TileDistillationTowerTR"),
-	VACUUM_FREEZER(TileVacuumFreezer.class,  "vacuum_freezer", "TileVacuumFreezerTR"),
-	FUSION_CONTROL_COMPUTER(TileFusionControlComputer.class,  "fusion_control_computer", "TileFusionControlComputerTR"),
-	LIGHTNING_ROD(TileLightningRod.class,  "lightning_rod", "TileLightningRodTR"),
-	INDUSTRIAL_SAWMILL(TileIndustrialSawmill.class,  "industrial_sawmill", "TileIndustrialSawmillTR"),
-	GRINDER(TileGrinder.class,  "grinder", "TileGrinderTR"),
-	SOLID_FUEL_GENEREATOR(TileSolidFuelGenerator.class,  "solid_fuel_generator", "TileSolidFuelGeneratorTR"),
-	EXTRACTOR(TileExtractor.class,  "extractor", "TileExtractorTR"),
-	COMPRESSOR(TileCompressor.class,  "compressor", "TileCompressorTR"),
-	ELECTRIC_FURNACE(TileElectricFurnace.class,  "electric_furnace", "TileElectricFurnaceTR"),
-	SOLAR_PANEL(TileSolarPanel.class,  "solar_panel", "TileSolarPanelTR"),
-	CREATIVE_SOLAR_PANEL(TileCreativeSolarPanel.class,  "creative_solar_panel", "TileCreativeSolarPanelTR"),
-	WATER_MILL(TileWaterMill.class,  "water_mill", "TileWaterMillTR"),
-	WIND_MILL(TileWindMill.class,  "wind_mill", "TileWindMillTR"),
-	MACHINE_BASE(TileLegacyMachineBase.class,  "machine_base", "TileMachineBaseTR"),
-	RECYCLER(TileRecycler.class,  "recycler", "TileRecyclerTR"),
-	LOW_VOLTAGE_SU(TileLowVoltageSU.class,  "low_voltage_su", "TileLowVoltageSUTR"),
-	MEDIUM_VOLTAGE_SU(TileMediumVoltageSU.class,  "medium_voltage_su", "TileMediumVoltageSUTR"),
-	HIGH_VOLTAGE_SU(TileHighVoltageSU.class,  "high_voltage_su", "TileHighVoltageSUTR"),
-	LV_TRANSFORMER(TileLVTransformer.class,  "lv_transformer", "TileLVTransformerTR"),
-	MV_TRANSFORMER(TileMVTransformer.class,  "mv_transformer", "TileMVTransformerTR"),
-	HV_TRANSFORMER(TileHVTransformer.class,  "hv_transformer", "TileHVTransformerTR"),
-	AUTO_CRAFTING_TABLE(TileAutoCraftingTable.class,  "auto_crafting_table", "TileAutoCraftingTableTR"),
-	IRON_FURNACE(TileIronFurnace.class,  "iron_furnace", "TileIronFurnaceTR"),
-	SCRAPBOXINATOR(TileScrapboxinator.class,  "scrapboxinator", "TileScrapboxinatorTR"),
-	PLASMA_GENERATOR(TilePlasmaGenerator.class,  "plasma_generator", "TilePlasmalGeneratorTR"),
-	LAMP(TileLamp.class,  "lamp", "TileLampTR"),
 	ALARM(TileAlarm.class,  "alarm", "TileAlarmTR"),
-	FLUID_REPLICATOR(TileFluidReplicator.class,  "fluid_replicator", "TileFluidReplicatorTR");
-
-	public Class<? extends TileEntity> tileClass;
-	public ResourceLocation name;
-	public Optional<String> oldName;
-
-	ModTileEntities(Class<? extends TileEntity> tileClass, ResourceLocation name, String oldName) {
-		this.tileClass = tileClass;
-		this.name = name;
-		this.oldName = Optional.of(oldName);
-	}
-
-	ModTileEntities(Class<? extends TileEntity> tileClass, ResourceLocation name) {
-		this.tileClass = tileClass;
-		this.name = name;
-		this.oldName = Optional.empty();
-	}
-
-	ModTileEntities(Class<? extends TileEntity> tileClass, String name, String oldName) {
-		this(tileClass, new ResourceLocation(ModInfo.MOD_ID, name), oldName);
-	}
-
-	ModTileEntities(Class<? extends TileEntity> tileClass, String name) {
-		this(tileClass,  new ResourceLocation(ModInfo.MOD_ID, name));
-	}
-
-	public boolean hasUpgradePath(){
-		return oldName.isPresent();
-	}
-
-	public static void init(){
-		Arrays.stream(ModTileEntities.values())
-			.forEach(modTileEntities -> GameRegistry.registerTileEntity(modTileEntities.tileClass, modTileEntities.name));
-	}
+	ALLOY_SMELTER(TileAlloySmelter.class,  "alloy_smelter", "TileAlloySmalterTR"),
+	ASSEMBLY_MACHINE(TileAssemblingMachine.class,  "assembly_machine", "TileAssemblyMachineTR"),
+	AUTO_CRAFTING_TABLE(TileAutoCraftingTable.class,  "auto_crafting_table", "TileAutoCraftingTableTR"),
+	CABLE(TileCable.class,  "cable", "TileCableTR"),
+	CHARGE_O_MAT(TileChargeOMat.class,  "charge_o_mat", "TileChargeOMatTR"),
+	CHEMICAL_REACTOR(TileChemicalReactor.class,  "chemical_reactor", "TileChemicalReactorTR"),
+	CHUNK_LOADER(TileChunkLoader.class,  "chunk_loader", "TileChunkLoaderTR"),
+	COMPRESSOR(TileCompressor.class,  "compressor", "TileCompressorTR"),
+	CREATIVE_QUANTUM_CHEST(TileCreativeQuantumChest.class,  "creative_quantum_chest"),
+	CREATIVE_QUANTUM_TANK(TileCreativeQuantumTank.class,  "creative_quantum_tank"),
+	CREATIVE_SOLAR_PANEL(TileCreativeSolarPanel.class,  "creative_solar_panel", "TileCreativeSolarPanelTR"),
+	DIESEL_GENERATOR(TileDieselGenerator.class,  "diesel_generator", "TileDieselGeneratorTR"),
+	DIGITAL_CHEST(TileDigitalChest.class,  "digital_chest", "TileDigitalChestTR"),
+	DISTILLATION_TOWER(TileDistillationTower.class,  "distillation_tower", "TileDistillationTowerTR"),
+	DRAGON_EGG_SYPHON(TileDragonEggSyphon.class,  "dragon_egg_syphon", "TileDragonEggSyphonTR"),
+	ELECTRIC_FURNACE(TileElectricFurnace.class,  "electric_furnace", "TileElectricFurnaceTR"),
+	EXTRACTOR(TileExtractor.class,  "extractor", "TileExtractorTR"),
+	FLUID_REPLICATOR(TileFluidReplicator.class,  "fluid_replicator", "TileFluidReplicatorTR"),
+	FUSION_CONTROL_COMPUTER(TileFusionControlComputer.class,  "fusion_control_computer", "TileFusionControlComputerTR"),
+	GAS_TURBINE(TileGasTurbine.class,  "gas_turbine", "TileGasTurbineTR"),
+	GRINDER(TileGrinder.class,  "grinder", "TileGrinderTR"),
+	HIGH_VOLTAGE_SU(TileHighVoltageSU.class,  "high_voltage_su", "TileHighVoltageSUTR"),
+	HV_TRANSFORMER(TileHVTransformer.class,  "hv_transformer", "TileHVTransformerTR"),
+	IMPLOSION_COMPRESSOR(TileImplosionCompressor.class,  "implosion_compressor", "TileImplosionCompressorTR"),
+	INDUSTRIAL_BLAST_FURNACE(TileIndustrialBlastFurnace.class,  "industrial_blast_furnace", "TileIndustrialBlastFurnaceTR"),
+	INDUSTRIAL_CENTRIFUGE(TileIndustrialCentrifuge.class,  "industrial_centrifuge", "TileIndustrialCentrifugeTR"),
+	INDUSTRIAL_ELECTROLYZER(TileIndustrialElectrolyzer.class,  "industrial_electrolyzer", "TileIndustrialElectrolyzerTR"),
+	INDUSTRIAL_GRINDER(TileIndustrialGrinder.class,  "industrial_grinder", "TileIndustrialGrinderTR"),
+	INDUSTRIAL_SAWMILL(TileIndustrialSawmill.class,  "industrial_sawmill", "TileIndustrialSawmillTR"),
+	INTERDIMENSIONAL_SU(TileInterdimensionalSU.class,  "interdimensional_su", "TileInterdimensionalSUTR"),
+	IRON_ALLOY_FURNACE(TileIronAlloyFurnace.class,  "iron_alloy_furnace", "TileIronAlloyFurnaceTR"),
+	IRON_FURNACE(TileIronFurnace.class,  "iron_furnace", "TileIronFurnaceTR"),
+	LAMP(TileLamp.class,  "lamp", "TileLampTR"),
+	LAPOTRONIC_SU(TileLapotronicSU.class,  "lapotronic_su", "TileLapotronicSUTR"),
+	LIGHTNING_ROD(TileLightningRod.class,  "lightning_rod", "TileLightningRodTR"),
+	LOW_VOLTAGE_SU(TileLowVoltageSU.class,  "low_voltage_su", "TileLowVoltageSUTR"),
+	LSU_STORAGE(TileLSUStorage.class,  "lsu_storage", "TileLSUStorageTR"),
+	LV_TRANSFORMER(TileLVTransformer.class,  "lv_transformer", "TileLVTransformerTR"),
+	MACHINE_BASE(TileLegacyMachineBase.class,  "machine_base", "TileMachineBaseTR"),
+	MACHINE_CASINGS(TileMachineCasing.class,  "machine_casing", "TileMachineCasingTR"),
+	MATTER_FABRICATOR(TileMatterFabricator.class,  "matter_fabricator", "TileMatterFabricatorTR"),
+	MEDIUM_VOLTAGE_SU(TileMediumVoltageSU.class,  "medium_voltage_su", "TileMediumVoltageSUTR"),
+	MV_TRANSFORMER(TileMVTransformer.class,  "mv_transformer", "TileMVTransformerTR"),
+	PLASMA_GENERATOR(TilePlasmaGenerator.class,  "plasma_generator", "TilePlasmalGeneratorTR"),
+	PLAYER_DETECTOR(TilePlayerDectector.class,  "player_detector", "TilePlayerDectectorTR"),
+	QUANTUM_CHEST(TileQuantumChest.class,  "quantum_chest", "TileQuantumChestTR"),
+	QUANTUM_TANK(TileQuantumTank.class,  "quantum_tank", "TileQuantumTankTR"),
+	RECYCLER(TileRecycler.class,  "recycler", "TileRecyclerTR"),
+	ROLLING_MACHINE(TileRollingMachine.class,  "rolling_machine", "TileRollingMachineTR"),
+	SCRAPBOXINATOR(TileScrapboxinator.class,  "scrapboxinator", "TileScrapboxinatorTR"),
+	SEMI_FLUID_GENERATOR(TileSemiFluidGenerator.class,  "semi_fluid_generator", "TileSemiFluidGeneratorTR"),
+	SOLAR_PANEL(TileSolarPanel.class,  "solar_panel", "TileSolarPanelTR"),
+	SOLID_FUEL_GENEREATOR(TileSolidFuelGenerator.class,  "solid_fuel_generator", "TileSolidFuelGeneratorTR"),
+	THERMAL_GEN(TileThermalGenerator.class,  "thermal_generator", "TileThermalGeneratorTR"),
+	VACUUM_FREEZER(TileVacuumFreezer.class,  "vacuum_freezer", "TileVacuumFreezerTR"),
+	WATER_MILL(TileWaterMill.class,  "water_mill", "TileWaterMillTR"),
+	WIND_MILL(TileWindMill.class,  "wind_mill", "TileWindMillTR");
 
 	public static Optional<ModTileEntities> getFromOldName(String name){
 		for(ModTileEntities entry : ModTileEntities.values()){
@@ -159,14 +126,12 @@ public enum ModTileEntities {
 		}
 		return Optional.empty();
 	}
-
+	public static void init(){
+		Arrays.stream(ModTileEntities.values())
+			.forEach(modTileEntities -> GameRegistry.registerTileEntity(modTileEntities.tileClass, modTileEntities.name));
+	}
 	public static void initDataFixer(ModFixs dataFixes){
 		dataFixes.registerFix(FixTypes.BLOCK_ENTITY, new IFixableData() {
-			@Override
-			public int getFixVersion() {
-				return 1;
-			}
-
 			@Override
 			public NBTTagCompound fixTagCompound(NBTTagCompound compound) {
 				ResourceLocation tileEntityLocation = new ResourceLocation(compound.getString("id"));
@@ -176,6 +141,41 @@ public enum ModTileEntities {
 				}
 				return compound;
 			}
+
+			@Override
+			public int getFixVersion() {
+				return 1;
+			}
 		});
+	}
+
+	public ResourceLocation name;
+
+	public Optional<String> oldName;
+
+	public Class<? extends TileEntity> tileClass;
+
+	ModTileEntities(Class<? extends TileEntity> tileClass, ResourceLocation name) {
+		this.tileClass = tileClass;
+		this.name = name;
+		this.oldName = Optional.empty();
+	}
+
+	ModTileEntities(Class<? extends TileEntity> tileClass, ResourceLocation name, String oldName) {
+		this.tileClass = tileClass;
+		this.name = name;
+		this.oldName = Optional.of(oldName);
+	}
+
+	ModTileEntities(Class<? extends TileEntity> tileClass, String name) {
+		this(tileClass,  new ResourceLocation(ModInfo.MOD_ID, name));
+	}
+
+	ModTileEntities(Class<? extends TileEntity> tileClass, String name, String oldName) {
+		this(tileClass, new ResourceLocation(ModInfo.MOD_ID, name), oldName);
+	}
+
+	public boolean hasUpgradePath(){
+		return oldName.isPresent();
 	}
 }
