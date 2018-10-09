af04149f1c56fdebb7050b55f8e2ee8a06f16f64
modmuss50 <modmuss50@gmail.com>
2018-10-09 17:08:40 +0100

    Added power to armor

--- src/main/java/techreborn/items/armor/modular/ModularArmorManager.java
+++ src/main/java/techreborn/items/armor/modular/ModularArmorManager.java
@@ -5,11 +5,14 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
+import net.minecraftforge.energy.CapabilityEnergy;
 import net.minecraftforge.energy.IEnergyStorage;
 import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandlerModifiable;
 import org.apache.commons.lang3.Validate;
+import reborncore.api.power.IEnergyItemInfo;
 import reborncore.common.items.InventoryItem;
+import reborncore.common.powerSystem.PoweredItemContainerProvider;
 import techreborn.api.armor.*;
 
 import javax.annotation.Nonnull;
@@ -19,16 +22,19 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 //Think of this class as the tile entity on the item. It handles sorting all the upgrades out, and the inventoryes, and the power
-public class ModularArmorManager implements ICapabilityProvider, IModularArmorManager {
+public class ModularArmorManager implements ICapabilityProvider, IModularArmorManager, IEnergyItemInfo {
 
 	ItemStack stack;
 	ArmorSlot armorSlot;
+
+	PoweredItemContainerProvider energyProvider;
 
 	public ModularArmorManager(ItemStack stack) {
 		Validate.isInstanceOf(ItemArmor.class, stack.getItem());
 		this.stack = stack;
 		ItemArmor itemArmor = (ItemArmor) stack.getItem();
 		this.armorSlot = ArmorSlot.fromEntityEquipmentSlot(itemArmor.armorType);
+		energyProvider = new PoweredItemContainerProvider(stack, this);
 	}
 
 	@Override
@@ -43,7 +49,7 @@ public class ModularArmorManager implements ICapabilityProvider, IModularArmorMa
 
 	@Override
 	public IEnergyStorage getEnergyStorage() {
-		return null; //TODO power
+		return energyProvider.getCapability(CapabilityEnergy.ENERGY, null);
 	}
 
 	@Override
@@ -70,7 +76,7 @@ public class ModularArmorManager implements ICapabilityProvider, IModularArmorMa
 			Capability<?> capability,
 		@Nullable
 			EnumFacing facing) {
-		return capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY || capability == CapabilityArmorUpgrade.ARMOR_MANAGER_CAPABILITY;
+		return capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY || capability == CapabilityArmorUpgrade.ARMOR_MANAGER_CAPABILITY || energyProvider.hasCapability(capability, facing);
 	}
 
 	@Nullable
@@ -86,6 +92,26 @@ public class ModularArmorManager implements ICapabilityProvider, IModularArmorMa
 		if(capability == CapabilityArmorUpgrade.ARMOR_MANAGER_CAPABILITY){
 			return CapabilityArmorUpgrade.ARMOR_MANAGER_CAPABILITY.cast(this);
 		}
-		return null;
+		return energyProvider.getCapability(capability, facing);
+	}
+
+	@Override
+	public double getMaxPower(ItemStack stack) {
+		return 40_000; //TODO changed based on the armor type and uprgades
+	}
+
+	@Override
+	public boolean canAcceptEnergy(ItemStack stack) {
+		return true;
+	}
+
+	@Override
+	public boolean canProvideEnergy(ItemStack stack) {
+		return false;
+	}
+
+	@Override
+	public double getMaxTransfer(ItemStack stack) {
+		return 512;
 	}
 }
