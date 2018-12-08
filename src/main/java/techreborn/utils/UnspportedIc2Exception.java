a04760bac19c48bf6a6306c5ad67792475c40cfa
modmuss50 <modmuss50@gmail.com>
2018-12-08 22:34:15 +0000

    Tweak incompatible message

--- src/main/java/techreborn/utils/UnspportedIc2Exception.java
+++ src/main/java/techreborn/utils/UnspportedIc2Exception.java
@@ -4,7 +4,6 @@ import net.minecraft.client.gui.FontRenderer;
 import net.minecraft.client.gui.GuiErrorScreen;
 import net.minecraftforge.fml.client.CustomModLoadingErrorDisplayException;
 import net.minecraftforge.fml.client.IDisplayableError;
-import net.minecraftforge.fml.common.Loader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -27,11 +26,9 @@ public class UnspportedIc2Exception extends CustomModLoadingErrorDisplayExceptio
 	@Override
 	public void drawScreen(GuiErrorScreen errorScreen, FontRenderer fontRenderer, int mouseRelX, int mouseRelY, float tickTime) {
 		errorScreen.drawCenteredString(fontRenderer, "IC2 is installed, but no supported helper was setup", errorScreen.width / 2, 10, Color.RED.getRGB());
-		errorScreen.drawCenteredString(fontRenderer, "This can happen due to unoffical versions of ic2.", errorScreen.width / 2, 50, 0xEEEEEE);
-		errorScreen.drawCenteredString(fontRenderer, "If you are using offical ic2 please open an issue @ https://github.com/TechReborn/TechReborn/issues", errorScreen.width / 2, 60, 0xEEEEEE);
+		errorScreen.drawCenteredString(fontRenderer, "This can happen due to using forked versions of ic2.", errorScreen.width / 2, 50, 0xEEEEEE);
+		errorScreen.drawCenteredString(fontRenderer, "If you are using offical ic2 please open an issue @https://github.com/TechReborn/TechReborn/issues", errorScreen.width / 2, 60, 0xEEEEEE);
 
-		if(Loader.isModLoaded("ic2-classic-spmod")){
-			errorScreen.drawCenteredString(fontRenderer, "If you are using IC2 Classic please contact the mod author and ask for compatbility to be fixed", errorScreen.width / 2, 100, Color.GREEN.getRGB());
-		}
+		errorScreen.drawCenteredString(fontRenderer, "If you are using a fork of IC2 please contact the developer and ask for TechReborn support", errorScreen.width / 2, 100, Color.GREEN.getRGB());
 	}
 }
