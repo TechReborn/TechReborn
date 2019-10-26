4564be7b490c5206676c83cabd426825f786aac0
modmuss50 <modmuss50@gmail.com>
2019-10-26 21:56:07 +0100

    Fix #1869

--- src/main/java/techreborn/blockentity/ChargeOMatBlockEntity.java
+++ src/main/java/techreborn/blockentity/ChargeOMatBlockEntity.java
@@ -36,8 +36,8 @@ import reborncore.common.powerSystem.PowerAcceptorBlockEntity;
 import reborncore.common.util.RebornInventory;
 import team.reborn.energy.Energy;
 import techreborn.config.TechRebornConfig;
-import techreborn.init.TRContent;
 import techreborn.init.TRBlockEntities;
+import techreborn.init.TRContent;
 
 public class ChargeOMatBlockEntity extends PowerAcceptorBlockEntity
 	implements IToolDrop, InventoryProvider, IContainerProvider {
@@ -82,7 +82,7 @@ public class ChargeOMatBlockEntity extends PowerAcceptorBlockEntity
 
 	@Override
 	public boolean canProvideEnergy(final Direction direction) {
-		return false;
+		return direction == null;
 	}
 
 	@Override
