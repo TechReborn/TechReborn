7b99e2e9b7851b55fb8a7676e045673245729617
modmuss50 <modmuss50@gmail.com>
2020-01-03 22:46:07 +0000

    License updates + fixes

--- src/main/java/techreborn/client/gui/GuiRollingMachine.java
+++ src/main/java/techreborn/client/gui/GuiRollingMachine.java
@@ -1,7 +1,7 @@
 /*
  * This file is part of TechReborn, licensed under the MIT License (MIT).
  *
- * Copyright (c) 2018 TechReborn
+ * Copyright (c) 2020 TechReborn
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
