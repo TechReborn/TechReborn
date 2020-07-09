a36a0e6f4ad19eb5746c3ebfda9b4db9bbcf7674
Justin Vitale <4002351+justinvvitale@users.noreply.github.com>
2020-07-09 22:55:00 +1000

    Check multiblock before crafting vacuum freezer

--- src/main/java/techreborn/blockentity/machine/multiblock/VacuumFreezerBlockEntity.java
+++ src/main/java/techreborn/blockentity/machine/multiblock/VacuumFreezerBlockEntity.java
@@ -25,12 +25,14 @@
 package techreborn.blockentity.machine.multiblock;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.Direction;
 import reborncore.client.screen.BuiltScreenHandlerProvider;
 import reborncore.client.screen.builder.BuiltScreenHandler;
 import reborncore.client.screen.builder.ScreenHandlerBuilder;
 import reborncore.common.blockentity.MultiblockWriter;
+import reborncore.common.crafting.RebornRecipe;
 import reborncore.common.recipes.RecipeCrafter;
 import reborncore.common.util.RebornInventory;
 import techreborn.blockentity.machine.GenericMachineBlockEntity;
@@ -66,5 +68,14 @@ public class VacuumFreezerBlockEntity extends GenericMachineBlockEntity implemen
 		return new ScreenHandlerBuilder("vacuumfreezer").player(player.inventory).inventory().hotbar().addInventory()
 				.blockEntity(this).slot(0, 55, 45).outputSlot(1, 101, 45).energySlot(2, 8, 72).syncEnergyValue()
 				.syncCrafterValue().addInventory().create(this, syncID);
+	}
+
+	@Override
+	public boolean canCraft(RebornRecipe rebornRecipe) {
+		if (!this.isMultiblockValid()) {
+			return false;
+		}
+
+		return super.canCraft(rebornRecipe);
 	}
 }
