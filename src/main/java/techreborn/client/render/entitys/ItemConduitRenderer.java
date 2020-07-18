9e0d6a33f197983ddf6ef0c866832d517e6f1545
Justin Vitale <4002351+justinvvitale@users.noreply.github.com>
2020-07-18 15:37:45 +1000

    Conduit modules working fairly decent

--- src/main/java/techreborn/client/render/entitys/ItemConduitRenderer.java
+++ src/main/java/techreborn/client/render/entitys/ItemConduitRenderer.java
@@ -25,7 +25,11 @@
 package techreborn.client.render.entitys;
 
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.model.Model;
+import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.render.OverlayTexture;
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.VertexConsumer;
 import net.minecraft.client.render.VertexConsumerProvider;
 import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
 import net.minecraft.client.render.block.entity.BlockEntityRenderer;
@@ -35,6 +39,7 @@ import net.minecraft.client.util.math.Vector3f;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.math.Quaternion;
 import reborncore.api.systems.conduit.functionalfaces.ConduitFunction;
@@ -46,6 +51,23 @@ import techreborn.blockentity.conduit.item.ItemConduitBlockEntity;
 import java.util.Map;
 
 public class ItemConduitRenderer extends BlockEntityRenderer<ItemConduitBlockEntity> {
+	private static final ExportModel EXPORT_MODEL = new ExportModel();
+	public static final Identifier EXPORT_TEXTURE = new Identifier("techreborn:textures/block/conduit/exporter.png");
+
+	private static final ImportModel IMPORT_MODEL = new ImportModel();
+	public static final Identifier IMPORT_TEXTURE = new Identifier("techreborn:textures/block/conduit/importer.png");
+
+	private static final OneWayModel ONE_WAY_MODEL = new OneWayModel();
+	public static final Identifier ONE_WAY_TEXTURE = new Identifier("techreborn:textures/block/conduit/one-way.png");
+
+	private static final BlockingModel BLOCKING_MODEL = new BlockingModel();
+	public static final Identifier BLOCKING_TEXTURE = new Identifier("techreborn:textures/block/conduit/blocking.png");
+
+	private static final RestrictiveModel RESTRICTIVE_MODEL = new RestrictiveModel();
+	public static final Identifier RESTRICTIVE_TEXTURE_ALLOWING = new Identifier("techreborn:textures/block/conduit/restrictive_allowing.png");
+	public static final Identifier RESTRICTIVE_TEXTURE_BLOCKING = new Identifier("techreborn:textures/block/conduit/restrictive_blocking.png");
+
+
 	public ItemConduitRenderer(BlockEntityRenderDispatcher dispatcher) {
 		super(dispatcher);
 	}
@@ -56,12 +78,12 @@ public class ItemConduitRenderer extends BlockEntityRenderer<ItemConduitBlockEnt
 
 //		int light = WorldRenderer.getLightmapCoordinates(conduit.getWorld(), conduit.getPos());
 
-		renderIOFaces(conduit.getFunctionalFaces(), matrices, vertexConsumers, light);
+		renderIOFaces(conduit.getFunctionalFaces(), matrices, vertexConsumers, light, overlay);
 		renderItemMoving(conduit.getStored(), matrices, vertexConsumers, light);
 
 	}
 
-	private void renderIOFaces(FunctionalFaceStorage<ConduitFunctionalFace> functionalFaceStorage, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light){
+	private void renderIOFaces(FunctionalFaceStorage<ConduitFunctionalFace> functionalFaceStorage, MatrixStack matrices, VertexConsumerProvider vertexConsumers, int light, int overlay){
 		if (functionalFaceStorage.isEmpty()) {
 			return;
 		}
@@ -72,40 +94,42 @@ public class ItemConduitRenderer extends BlockEntityRenderer<ItemConduitBlockEnt
 
 			matrices.push();
 
-			matrices.translate(0.5, 0.5, 0.5); // Translate center
+			matrices.translate(0.5f, 0.5f, 0.5f); // Translate center
 
-			matrices.scale(0.45f, 0.45f, 0.45f); // TEMPscale
-
-			// Rotating
+//			// Rotating
 			if(facing.getHorizontal() == -1){
+				// UP/DOWN
+
 				int offSet = facing.getOffsetY();
 				matrices.translate(0, offSet * 0.5,0);
 				matrices.multiply(facing.getRotationQuaternion());
 			}else{
+				// NORTH, SOUTH, WEST, EAST
 				matrices.multiply(Vector3f.POSITIVE_Y.getDegreesQuaternion(-facing.rotateYCounterclockwise().asRotation() + 90)); // Rotate depending on face
-				matrices.translate(0, 0, -0.5);
 
+				matrices.translate(0, 0, -0.5);
 				Quaternion faceQuaternion = facing.getRotationQuaternion();
 				matrices.multiply(new Quaternion(-faceQuaternion.getX(), faceQuaternion.getY(), faceQuaternion.getZ(), faceQuaternion.getW()));
 			}
 
+			// Janky translating due to unfound issues
+			matrices.translate(0,0.031f, 0);
+
 			 // Model rendering
-			matrices.translate(0,0.25f,0f);
 			switch (mode){
 				case EXPORT:
-					MinecraftClient.getInstance().getItemRenderer().renderItem(Items.BEACON.getStackForRender(), ModelTransformation.Mode.FIXED, light, OverlayTexture.DEFAULT_UV, matrices, vertexConsumers);
+					EXPORT_MODEL.render(matrices, vertexConsumers.getBuffer(RenderLayer.getEntitySolid(EXPORT_TEXTURE)), light, overlay, 1F, 1F, 1F, 1F);
 					break;
 				case IMPORT:
-					matrices.scale(1f,0.7f,4f);
-					MinecraftClient.getInstance().getItemRenderer().renderItem(Items.HOPPER.getStackForRender(), ModelTransformation.Mode.FIXED, light, OverlayTexture.DEFAULT_UV, matrices, vertexConsumers);
+					IMPORT_MODEL.render(matrices, vertexConsumers.getBuffer(RenderLayer.getEntitySolid(IMPORT_TEXTURE)), light, overlay, 1F, 1F, 1F, 1F);
 					break;
 				case ONE_WAY:
-					MinecraftClient.getInstance().getItemRenderer().renderItem(Items.PISTON.getStackForRender(), ModelTransformation.Mode.FIXED, light, OverlayTexture.DEFAULT_UV, matrices, vertexConsumers);
+					matrices.scale(1.15f, 1.15f,  1.15f);
+					ONE_WAY_MODEL.render(matrices, vertexConsumers.getBuffer(RenderLayer.getEntitySolid(ONE_WAY_TEXTURE)), light, overlay, 1F, 1F, 1F, 1F);
 					break;
 				case BLOCK:
-					matrices.scale(1,0.2f,1f);
-					matrices.translate(0,-1,0);
-					MinecraftClient.getInstance().getItemRenderer().renderItem(Items.BEDROCK.getStackForRender(), ModelTransformation.Mode.FIXED, light, OverlayTexture.DEFAULT_UV, matrices, vertexConsumers);
+					matrices.translate(0, -0.0622, 0); // Put it 1 block inside the block
+					new BlockingModel().render(matrices, vertexConsumers.getBuffer(RenderLayer.getEntitySolid(BLOCKING_TEXTURE)), light, overlay, 1F, 1F, 1F, 1F);
 					break;
 			}
 
@@ -138,5 +162,113 @@ public class ItemConduitRenderer extends BlockEntityRenderer<ItemConduitBlockEnt
 		MinecraftClient.getInstance().getItemRenderer().renderItem(itemStack, ModelTransformation.Mode.FIXED, light, OverlayTexture.DEFAULT_UV, matrices, vertexConsumers);
 		matrices.pop();
 
+	}
+
+
+	// Sided function models
+
+	private static class ExportModel extends Model {
+
+		private final ModelPart base;
+
+		public ExportModel() {
+			super(RenderLayer::getEntityCutoutNoCull);
+			textureWidth = 48;
+			textureHeight = 32;
+
+			base = new ModelPart(this);
+
+			base.setTextureOffset(0, 0).addCuboid(-4.5F, -4.0F, -4.5F, 9.0F, 2.0F, 9.0F, 0.0F, false);
+			base.setTextureOffset(0, 11).addCuboid(-3.5F, -2.0F, -3.5F, 7.0F, 2.0F, 7.0F, 0.0F, false);
+		}
+
+		@Override
+		public void render(MatrixStack matrixStack, VertexConsumer vertexConsumer, int light, int overlay, float red, float green, float blue, float alpha) {
+			base.render(matrixStack, vertexConsumer, light, overlay);
+		}
+	}
+
+	private static class ImportModel extends Model {
+
+		private final ModelPart base;
+
+		public ImportModel() {
+			super(RenderLayer::getEntityCutoutNoCull);
+			textureWidth = 48;
+			textureHeight = 32;
+
+			base = new ModelPart(this);
+
+			base.setTextureOffset(0, 9).addCuboid(-4.5F, -2.0F, -4.5F, 9.0F, 2.0F, 9.0F, 0.0F, false);
+			base.setTextureOffset(0, 0).addCuboid(-3.5F, -4.0F, -3.5F, 7.0F, 2.0F, 7.0F, 0.0F, false);
+		}
+
+		@Override
+		public void render(MatrixStack matrixStack, VertexConsumer vertexConsumer, int light, int overlay, float red, float green, float blue, float alpha) {
+			base.render(matrixStack, vertexConsumer, light, overlay);
+		}
+	}
+
+	private static class OneWayModel extends Model {
+
+		private final ModelPart base;
+
+		public OneWayModel() {
+			super(RenderLayer::getEntityCutoutNoCull);
+			textureWidth = 32;
+			textureHeight = 32;
+
+			base = new ModelPart(this);
+
+			base.setTextureOffset(0, 0).addCuboid(-3.5f, -4F, -3.5f, 7.0F, 4.0F, 7.0F, 0.0F, false);
+		}
+
+		@Override
+		public void render(MatrixStack matrixStack, VertexConsumer vertexConsumer, int light, int overlay, float red, float green, float blue, float alpha) {
+			base.render(matrixStack, vertexConsumer, light, overlay);
+		}
+	}
+
+	private static class BlockingModel extends Model {
+
+		private final ModelPart base;
+
+		public BlockingModel() {
+			super(RenderLayer::getEntityCutoutNoCull);
+			textureWidth = 48;
+			textureHeight = 32;
+
+			base = new ModelPart(this);
+
+			base.setTextureOffset(0, 4).addCuboid(-4.5F, -2.0F, -4.5F, 9.0F, 1.0F, 9.0F, 0.0F, false);
+			base.setTextureOffset(0, 0).addCuboid(-3.5F, -4.0F, 1.5F, 7.0F, 2.0F, 2.0F, 0.0F, false);
+			base.setTextureOffset(0, 0).addCuboid(-3.5F, -4.0F, -3.5F, 7.0F, 2.0F, 2.0F, 0.0F, false);
+		}
+
+		@Override
+		public void render(MatrixStack matrixStack, VertexConsumer vertexConsumer, int light, int overlay, float red, float green, float blue, float alpha) {
+			base.render(matrixStack, vertexConsumer, light, overlay);
+		}
+	}
+
+	private static class RestrictiveModel extends Model {
+
+		private final ModelPart base;
+
+		public RestrictiveModel() {
+			super(RenderLayer::getEntityCutoutNoCull);
+			textureWidth = 48;
+			textureHeight = 32;
+
+			base = new ModelPart(this);
+
+			base.setPivot(0, 0, 0);
+			base.setTextureOffset(0, 0).addCuboid(-7.5F, 0.0F, 4.4F, 7.0F, 4.0F, 7.0F, 0.0F, false);
+		}
+
+		@Override
+		public void render(MatrixStack matrixStack, VertexConsumer vertexConsumer, int light, int overlay, float red, float green, float blue, float alpha) {
+			base.render(matrixStack, vertexConsumer, light, overlay);
+		}
 	}
 }
---
+++ src/main/resources/assets/techreborn/TODO/export.java
@@ -0,0 +1,35 @@
+// Made with Blockbench 3.5.4
+// Exported for Minecraft version 1.15
+// Paste this class into your mod and generate all required imports
+
+
+public class custom_model extends EntityModel<Entity> {
+	private final ModelRenderer bone;
+
+	public custom_model() {
+		textureWidth = 48;
+		textureHeight = 32;
+
+		bone = new ModelRenderer(this);
+		bone.setRotationPoint(4.0F, 24.0F, -8.0F);
+		setRotationAngle(bone, 1.5708F, 0.0F, 0.0F);
+		bone.setTextureOffset(0, 0).addBox(-8.5F, 0.0F, 3.5F, 9.0F, 2.0F, 9.0F, 0.0F, false);
+		bone.setTextureOffset(0, 11).addBox(-7.5F, 2.0F, 4.5F, 7.0F, 2.0F, 7.0F, 0.0F, false);
+	}
+
+	@Override
+	public void setRotationAngles(Entity entity, float limbSwing, float limbSwingAmount, float ageInTicks, float netHeadYaw, float headPitch){
+		//previously the render function, render code was moved to a method below
+	}
+
+	@Override
+	public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
+		bone.render(matrixStack, buffer, packedLight, packedOverlay);
+	}
+
+	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+		modelRenderer.rotateAngleX = x;
+		modelRenderer.rotateAngleY = y;
+		modelRenderer.rotateAngleZ = z;
+	}
+}
