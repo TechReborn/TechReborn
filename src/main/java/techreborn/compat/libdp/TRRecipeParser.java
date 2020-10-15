972f12cff87aeb8740797fe02a733ff7c9a4d564
Yoghurt4C <yoghurt4242@gmail.com>
2020-10-15 12:23:04 +0300

    remove certain things and added stuff

---
+++ src/main/java/techreborn/compat/libdp/TRRecipeParser.java
@@ -0,0 +1,127 @@
+/*
+ * This file is part of TechReborn, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2020 TechReborn
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package techreborn.compat.libdp;
+
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import io.github.cottonmc.libdp.api.DPSyntaxError;
+import io.github.cottonmc.libdp.api.driver.util.DriverUtils;
+import net.minecraft.fluid.Fluid;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.StringNbtReader;
+import net.minecraft.recipe.Ingredient;
+import net.minecraft.tag.ServerTagManagerHolder;
+import net.minecraft.tag.Tag;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import reborncore.common.crafting.ingredient.*;
+import reborncore.common.fluid.FluidValue;
+import reborncore.common.fluid.container.FluidInstance;
+
+import java.util.Collections;
+import java.util.Optional;
+
+public class TRRecipeParser {
+
+	public static RebornIngredient processIngredient(Object input) throws DPSyntaxError {
+		if (input instanceof RebornIngredient) {
+			return (RebornIngredient) input;
+		} else if (input instanceof Ingredient) {
+			return new WrappedIngredient((Ingredient) input);
+		} else if (input instanceof String) {
+			String in = (String) input;
+			Optional<Integer> amount = Optional.empty();
+			int atIndex = in.lastIndexOf('@');
+			if (atIndex != -1 && in.lastIndexOf('}') < atIndex) {
+				String count = in.substring(atIndex + 1);
+				amount = Optional.of(Integer.parseInt(count));
+				in = in.substring(0, atIndex);
+			}
+			if (in.indexOf('~') == 0) {
+				//fluids
+				Identifier id = new Identifier(in.substring(1));
+				Fluid fluid = Registry.FLUID.get(id);
+				if (fluid == Fluids.EMPTY) throw new DPSyntaxError("Failed to get fluid for input: " + in);
+				return new FluidIngredient(fluid, Optional.empty(), amount);
+			} else if (in.indexOf('#') == 0) {
+				Identifier id = new Identifier(in.substring(1));
+				Tag<Item> itemTag = ServerTagManagerHolder.getTagManager().getItems().getTag(id);
+				if (itemTag == null) throw new DPSyntaxError("Failed to get item tag for input: " + in);
+				return new TagIngredient(id, itemTag, amount);
+			} else {
+				ItemStack stack;
+				Optional<CompoundTag> tag = Optional.empty();
+				boolean requireEmpty = false;
+				if (in.contains("->")) {
+					ItemStack readStack = DriverUtils.INSTANCE.makeSpecialStack(in);
+					if (readStack.isEmpty())
+						throw new DPSyntaxError("Failed to get special stack for input: " + in);
+					if (readStack.hasTag()) {
+						tag = Optional.of(readStack.getTag());
+					} else {
+						requireEmpty = true;
+					}
+					stack = readStack;
+				} else {
+					int nbtIndex = in.indexOf('{');
+					if (nbtIndex != -1) {
+						try {
+							String nbt = in.substring(nbtIndex);
+							in = in.substring(0, nbtIndex);
+							StringNbtReader reader = new StringNbtReader(new StringReader(nbt));
+							CompoundTag parsedTag = reader.parseCompoundTag();
+							if (parsedTag.isEmpty()) requireEmpty = true;
+							else tag = Optional.of(reader.parseCompoundTag());
+						} catch (CommandSyntaxException e) {
+							throw new DPSyntaxError(e.getMessage());
+						}
+					}
+					Identifier id = new Identifier(in);
+					Item item = Registry.ITEM.get(id);
+					if (item == Items.AIR) throw new DPSyntaxError("Failed to get item for input: " + in);
+					stack = new ItemStack(item);
+				}
+				return new StackIngredient(Collections.singletonList(stack), amount, tag, requireEmpty);
+			}
+		} else
+			throw new DPSyntaxError("Illegal object passed to TechReborn ingredient parser of type " + input.getClass().getName());
+	}
+
+	public static FluidInstance parseFluid(String fluid) {
+		int amount = 1000;
+		int amtIndex = fluid.indexOf('@');
+		if (amtIndex != -1) {
+			String amtStr = fluid.substring(amtIndex + 1);
+			fluid = fluid.substring(0, amtIndex);
+			amount = Integer.parseInt(amtStr);
+		}
+		Identifier id = new Identifier(fluid);
+		return new FluidInstance(Registry.FLUID.get(id), FluidValue.fromRaw(amount));
+	}
+}
---
+++ src/main/resources/data/techreborn/diskettes/tweaker_test.js
@@ -0,0 +1,92 @@
+//Warn that the sample script is running
+log.warn("WARNING! The TechReborn tweaker sample script is running!");
+log.warn("If you're seeing this and aren't in dev mode, please report it!");
+
+var TRTweaker = diskette.require("techreborn.TRTweaker");
+var DriverUtils = diskette.require("libdp.util.DriverUtils");
+
+//Add an alloy smelter recipe that takes 8 coal and 2 obsidian -> 2 diamonds
+TRTweaker.addAlloySmelter(["minecraft:coal@8", "minecraft:obsidian@2"], ["minecraft:diamond@2"], 6, 200);
+
+//Add an assembling machine recipe that takes 3 diamonds and 2 sticks -> 1 diamond pickaxe
+TRTweaker.addAssemblingMachine(["minecraft:diamond@3", "minecraft:stick@2"], ["minecraft:diamond_pickaxe"], 20, 200);
+
+//Add a blast furnace recipe that takes 64 of any item in the minecraft:coals tag -> 8 diamonds
+TRTweaker.addBlastFurnace(["#minecraft:coals@64"], ["minecraft:diamond@8"], 128, 200, 1300);
+
+//Add a centrifuge recipe that takes any potion -> a water bottle
+TRTweaker.addCentrifuge(["minecraft:potion"], [DriverUtils.makeSpecialStack("minecraft:potion->minecraft:water")], 5, 1000);
+
+//Add a chemical reactor recipe that takes a water bottle and a can of methane -> a potion of fire resistance
+//DISCLAIMER: do not try this at home
+TRTweaker.addChemicalReactor(["minecraft:potion->minecraft:water", TRTweaker.createFluidIngredient("techreborn:methane", ["techreborn:cell"], -1)], [DriverUtils.makeSpecialStack("minecraft:potion->minecraft:fire_resistance")], 30, 800);
+
+//Add a compressor recipe that takes 9 coal blocks -> 3 pieces of obsidian
+TRTweaker.addCompressor(["minecraft:coal_ore@9"], ["minecraft:coal_block@3"], 10, 300);
+
+//Add a distillation tower recipe that takes a potion of regneration -> a strong potion of regeneration
+TRTweaker.addDistillationTower(["minecraft:potion->minecraft:regeneration"], [DriverUtils.makeSpecialStack("minecraft:potion->minecraft:strong_regeneration")], 20, 400);
+
+//Add an extractor recipe that takes 4 coal -> 1 gunpowder
+TRTweaker.addExtractor(["minecraft:coal@4"], ["minecraft:gunpowder"], 10, 300);
+
+//Add a grinder recipe of 1 sugar cane -> 3 sugar
+TRTweaker.addGrinder(["minecraft:sugar_cane"], ["minecraft:sugar@3"], 4, 270);
+
+//Add an implosion compressor recipe of 32 coal and 16 flint -> 16 diamonds
+TRTweaker.addImplosionCompressor(["minecraft:coal@32", "minecraft:flint@16"], ["minecraft:diamond@16"], 30, 2000);
+
+//Add an industrial electrolyzer recipe of 1 skeleton skull -> 1 wither skeleton skull
+TRTweaker.addIndustrialElectrolyzer(["minecraft:skeleton_skull"], ["minecraft:wither_skeleton_skull"], 50, 1400);
+
+//Add an industrial electrolyzer recipe of 1 sea lantern -> 5 prismarine crystals and 4 prismarine shards
+TRTweaker.addIndustrialGrinder(["minecraft:sea_lantern"], ["minecraft:prismarine_crystals@5", "minecraft:prismarine_shard@4"], 64, 100);
+
+//Add an industrial electrolyzer recipe of 1 sea lantern and 1 bucket of electrolyzed water -> 9 prismarine crystals
+TRTweaker.addIndustrialGrinder(["minecraft:sea_lantern"], ["minecraft:prismarine_crystals@9"], 64, 100, "techreborn:electrolyzed_water@1000");
+
+//Add an industrial sawmill recipe of 3 sugar cane -> 18 paper
+TRTweaker.addIndustrialSawmill(["minecraft:sugar_cane@3"], ["minecraft:paper@18"], 40, 200);
+
+//Add an industrial sawmill recipe of 1 heart of the sea and 1/2 bucket of water -> 16 nautilus shells
+TRTweaker.addIndustrialSawmill(["minecraft:heart_of_the_sea"], ["minecraft:nautilus_shell@16"], 40, 200, "minecraft:water@500");
+
+//Add a recycler recipe of 1 water bucket -> 1 empty bucket
+TRTweaker.addRecycler(["minecraft:water_bucket"], ["minecraft:bucket"], 5, 40);
+
+//Add a scrapbox recipe of 1 scrap box -> 1 shulker box
+TRTweaker.addScrapbox("minecraft:shulker_box", 10, 20);
+
+//Add a scrapbox recipe of 1 cell of water -> 1 blue ice
+TRTweaker.addVacuumFreezer([TRTweaker.createFluidIngredient("minecraft:water", ["techreborn:cell"], -1)], ["minecraft:blue_ice"], 60, 440);
+
+//Add a fluid replicator recipe of 2 uu matter and 1 bucket of wulframium -> 2 buckets of wolframium
+TRTweaker.addFluidReplicator(["techreborn:uu_matter@2"], 40, 100, "techreborn:wolframium@1000");
+
+//Add a fusion reactor recipe of 3 wither skeleton skulls and 4 soul sand -> 1 nether star
+TRTweaker.addFusionReactor(["minecraft:wither_skeleton_skull@3", "minecraft:soul_sand@4"], ["minecraft:nether_star"], -2048, 1024, 90000, 1);
+
+//Add a rolling machine recipe for 5 popped chorus fruit in a helmet shape -> a shulker shell
+var chorus = "minecraft:popped_chorus_fruit";
+TRTweaker.addRollingMachine([[chorus, chorus, chorus], [chorus, "", chorus]], "minecraft:shulker_shell");
+
+//Create a pattern/dictionary set for a shaped recipe
+var pattern = [ '/ /',
+                '/_/',
+                '/ /'];
+var dict = {
+    "/": "minecraft:stick",
+    "_": "#minecraft:wooden_slabs"
+};
+
+//Add a rolling machine recipe for sticks on the sides and any wooden slab in the middle -> six ladders
+TRTweaker.addDictRollingMachine(pattern, dict, "minecraft:ladder@12");
+
+//Add a solid canning machine recipe for 1 empty cell and 1 blue ice -> a cell full of water
+TRTweaker.addSolidCanningMachine(["techreborn:cell{}", "minecraft:blue_ice"], [DriverUtils.makeSpecialStack("techreborn:cell->minecraft:water")], 1, 100);
+
+//Add a wire mill recipe for 1 woll -> 4 string
+TRTweaker.addWireMill(["#minecraft:wool"], ["minecraft:string@4"], 2, 200);
+
+//Add a plasma fluid generator recipe for 1 mB of wolframium -> 300 EU
+TRTweaker.addFluidGenerator("plasma", "techreborn:wolframium", 300);
