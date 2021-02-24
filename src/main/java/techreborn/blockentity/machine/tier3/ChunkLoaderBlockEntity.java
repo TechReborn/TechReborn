9b47348fd3de44696a0d6d6bacb99e8e43b0f9c8
drcrazy <hcb@wowhcb.ru>
2021-02-24 12:29:15 +0300

    Fix crash with chunkloader placed by command. Closes #2349

--- src/main/java/techreborn/blockentity/machine/tier3/ChunkLoaderBlockEntity.java
+++ src/main/java/techreborn/blockentity/machine/tier3/ChunkLoaderBlockEntity.java
@@ -77,11 +77,6 @@ public class ChunkLoaderBlockEntity extends MachineBaseBlockEntity implements IT
 		}
 	}
 
-	@Override
-	public ItemStack getToolDrop(final PlayerEntity entityPlayer) {
-		return TRContent.Machine.CHUNK_LOADER.getStack();
-	}
-
 	private void reload() {
 		unloadAll();
 		load();
@@ -102,7 +97,16 @@ public class ChunkLoaderBlockEntity extends MachineBaseBlockEntity implements IT
 		}
 	}
 
+	private void unloadAll() {
+		ChunkLoaderManager manager = ChunkLoaderManager.get(world);
+		manager.unloadChunkLoader(world, getPos());
+	}
 
+	public ChunkPos getChunkPos() {
+		return new ChunkPos(getPos());
+	}
+
+	// MachineBaseBlockEntity
 	@Override
 	public void onBreak(World world, PlayerEntity playerEntity, BlockPos blockPos, BlockState blockState) {
 		if (world.isClient) {
@@ -119,20 +123,13 @@ public class ChunkLoaderBlockEntity extends MachineBaseBlockEntity implements IT
 		reload();
 	}
 
-	private void unloadAll() {
-		ChunkLoaderManager manager = ChunkLoaderManager.get(world);
-		manager.unloadChunkLoader(world, getPos());
-	}
-
-	public ChunkPos getChunkPos() {
-		return new ChunkPos(getPos());
-	}
-
 	@Override
 	public CompoundTag toTag(CompoundTag tagCompound) {
 		super.toTag(tagCompound);
 		tagCompound.putInt("radius", radius);
-		tagCompound.putString("ownerUdid", ownerUdid);
+		if (ownerUdid != null && !ownerUdid.isEmpty()){
+			tagCompound.putString("ownerUdid", ownerUdid);
+		}
 		inventory.write(tagCompound);
 		return tagCompound;
 	}
@@ -148,9 +145,23 @@ public class ChunkLoaderBlockEntity extends MachineBaseBlockEntity implements IT
 		inventory.read(nbttagcompound);
 	}
 
+	// IToolDrop
+	@Override
+	public ItemStack getToolDrop(final PlayerEntity entityPlayer) {
+		return TRContent.Machine.CHUNK_LOADER.getStack();
+	}
+
+	// InventoryProvider
 	@Override
 	public RebornInventory<ChunkLoaderBlockEntity> getInventory() {
 		return this.inventory;
+	}
+
+	// BuiltScreenHandlerProvider
+	@Override
+	public BuiltScreenHandler createScreenHandler(int syncID, PlayerEntity player) {
+		return new ScreenHandlerBuilder("chunkloader").player(player.inventory).inventory().hotbar().addInventory()
+				.blockEntity(this).sync(this::getRadius, this::setRadius).addInventory().create(this, syncID);
 	}
 
 	public int getRadius() {
@@ -161,10 +172,5 @@ public class ChunkLoaderBlockEntity extends MachineBaseBlockEntity implements IT
 		this.radius = radius;
 	}
 
-	@Override
-	public BuiltScreenHandler createScreenHandler(int syncID, PlayerEntity player) {
-		return new ScreenHandlerBuilder("chunkloader").player(player.inventory).inventory().hotbar().addInventory()
-				.blockEntity(this).sync(this::getRadius, this::setRadius).addInventory().create(this, syncID);
-	}
 
 }
