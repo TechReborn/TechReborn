d5b35148af43ff4592a889a9734c1b28a16fba5d
Dave (Dizzy) Smith <dizzyd@dizzyd.com>
2021-09-20 13:54:49 -0600

    Add a flat X/Z cylinder generator for multi-blocks

--- RebornCore/src/main/java/reborncore/common/blockentity/MultiblockWriter.java
+++ RebornCore/src/main/java/reborncore/common/blockentity/MultiblockWriter.java
@@ -151,6 +151,24 @@ public interface MultiblockWriter {
 		return this;
 	}
 
+	default MultiblockWriter cylinder(int radius, BlockState border, BlockState fill) {
+		double rSquared = Math.pow(radius, 2);
+		for (int x = -radius; x <= radius; x++) {
+			for (int z = -radius; z <= radius; z++) {
+				double dist = (Math.pow(x, 2) + Math.pow(z, 2));
+				double diff = Math.abs(rSquared - dist);
+				if (diff < radius) {
+					add(x, 0, z, border);
+				} else if (dist < rSquared) {
+					add(x, 0, z, fill);
+				}
+			}
+		}
+		return this;
+	}
+
+
+
 	default MultiblockWriter ringWithAir(Direction.Axis through, int x, int y, int z, BiPredicate<BlockView, BlockPos> predicate, BlockState state) {
 		return ring(through, x, y, z, predicate, state, (view, pos) -> view.getBlockState(pos).getBlock() == Blocks.AIR, Blocks.AIR.getDefaultState());
 	}
