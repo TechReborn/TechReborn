89b47b33fd1d34783f210c3b1f9076e871e98825
modmuss50 <modmuss50@gmail.com>
2021-12-03 15:58:43 +0000

    Add base datagen sourceset

---
+++ src/datagen/groovy/techreborn/datagen/TechRebornDataGen.groovy
@@ -0,0 +1,11 @@
+package techreborn.datagen
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+class TechRebornDataGen implements DataGeneratorEntrypoint {
+    @Override
+    void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        // TODO generate data here
+    }
+}
