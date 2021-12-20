529291cf1126d8dd1cab65929959ba5e934e6472
xanthian <xanneh@hotmail.com>
2021-12-20 17:28:43 +0000

    this time its fixed
    promise

--- src/main/java/techreborn/items/tool/ChainsawItem.java
+++ src/main/java/techreborn/items/tool/ChainsawItem.java
@@ -32,6 +32,7 @@ import net.minecraft.item.*;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import reborncore.common.powerSystem.PowerSystem;
 import reborncore.common.powerSystem.RcEnergyItem;
 import reborncore.common.powerSystem.RcEnergyTier;
 import reborncore.common.util.ItemDurabilityExtensions;
