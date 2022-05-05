f3af644bf4d595bd800e2015fbf8aec34c7b1e48
Ayutac <skoch02@students.uni-mainz.de>
2022-05-05 18:48:18 +0200

    Started working on upgraders

--- src/main/java/techreborn/init/TRContent.java
+++ src/main/java/techreborn/init/TRContent.java
@@ -90,6 +90,7 @@ import techreborn.entities.EntityNukePrimed;
 import techreborn.events.ModRegistry;
 import techreborn.items.DynamicCellItem;
 import techreborn.items.UpgradeItem;
+import techreborn.items.UpgraderItem;
 import techreborn.items.armor.QuantumSuitItem;
 import techreborn.utils.InitUtils;
 import techreborn.world.OreDistribution;
@@ -302,22 +303,23 @@ public class TRContent {
 	}
 
 	public enum StorageUnit implements ItemConvertible {
-		BUFFER(1),
-		CRUDE(TechRebornConfig.crudeStorageUnitMaxStorage),
-		BASIC(TechRebornConfig.basicStorageUnitMaxStorage),
-		ADVANCED(TechRebornConfig.advancedStorageUnitMaxStorage),
-		INDUSTRIAL(TechRebornConfig.industrialStorageUnitMaxStorage),
-		QUANTUM(TechRebornConfig.quantumStorageUnitMaxStorage),
-		CREATIVE(Integer.MAX_VALUE);
+		BUFFER(1, false),
+		CRUDE(TechRebornConfig.crudeStorageUnitMaxStorage, true),
+		BASIC(TechRebornConfig.basicStorageUnitMaxStorage, true),
+		ADVANCED(TechRebornConfig.advancedStorageUnitMaxStorage, true),
+		INDUSTRIAL(TechRebornConfig.industrialStorageUnitMaxStorage, true),
+		QUANTUM(TechRebornConfig.quantumStorageUnitMaxStorage, false),
+		CREATIVE(Integer.MAX_VALUE, false);
 
 		public final String name;
 		public final Block block;
+		public final Item upgrader;
 
 		// How many items it can hold
 		public int capacity;
 
 
-		StorageUnit(int capacity) {
+		StorageUnit(int capacity, boolean upgradable) {
 			name = this.toString().toLowerCase(Locale.ROOT);
 			block = new StorageUnitBlock(this);
 			this.capacity = capacity;
@@ -326,11 +328,37 @@ public class TRContent {
 				InitUtils.setup(block, "storage_buffer");
 			else
 				InitUtils.setup(block, name + "_storage_unit");
+			if (upgradable) {
+				if (name.equals("buffer"))
+					upgrader = InitUtils.setup(new UpgraderItem(), "storage_buffer_upgrader");
+				else
+					upgrader = InitUtils.setup(new UpgraderItem(), name + "_unit_upgrader");
+			}
+			else
+				upgrader = null;
+		}
+
+		public Block asBlock() {
+			return block;
 		}
 
 		@Override
 		public Item asItem() {
 			return block.asItem();
+		}
+
+		public Optional<Item> getUpgrader() {
+			return Optional.ofNullable(upgrader);
+		}
+
+		public static Optional<StorageUnit> getUpgradableFor(UpgraderItem item) {
+			if (item == null)
+				return Optional.empty();
+			for (StorageUnit unit : StorageUnit.values()) {
+				if (item.equals(unit.getUpgrader().orElse(null)))
+					return Optional.of(unit);
+			}
+			return Optional.empty();
 		}
 	}
 
@@ -356,9 +384,32 @@ public class TRContent {
 			InitUtils.setup(block, name + "_tank_unit");
 		}
 
+		public Block asBlock() {
+			return block;
+		}
+
 		@Override
 		public Item asItem() {
 			return block.asItem();
+		}
+
+		public Optional<Item> getUpgrader() {
+			try {
+				return StorageUnit.valueOf(name()).getUpgrader();
+			}
+			catch (IllegalArgumentException ex) {
+				return Optional.empty();
+			}
+		}
+
+		public static Optional<TankUnit> getUpgradableFor(UpgraderItem item) {
+			if (item == null)
+				return Optional.empty();
+			for (TankUnit unit : TankUnit.values()) {
+				if (item.equals(unit.getUpgrader().orElse(null)))
+					return Optional.of(unit);
+			}
+			return Optional.empty();
 		}
 	}
 
