027568e5be868130d9856a6a5efb4603c4e69244
ayutac <fly.high.android@gmail.com>
2022-11-08 01:07:28 +0100

    crude launchpad functionality

---
+++ src/main/java/techreborn/blockentity/machine/tier2/LaunchpadBlockEntity.java
@@ -0,0 +1,169 @@
+/*
+ * This file is part of TechReborn, licensed under the MIT License (MIT).
+ *
+ * Copyright (c) 2020 TechReborn
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package techreborn.blockentity.machine.tier2;
+
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.MovementType;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Direction;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+import reborncore.api.IToolDrop;
+import reborncore.common.blockentity.MachineBaseBlockEntity;
+import reborncore.common.powerSystem.PowerAcceptorBlockEntity;
+import reborncore.common.screen.BuiltScreenHandler;
+import reborncore.common.screen.BuiltScreenHandlerProvider;
+import reborncore.common.screen.builder.ScreenHandlerBuilder;
+import reborncore.common.util.WorldUtils;
+import techreborn.blocks.machine.tier1.PlayerDetectorBlock;
+import techreborn.blocks.machine.tier1.PlayerDetectorBlock.PlayerDetectorType;
+import techreborn.config.TechRebornConfig;
+import techreborn.init.TRBlockEntities;
+import techreborn.init.TRContent;
+
+public class LaunchpadBlockEntity extends PowerAcceptorBlockEntity implements IToolDrop, BuiltScreenHandlerProvider {
+
+	boolean redstone = false;
+	int radius = 16;
+
+	public LaunchpadBlockEntity(BlockPos pos, BlockState state) {
+		super(TRBlockEntities.LAUNCHPAD, pos, state);
+	}
+
+	public boolean isProvidingPower() {
+		return redstone;
+	}
+
+	public void handleGuiInputFromClient(int amount) {
+		radius += amount;
+
+		if (radius > TechRebornConfig.playerDetectorMaxRadius) {
+			radius = TechRebornConfig.playerDetectorMaxRadius;
+		}
+		if (radius <= 1) {
+			radius = 1;
+		}
+	}
+
+	// PowerAcceptorBlockEntity
+	@Override
+	public void tick(World world, BlockPos pos, BlockState state, MachineBaseBlockEntity blockEntity) {
+		super.tick(world, pos, state, blockEntity);
+
+		if (world == null) {
+			return;
+		}
+
+		if (world.getTime() % 20 != 0) {
+			return;
+		}
+
+		boolean lastRedstone = redstone;
+		redstone = false;
+		if (getStored() > TechRebornConfig.playerDetectorEuPerTick) { //TODO
+			for (PlayerEntity player : world.getPlayers()) {
+				if (player.getBlockPos().down().equals(pos))
+					player.addVelocity(0d, 5d, 0d);
+			}
+			useEnergy(TechRebornConfig.playerDetectorEuPerTick); //TODO
+		}
+		if (lastRedstone != redstone) {
+			WorldUtils.updateBlock(world, pos);
+			world.updateNeighborsAlways(pos, world.getBlockState(pos).getBlock());
+		}
+	}
+
+	@Override
+	public long getBaseMaxPower() {
+		return TechRebornConfig.playerDetectorMaxEnergy; //TODO
+	}
+
+	@Override
+	public boolean canProvideEnergy(@Nullable Direction side) {
+		return false;
+	}
+
+	@Override
+	public long getBaseMaxOutput() {
+		return 0;
+	}
+
+	@Override
+	public long getBaseMaxInput() {
+		return TechRebornConfig.playerDetectorMaxInput; //TODO
+	}
+
+	@Override
+	public void readNbt(NbtCompound tag) {
+		super.readNbt(tag);
+	}
+
+	@Override
+	public void writeNbt(NbtCompound tag) {
+		super.writeNbt(tag);
+	}
+
+	// MachineBaseBlockEntity
+	@Override
+	public boolean hasSlotConfig() {
+		return false;
+	}
+
+	@Override
+	public boolean canBeUpgraded() {
+		return true;
+	}
+
+	// IToolDrop
+	@Override
+	public ItemStack getToolDrop(PlayerEntity p0) {
+		return TRContent.Machine.LAUNCHPAD.getStack();
+	}
+
+	// BuiltScreenHandlerProvider
+	@Override
+	public BuiltScreenHandler createScreenHandler(int syncID, PlayerEntity player) {
+		return new ScreenHandlerBuilder("player_detector")
+				.player(player.getInventory())
+				.inventory().hotbar().addInventory()
+				.blockEntity(this)
+				.syncEnergyValue()
+				.sync(this::getCurrentRadius, this::setCurrentRadius)
+				.addInventory().create(this, syncID);
+	}
+
+	public int getCurrentRadius() {
+		return radius;
+	}
+
+	public void setCurrentRadius(int radius) {
+		this.radius = radius;
+	}
+}
