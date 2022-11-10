51d75be991f12382deb5062b883e4fe64019ee61
ayutac <fly.high.android@gmail.com>
2022-11-10 10:36:49 +0100

    added elevator functionality w/o mixin

--- src/main/java/techreborn/blockentity/machine/tier1/ElevatorBlockEntity.java
+++ src/main/java/techreborn/blockentity/machine/tier1/ElevatorBlockEntity.java
@@ -25,19 +25,16 @@
 package techreborn.blockentity.machine.tier1;
 
 import net.minecraft.block.BlockState;
-import net.minecraft.entity.Entity;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.NbtCompound;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 import reborncore.api.IToolDrop;
-import reborncore.common.blockentity.MachineBaseBlockEntity;
 import reborncore.common.blockentity.RedstoneConfiguration;
 import reborncore.common.powerSystem.PowerAcceptorBlockEntity;
 import reborncore.common.screen.BuiltScreenHandler;
@@ -47,93 +44,89 @@ import techreborn.config.TechRebornConfig;
 import techreborn.init.TRBlockEntities;
 import techreborn.init.TRContent;
 
-import java.util.List;
+import java.util.function.BiFunction;
 
 public class ElevatorBlockEntity extends PowerAcceptorBlockEntity implements IToolDrop, BuiltScreenHandlerProvider {
 
-	public static final int MAX_SELECTION = 3;
-	private int selection = TechRebornConfig.launchpadDefaultSelection;
+	public static final int MIN_HEIGHT = -64;
+	public static final int MAX_HEIGHT = 319;
 
 	public ElevatorBlockEntity(BlockPos pos, BlockState state) {
 		super(TRBlockEntities.ELEVATOR, pos, state);
 	}
 
-	public void handleGuiInputFromClient(int amount) {
-		selection += amount;
-		ensureSelectionInRange();
+	public static boolean isRunning(final World world, final BlockPos pos) {
+		final BlockEntity entity = world.getBlockEntity(pos);
+		if (!(entity instanceof ElevatorBlockEntity)) {
+			return false;
+		}
+		return ((ElevatorBlockEntity)entity).getStored() > 0;
 	}
 
-	public void ensureSelectionInRange() {
-		if (selection > MAX_SELECTION) {
-			selection = MAX_SELECTION;
-		}
-		if (selection <= 0) {
-			selection = 0;
-		}
+	public static boolean isFree(final World world, final BlockPos pos) {
+		return world.getBlockState(pos.up()).isAir() && world.getBlockState(pos.up().up()).isAir();
 	}
+
+	public static boolean isValidTarget(final World world, final BlockPos pos) {
+		return isRunning(world, pos) && isFree(world, pos);
+	}
+
+	public static BlockPos nextUpElevator(final World world, final BlockPos pos) {
+		BlockPos upPos = pos.up().up();
+		do {
+			upPos = upPos.up();
+		} while (upPos.getY() <= MAX_HEIGHT && !isValidTarget(world, upPos));
+		if (upPos.getY() < MAX_HEIGHT || isValidTarget(world, upPos)) {
+			return upPos;
+		}
+		return null;
+	}
+
+	public static BlockPos nextDownElevator(final World world, final BlockPos pos) {
+		BlockPos downPos = pos.down().down();
+		do {
+			downPos = downPos.down();
+		} while (downPos.getY() >= MIN_HEIGHT && !isValidTarget(world, downPos));
+		if (downPos.getY() > MIN_HEIGHT || isValidTarget(world, downPos)) {
+			return downPos;
+		}
+		return null;
+	}
+
+	public static int energyCost(BlockPos startPos, BlockPos endPos) {
+		return Math.min(Math.abs(endPos.getY()-startPos.getY())*TechRebornConfig.elevatorEnergyPerBlock,0);
+	}
+
+	public void teleportUp(World world, BlockPos pos, PlayerEntity player) {
+		teleport(world, pos, player, ElevatorBlockEntity::nextUpElevator);
+	}
+
+	public void teleportDown(World world, BlockPos pos, PlayerEntity player) {
+		teleport(world, pos, player, ElevatorBlockEntity::nextDownElevator);
+	}
+
+	protected void teleport(World world, BlockPos pos, PlayerEntity player, BiFunction<World, BlockPos, BlockPos> targetCalculator) {
+		if (world == null || !isActive(RedstoneConfiguration.POWER_IO)) {
+			return;
+		}
+		BlockPos targetPos = targetCalculator.apply(world, pos);
+		if (targetPos == null) {
+			return;
+		}
+		final int energy = energyCost(pos, targetPos);
+		if (getStored() < energy) {
+			return;
+		}
+		world.playSound(null, pos, SoundEvents.ENTITY_ENDERMAN_TELEPORT, SoundCategory.BLOCKS, 1f, 1f);
+		player.teleport(targetPos.getX(), targetPos.getY(), targetPos.getZ(), true);
+		useEnergy(energy);
+	}
+	// world.playSound(null, pos, SoundEvents.ENTITY_ENDERMAN_TELEPORT, SoundCategory.BLOCKS, 1f, 1f);
 
 	// PowerAcceptorBlockEntity
 	@Override
-	public void tick(World world, BlockPos pos, BlockState state, MachineBaseBlockEntity blockEntity) {
-		super.tick(world, pos, state, blockEntity);
-		if (world == null || getStored() <= 0 || !isActive(RedstoneConfiguration.POWER_IO)) {
-			return;
-		}
-
-		if (world.getTime() % TechRebornConfig.launchpadInterval != 0) {
-			return;
-		}
-
-		ensureSelectionInRange();
-		final double speed = selectedSpeed(selection);
-		final int energyCost = selectedEnergyCost(selection);
-
-		if (getStored() > energyCost) {
-			List<Entity> entities = world.getNonSpectatingEntities(Entity.class, new Box(0d,1d,0d,1d,2d,1d).offset(pos));
-			if (entities.size() == 0) {
-				return;
-			}
-			world.playSound(null, pos, SoundEvents.BLOCK_PISTON_EXTEND, SoundCategory.BLOCKS, 1f, 1f);
-			for (Entity entity : entities) {
-				entity.addVelocity(0d, speed, 0d);
-			}
-			useEnergy(energyCost);
-		}
-	}
-
-	public static double selectedSpeed(final int selection) {
-		return switch(selection) {
-			case 0 -> TechRebornConfig.launchpadSpeedLow;
-			case 1 -> TechRebornConfig.launchpadSpeedMedium;
-			case 2 -> TechRebornConfig.launchpadSpeedHigh;
-			case MAX_SELECTION -> TechRebornConfig.launchpadSpeedExtreme;
-			default -> throw new IllegalArgumentException("Impossible launchpad selection value!");
-		};
-	}
-
-	public static int selectedEnergyCost(final int selection) {
-		return switch(selection) {
-			case 0 -> TechRebornConfig.launchpadEnergyLow;
-			case 1 -> TechRebornConfig.launchpadEnergyMedium;
-			case 2 -> TechRebornConfig.launchpadEnergyHigh;
-			case MAX_SELECTION -> TechRebornConfig.launchpadEnergyExtreme;
-			default -> throw new IllegalArgumentException("Impossible launchpad selection value!");
-		};
-	}
-
-	public static String selectedTranslationKey(final int selection) {
-		return switch(selection) {
-			case 0 -> "techreborn.message.info.block.techreborn.launchpad.low";
-			case 1 -> "techreborn.message.info.block.techreborn.launchpad.medium";
-			case 2 -> "techreborn.message.info.block.techreborn.launchpad.high";
-			case MAX_SELECTION -> "techreborn.message.info.block.techreborn.launchpad.extreme";
-			default -> throw new IllegalArgumentException("Impossible launchpad selection value!");
-		};
-	}
-
-	@Override
 	public long getBaseMaxPower() {
-		return TechRebornConfig.launchpadMaxEnergy;
+		return TechRebornConfig.elevatorMaxEnergy;
 	}
 
 	@Override
@@ -148,19 +141,7 @@ public class ElevatorBlockEntity extends PowerAcceptorBlockEntity implements ITo
 
 	@Override
 	public long getBaseMaxInput() {
-		return TechRebornConfig.launchpadMaxInput;
-	}
-
-	@Override
-	public void readNbt(NbtCompound tag) {
-		super.readNbt(tag);
-		selection = tag.getInt("selection");
-	}
-
-	@Override
-	public void writeNbt(NbtCompound tag) {
-		super.writeNbt(tag);
-		tag.putInt("selection", selection);
+		return TechRebornConfig.elevatorMaxInput;
 	}
 
 	// MachineBaseBlockEntity
@@ -177,7 +158,7 @@ public class ElevatorBlockEntity extends PowerAcceptorBlockEntity implements ITo
 	// IToolDrop
 	@Override
 	public ItemStack getToolDrop(PlayerEntity p0) {
-		return TRContent.Machine.LAUNCHPAD.getStack();
+		return TRContent.Machine.ELEVATOR.getStack();
 	}
 
 	// BuiltScreenHandlerProvider
@@ -188,15 +169,6 @@ public class ElevatorBlockEntity extends PowerAcceptorBlockEntity implements ITo
 				.inventory().hotbar().addInventory()
 				.blockEntity(this)
 				.syncEnergyValue()
-				.sync(this::getSelection, this::setSelection)
 				.addInventory().create(this, syncID);
-	}
-
-	public int getSelection() {
-		return selection;
-	}
-
-	public void setSelection(int selection) {
-		this.selection = selection;
 	}
 }
--- src/main/java/techreborn/config/TechRebornConfig.java
+++ src/main/java/techreborn/config/TechRebornConfig.java
@@ -621,6 +621,15 @@ public class TechRebornConfig {
 	@Config(config = "machines", category = "launchpad", key = "LaunchpadInterval", comment = "Launchpad Launch Interval in Ticks > 0")
 	public static int launchpadInterval = 100; // 5 seconds
 
+	@Config(config = "machines", category = "elevator", key = "ElevatorMaxInput", comment = "Elevator Max Input (Energy per tick)")
+	public static int elevatorMaxInput = 32;
+
+	@Config(config = "machines", category = "elevator", key = "ElevatorMaxEnergy", comment = "Elevator Max Energy")
+	public static int elevatorMaxEnergy = 1_000;
+
+	@Config(config = "machines", category = "elevator", key = "ElevatorEnergyPerBlock", comment = "Elevator Energy used per vertical block of transportation")
+	public static int elevatorEnergyPerBlock = 2;
+
 	// Misc
 	@Config(config = "misc", category = "general", key = "IC2TransformersStyle", comment = "Input from dots side, output from other sides, like in IC2.")
 	public static boolean IC2TransformersStyle = true;
