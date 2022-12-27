e2f36109a1def4f3b1e83fa32496a3943cd8f785
maxvar <varfick@gmail.com>
2022-12-27 21:15:03 +0300

    added proper outward scanner for blocks

--- src/main/java/techreborn/blockentity/machine/misc/PumpBlockEntity.java
+++ src/main/java/techreborn/blockentity/machine/misc/PumpBlockEntity.java
@@ -13,6 +13,7 @@ import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.NotNull;
 import reborncore.common.blockentity.MachineBaseBlockEntity;
 import reborncore.common.fluid.FluidValue;
 import reborncore.common.fluid.container.FluidInstance;
@@ -21,50 +22,73 @@ import reborncore.common.screen.BuiltScreenHandlerProvider;
 import reborncore.common.screen.builder.ScreenHandlerBuilder;
 import reborncore.common.util.RebornInventory;
 import reborncore.common.util.Tank;
+import techreborn.TechReborn;
 import techreborn.blockentity.machine.GenericMachineBlockEntity;
 import techreborn.config.TechRebornConfig;
 import techreborn.init.TRBlockEntities;
 import techreborn.init.TRContent;
+
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class PumpBlockEntity extends GenericMachineBlockEntity implements BuiltScreenHandlerProvider {
 
 	public static final FluidValue TANK_CAPACITY = FluidValue.BUCKET.multiply(16);
 	public static final int DEFAULT_RANGE = 10;
 	public static final int DEFAULT_DEPTH = 10;
-	protected Tank tank;
+	private Iterator<BlockPos> finder;
+	protected final Tank tank;
 	boolean exhausted;
 	BlockPos pumpedTargetBlockPos;
 	long timePumpingStarted;
 	long timeLastScanned;
-	int range = 10;
+	int range;
 
 	public int getRange() {
 		return range;
 	}
 
 	public void setRange(int newRange) {
-		range = newRange;
-		if (range < 1) range = 1;
-		if (range > 50) range = 50;
+		TechReborn.LOGGER.info("setRange {} → {}", range, newRange);
+		if (newRange != range) {
+			range = newRange;
+			if (range < 1) range = 1;
+			if (range > 50) range = 50;
+			finder = null;
+			exhausted = false;
+		}
 	}
 
-	int depth = 10;
+	int depth;
 
 	public int getDepth() {
 		return depth;
 	}
 
 	public void setDepth(int newDepth) {
-		depth = newDepth;
-		if (depth < 1) depth = 1;
-		if (depth > 50) depth = 50;
+		TechReborn.LOGGER.info("setDepth {} → {}", depth, newDepth);
+		if (newDepth != depth) {
+			depth = newDepth;
+			if (depth < 1) depth = 1;
+			if (depth > 50) depth = 50;
+			finder = null;
+			exhausted = false;
+		}
 	}
 
 	public PumpBlockEntity(BlockPos pos, BlockState state) {
 		super(TRBlockEntities.PUMP, pos, state, "Pump", TechRebornConfig.pumpMaxInput, TechRebornConfig.pumpMaxEnergy, TRContent.Machine.PUMP.block, 0);
+		TechReborn.LOGGER.debug("constructing pump at {} with state {}...", pos, state);
 		this.inventory = new RebornInventory<>(1, "PumpBlockEntity", 64, this);
 		this.tank = new Tank("PumpBlockEntity", TANK_CAPACITY, this);
 		this.exhausted = false;
+		this.range = DEFAULT_RANGE;
+		this.depth = DEFAULT_DEPTH;
+		TechReborn.LOGGER.debug("pump created");
+	}
+
+	private void setupFinder() {
+		this.finder = new BlockPosIterable(pos, range, depth).iterator();
 	}
 
 	@Override
@@ -81,38 +105,24 @@ public class PumpBlockEntity extends GenericMachineBlockEntity implements BuiltS
 	public void readNbt(final NbtCompound tagCompound) {
 		super.readNbt(tagCompound);
 		tank.read(tagCompound);
-		if (tagCompound.contains("rangeConfig")) {
-			NbtCompound rangeConfig = tagCompound.getCompound("rangeConfig");
-			this.range = rangeConfig.getInt("range");
-			this.depth = rangeConfig.getInt("depth");
-		} else {
-			this.range = DEFAULT_RANGE;
-			this.depth = DEFAULT_DEPTH;
-		}
+		this.range = tagCompound.getInt("range");
+		this.depth = tagCompound.getInt("depth");
+		TechReborn.LOGGER.debug("readNbt, contains range {} depth {}", range, depth);
+		finder = null;
 	}
 
 	@Override
 	public void writeNbt(final NbtCompound tagCompound) {
 		super.writeNbt(tagCompound);
 		tank.write(tagCompound);
-		NbtCompound rangeConfig = new NbtCompound();
-		rangeConfig.putInt("range", range);
-		rangeConfig.putInt("depth", depth);
-		tagCompound.put("rangeConfig", rangeConfig);
+		tagCompound.putInt("range", range);
+		tagCompound.putInt("depth", depth);
+		TechReborn.LOGGER.debug("writeNbt, range {} depth {}", range, depth);
 	}
 
 	@Override
 	public BuiltScreenHandler createScreenHandler(int syncID, PlayerEntity player) {
-		return new ScreenHandlerBuilder("pump")
-			.player(player.getInventory()).inventory().hotbar().addInventory()
-			.blockEntity(this)
-			.energySlot(0, 8, 72)
-			.sync(tank)
-			.syncEnergyValue()
-			.sync(this::getDepth, this::setDepth)
-			.sync(this::getRange, this::setRange)
-			.addInventory()
-			.create(this, syncID);
+		return new ScreenHandlerBuilder("pump").player(player.getInventory()).inventory().hotbar().addInventory().blockEntity(this).energySlot(0, 8, 72).sync(tank).syncEnergyValue().sync(this::getDepth, this::setDepth).sync(this::getRange, this::setRange).addInventory().create(this, syncID);
 	}
 
 	@Override
@@ -126,8 +136,7 @@ public class PumpBlockEntity extends GenericMachineBlockEntity implements BuiltS
 
 		//if pumping time completed
 		if (pumpedTargetBlockPos != null) {
-			if ((world.getTime() - timePumpingStarted >= (long) (TechRebornConfig.pumpTicksToComplete * (1 - getSpeedMultiplier())))
-				&& getEnergy() >= (long) (TechRebornConfig.pumpEnergyToCollect * getPowerMultiplier())) {
+			if ((world.getTime() - timePumpingStarted >= (long) (TechRebornConfig.pumpTicksToComplete * (1 - getSpeedMultiplier()))) && getEnergy() >= (long) (TechRebornConfig.pumpEnergyToCollect * getPowerMultiplier())) {
 				//collect fluid
 				Fluid fluid;
 				FluidValue fluidValue = FluidValue.BUCKET;
@@ -163,7 +172,6 @@ public class PumpBlockEntity extends GenericMachineBlockEntity implements BuiltS
 				//replace target with solid
 				world.setBlockState(pumpedTargetBlockPos, Blocks.COBBLESTONE.getDefaultState());
 				pumpedTargetBlockPos = null;
-				return;
 			}
 		} else if (world.getTime() - timeLastScanned >= TechRebornConfig.pumpTicksUntilNextAttempt) {
 			timeLastScanned = world.getTime();
@@ -176,27 +184,25 @@ public class PumpBlockEntity extends GenericMachineBlockEntity implements BuiltS
 				}
 				//else - consider exhausted
 				this.exhausted = true;
+				TechReborn.LOGGER.debug("pump {} exhausted", pos);
 			}
 		}
 	}
 
 	private void findNextToPump(World world) {
-		found:
-		for (int y = this.pos.getY() - 1; y >= this.pos.getY() - 1 - depth; y--) {
-			for (int x = this.pos.getX() - range; x <= this.pos.getX() + range; x++) {
-				for (int z = this.pos.getZ() - range; z <= this.pos.getZ() + range; z++) {
-					BlockPos blockPos = new BlockPos(x, y, z);
-					BlockState blockState = world.getBlockState(blockPos);
-					Block block = blockState.getBlock();
+		if (finder == null) {
+			setupFinder();
+		}
+		while (finder.hasNext()) {
+			BlockPos blockPos = finder.next();
 
-					if (block == Blocks.LAVA && blockState.getFluidState().getLevel() == 8 && (tank.getFluid() == Fluids.LAVA || tank.getFluid() == Fluids.EMPTY)
-						|| block == Blocks.WATER && blockState.getFluidState().getLevel() == 8 && (tank.getFluid() == Fluids.WATER || tank.getFluid() == Fluids.EMPTY)) {
-						//if any found - start pumping
-						pumpedTargetBlockPos = new BlockPos(x, y, z);
-						break found;
-					}
-				}
+			BlockState blockState = world.getBlockState(blockPos);
+			Block block = blockState.getBlock();
 
+			if (block == Blocks.LAVA && blockState.getFluidState().getLevel() == 8 && (tank.getFluid() == Fluids.LAVA || tank.getFluid() == Fluids.EMPTY) || block == Blocks.WATER && blockState.getFluidState().getLevel() == 8 && (tank.getFluid() == Fluids.WATER || tank.getFluid() == Fluids.EMPTY)) {
+				//if any found - start pumping
+				pumpedTargetBlockPos = blockPos;
+				return;
 			}
 		}
 	}
@@ -207,5 +213,59 @@ public class PumpBlockEntity extends GenericMachineBlockEntity implements BuiltS
 
 	public void handleRangeGuiInputFromClient(int buttonAmount) {
 		setRange(range + buttonAmount);
+	}
+
+	static class BlockPosIterable implements Iterable<BlockPos> {
+
+		static class MeasuredPos extends BlockPos {
+			private final double weight;
+
+			public MeasuredPos(int x, int y, int z, double weight) {
+				super(x, y, z);
+				this.weight = weight;
+			}
+		}
+
+		int index;
+		final int layerSize;
+		final int m;
+
+		final ArrayList<MeasuredPos> layer;
+
+		public BlockPosIterable(BlockPos centerTop, int range, int depth) {
+			this.index = 0;
+			this.layerSize = (range * 2 + 1) * (range * 2 + 1);
+			this.m = layerSize * depth;
+			layer = new ArrayList<>(layerSize);
+			for (int i = centerTop.getX() - range; i <= centerTop.getX() + range; i++)
+				for (int j = centerTop.getZ() - range; j <= centerTop.getZ() + range; j++)
+					layer.add(new MeasuredPos(i, centerTop.getY(), j, centerTop.getSquaredDistance(i, centerTop.getY(), j)));
+			layer.sort((o1, o2) -> (int) (o1.weight - o2.weight));
+			TechReborn.LOGGER.info("prepared layer info for pos {} with range {} and depth {}", centerTop, range, depth);
+			TechReborn.LOGGER.info("layer size = {} and m = {}", layerSize, m);
+		}
+
+		/**
+		 * Returns an iterator over elements of type {@code T}.
+		 *
+		 * @return an Iterator.
+		 */
+		@NotNull
+		@Override
+		public Iterator<BlockPos> iterator() {
+			return new Iterator<>() {
+				@Override
+				public boolean hasNext() {
+					return index < m;
+				}
+
+				@Override
+				public BlockPos next() {
+					BlockPos pos = layer.get(index % layerSize).down(1 + index / layerSize);
+					index++;
+					return pos;
+				}
+			};
+		}
 	}
 }
