45c43277f9929271dcca569c7a343e8b1d0ca452
reusu <i@teemo.name>
2024-02-27 22:42:41 +0800

    Update RcEnergyItem.java

--- RebornCore/src/main/java/reborncore/common/powerSystem/RcEnergyItem.java
+++ RebornCore/src/main/java/reborncore/common/powerSystem/RcEnergyItem.java
@@ -85,11 +85,7 @@ public interface RcEnergyItem extends SimpleEnergyItem, FabricItem {
 		Random random = Random.create();
 		int unbreakingLevel = EnchantmentHelper.getLevel(Enchantments.UNBREAKING, stack);
 		if (unbreakingLevel > 0) {
-			int divisor = 0;
-		    do {
-		        divisor = random.nextInt(unbreakingLevel + 1);
-		    } while (divisor == 0);
-			amount = amount / divisor;
+			amount = amount / (random.nextInt(unbreakingLevel) + 1);
 		}
 
 		return SimpleEnergyItem.super.tryUseEnergy(stack, amount);
