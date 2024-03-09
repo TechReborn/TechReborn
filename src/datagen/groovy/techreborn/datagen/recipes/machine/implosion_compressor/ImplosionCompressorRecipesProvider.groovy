47911a4ed69fd1760e22c51d100e3e0d7275733b
drcrazy <hcb@wowhcb.ru>
2024-03-09 22:40:06 +0300

    Implosion Compressor moved to datagen

--- src/datagen/groovy/techreborn/datagen/recipes/machine/implosion_compressor/ImplosionCompressorRecipesProvider.groovy
+++ src/datagen/groovy/techreborn/datagen/recipes/machine/implosion_compressor/ImplosionCompressorRecipesProvider.groovy
@@ -41,5 +41,125 @@ class ImplosionCompressorRecipesProvider extends TechRebornRecipesProvider {
 
 	@Override
 	void generateRecipes() {
+		generateGems()
+		generateMisc()
+	}
+
+	void generateGems(){
+		[
+			(TRConventionalTags.DIAMOND_DUSTS) : Items.DIAMOND,
+			(TRConventionalTags.EMERALD_DUSTS) : Items.EMERALD,
+			(TRConventionalTags.PERIDOT_DUSTS) : TRContent.Gems.PERIDOT,
+			(TRConventionalTags.RED_GARNET_DUSTS) : TRContent.Gems.RED_GARNET,
+			(TRConventionalTags.RUBY_DUSTS) : TRContent.Gems.RUBY,
+			(TRConventionalTags.SAPPHIRE_DUSTS) : TRContent.Gems.SAPPHIRE,
+			(TRConventionalTags.YELLOW_GARNET_DUSTS) : TRContent.Gems.YELLOW_GARNET
+		].each {dust, gem->
+			offerImplosionCompressorRecipe {
+				power 30
+				time 2000
+				ingredient {
+					tag(dust, 4)
+				}
+				ingredients stack(Items.TNT, 16)
+				outputs stack(gem, 3), stack(TRContent.Dusts.DARK_ASHES, 12)
+				criterion getCriterionName(dust), getCriterionConditions(dust)
+			}
+			offerImplosionCompressorRecipe {
+				power 30
+				time 2000
+				ingredient {
+					tag(dust, 4)
+				}
+				ingredients stack(Items.END_CRYSTAL, 4)
+				outputs stack(gem, 3), stack(TRContent.SmallDusts.ENDER_EYE, 4)
+				criterion getCriterionName(dust), getCriterionConditions(dust)
+			}
+		}
+	}
+
+	void generateMisc(){
+		offerImplosionCompressorRecipe {
+			power 30
+			time 2000
+			ingredient {
+				tag(TRConventionalTags.AMETHYST_DUSTS, 8)
+			}
+			ingredients stack(Items.TNT, 16)
+			outputs stack(Items.AMETHYST_SHARD, 16), TRContent.Dusts.QUARTZ
+			criterion getCriterionName(TRConventionalTags.AMETHYST_DUSTS), getCriterionConditions(TRConventionalTags.AMETHYST_DUSTS)
+		}
+		offerImplosionCompressorRecipe {
+			power 30
+			time 2000
+			ingredient {
+				tag(TRConventionalTags.AMETHYST_DUSTS, 8)
+			}
+			ingredients stack(Items.END_CRYSTAL, 4)
+			outputs stack(Items.AMETHYST_SHARD, 16), TRContent.Dusts.QUARTZ
+			criterion getCriterionName(TRConventionalTags.AMETHYST_DUSTS), getCriterionConditions(TRConventionalTags.AMETHYST_DUSTS)
+		}
+		offerImplosionCompressorRecipe {
+			power 30
+			time 2000
+			ingredients TRContent.Ingots.IRIDIUM_ALLOY,  stack(Items.TNT, 8)
+			outputs TRContent.Plates.IRIDIUM_ALLOY, stack(TRContent.Dusts.DARK_ASHES, 4)
+			criterion getCriterionName(TRContent.Ingots.IRIDIUM_ALLOY), getCriterionConditions(TRContent.Ingots.IRIDIUM_ALLOY)
+		}
+		offerImplosionCompressorRecipe {
+			power 30
+			time 2000
+			ingredients TRContent.Ingots.IRIDIUM_ALLOY,  stack(Items.END_CRYSTAL, 2)
+			outputs TRContent.Plates.IRIDIUM_ALLOY, stack(TRContent.SmallDusts.ENDER_EYE, 2)
+			criterion getCriterionName(TRContent.Ingots.IRIDIUM_ALLOY), getCriterionConditions(TRContent.Ingots.IRIDIUM_ALLOY)
+		}
+		offerImplosionCompressorRecipe {
+			power 30
+			time 2000
+			ingredient {
+				tag(TRConventionalTags.NETHERITE_NUGGETS, 9)
+			}
+			ingredients stack(Items.TNT, 16)
+			outputs Items.NETHERITE_INGOT, stack(TRContent.Dusts.DARK_ASHES, 12)
+			criterion getCriterionName(TRConventionalTags.NETHERITE_NUGGETS), getCriterionConditions(TRConventionalTags.NETHERITE_NUGGETS)
+		}
+		offerImplosionCompressorRecipe {
+			power 30
+			time 2000
+			ingredient {
+				tag(TRConventionalTags.NETHERITE_NUGGETS, 9)
+			}
+			ingredients stack(Items.END_CRYSTAL, 4)
+			outputs Items.NETHERITE_INGOT, stack(TRContent.SmallDusts.ENDER_EYE, 4)
+			criterion getCriterionName(TRConventionalTags.NETHERITE_NUGGETS), getCriterionConditions(TRConventionalTags.NETHERITE_NUGGETS)
+		}
+		offerImplosionCompressorRecipe {
+			power 30
+			time 2000
+			ingredients stack(Items.REDSTONE_BLOCK, 9), stack(Items.TNT, 16)
+			outputs stack(TRContent.Dusts.RED_GARNET, 4), stack(TRContent.Dusts.DARK_ASHES, 4)
+			criterion getCriterionName(Items.REDSTONE_BLOCK), getCriterionConditions(Items.REDSTONE_BLOCK)
+		}
+		offerImplosionCompressorRecipe {
+			power 30
+			time 2000
+			ingredients stack(Items.REDSTONE_BLOCK, 9), stack(Items.END_CRYSTAL, 4)
+			outputs stack(TRContent.Dusts.RED_GARNET, 4), stack(TRContent.SmallDusts.ENDER_EYE, 4)
+			criterion getCriterionName(Items.REDSTONE_BLOCK), getCriterionConditions(Items.REDSTONE_BLOCK)
+		}
+		offerImplosionCompressorRecipe {
+			power 30
+			time 2000
+			ingredients stack(Items.SCULK, 64), stack(Items.TNT, 8)
+			outputs Items.SCULK_CATALYST, stack(TRContent.Dusts.DARK_ASHES, 4)
+			criterion getCriterionName(Items.SCULK), getCriterionConditions(Items.SCULK)
+		}
+		offerImplosionCompressorRecipe {
+			power 30
+			time 2000
+			ingredients stack(Items.SCULK, 64), stack(Items.END_CRYSTAL, 2)
+			outputs Items.SCULK_CATALYST, stack(TRContent.SmallDusts.ENDER_EYE, 2)
+			criterion getCriterionName(Items.SCULK), getCriterionConditions(Items.SCULK)
+		}
 	}
 }
