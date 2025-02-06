b9ac16b23ca184463d9120a736203c69aa01caad
ZurrTum <zurrtum@foxmail.com>
2025-02-06 16:43:41 +0800

    Fix Chunkloader render

--- RebornCore/src/client/java/reborncore/client/ClientChunkManager.java
+++ RebornCore/src/client/java/reborncore/client/ClientChunkManager.java
@@ -24,15 +24,14 @@
 
 package reborncore.client;
 
-import com.mojang.blaze3d.systems.RenderSystem;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.BufferBuilder;
-import net.minecraft.client.render.Tessellator;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.VertexFormat;
-import net.minecraft.client.render.VertexFormats;
+import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.ChunkPos;
+import net.minecraft.util.math.ColorHelper;
+import org.joml.Matrix4f;
 import reborncore.common.chunkloading.ChunkLoaderManager;
 import reborncore.common.network.ServerBoundPackets;
 
@@ -49,7 +48,7 @@ public class ClientChunkManager {
 	}
 
 	public static void toggleLoadedChunks(BlockPos chunkLoader) {
-		if (loadedChunks.size() == 0) {
+		if (loadedChunks.isEmpty()) {
 			ClientNetworkManager.sendToServer(ServerBoundPackets.requestChunkLoaderChunks(chunkLoader));
 		} else {
 			loadedChunks.clear();
@@ -67,42 +66,161 @@ public class ClientChunkManager {
 	}
 
 	public static void render(MatrixStack matrices, VertexConsumerProvider vertexConsumers, double x, double y, double z) {
-		if (loadedChunks.size() == 0) {
+		int size = loadedChunks.size();
+		if (size == 0) {
 			return;
 		}
+
 		final MinecraftClient minecraftClient = MinecraftClient.getInstance();
+		Identifier worldName = ChunkLoaderManager.getWorldName(minecraftClient.world);
+		int[] posX = new int[size], posZ = new int[size];
+		int right = 0, startX, startZ, maxX = Integer.MIN_VALUE, minX = Integer.MAX_VALUE, maxZ = Integer.MIN_VALUE, minZ = Integer.MAX_VALUE;
+		for (int i = 0; i < size; i++) {
+			ChunkLoaderManager.LoadedChunk chunk = loadedChunks.get(i);
+			if (chunk.getWorld().equals(worldName)) {
+				ChunkPos pos = chunk.getChunk();
+				startX = posX[right] = pos.getStartX();
+				startZ = posZ[right] = pos.getStartZ();
+				if (maxX < startX) maxX = startX;
+				if (minX > startX) minX = startX;
+				if (maxZ < startZ) maxZ = startZ;
+				if (minZ > startZ) minZ = startZ;
+				right++;
+			}
+		}
+		if (right == 0) {
+			return;
+		}
+		size = right;
 
-		RenderSystem.enableDepthTest();
+		int bottom = minecraftClient.world.getBottomY();
+		int top = minecraftClient.world.getTopY();
+		DrawContext ctx = new DrawContext(
+			vertexConsumers.getBuffer(RenderLayer.getDebugLineStrip(1.0)),
+			matrices.peek().getPositionMatrix(),
+			x, y, z, bottom, top
+		);
 
-		// FIXME 1.17
+		int chunkSize = 16, middle = chunkSize / 2, end = chunkSize;
+		for (int i = 0; i < size; i++) {
+			ctx.updatePos(posX[i], posZ[i]);
+			ctx.drawVerticalRed(middle, middle);
+			if (posX[i] == minX) {
+				ctx.drawVerticalBlue(0, 0);
+				for (int j = 2; j < chunkSize; j += 4) ctx.drawVerticalYellow(0, j);
+				for (int j = 4; j < chunkSize; j += 4) ctx.drawVerticalCyan(0, j);
+			}
+			if (posX[i] == maxX) {
+				ctx.drawVerticalBlue(end, end);
+				for (int j = 2; j < chunkSize; j += 4) ctx.drawVerticalYellow(end, j);
+				for (int j = 4; j < chunkSize; j += 4) ctx.drawVerticalCyan(end, j);
+			}
+			if (posZ[i] == minZ) {
+				ctx.drawVerticalBlue(end, 0);
+				for (int j = 2; j < chunkSize; j += 4) ctx.drawVerticalYellow(j, 0);
+				for (int j = 4; j < chunkSize; j += 4) ctx.drawVerticalCyan(j, 0);
+			}
+			if (posZ[i] == maxZ) {
+				ctx.drawVerticalBlue(0, end);
+				for (int j = 2; j < chunkSize; j += 4) ctx.drawVerticalYellow(j, end);
+				for (int j = 4; j < chunkSize; j += 4) ctx.drawVerticalCyan(j, end);
+			}
+		}
 
-		/*RenderSystem.shadeModel(7425);
-		RenderSystem.enableAlphaTest();
-		RenderSystem.defaultAlphaFunc();*/
-
-		final Tessellator tessellator = Tessellator.getInstance();
-		final BufferBuilder bufferBuilder = tessellator.getBuffer();
-
-		RenderSystem.disableBlend();
-		RenderSystem.lineWidth(5.0F);
-
-		bufferBuilder.begin(VertexFormat.DrawMode.LINE_STRIP, VertexFormats.POSITION_COLOR);
-
-		loadedChunks.stream()
-				.filter(loadedChunk -> loadedChunk.getWorld().equals(ChunkLoaderManager.getWorldName(minecraftClient.world)))
-				.forEach(loadedChunk -> {
-					double chunkX = (double) loadedChunk.getChunk().getStartX() - x;
-					double chunkY = (double) loadedChunk.getChunk().getStartZ() - z;
-
-					bufferBuilder.vertex(chunkX + 8, 0.0D - y, chunkY + 8).color(1.0F, 0.0F, 0.0F, 0.0F).next();
-					bufferBuilder.vertex(chunkX + 8, 0.0D - y, chunkY + 8).color(1.0F, 0.0F, 0.0F, 0.5F).next();
-					bufferBuilder.vertex(chunkX + 8, 256.0D - y, chunkY + 8).color(1.0F, 0.0F, 0.0F, 0.5F).next();
-					bufferBuilder.vertex(chunkX + 8, 256.0D - y, chunkY + 8).color(1.0F, 0.0F, 0.0F, 0.0F).next();
-				});
-
-		tessellator.draw();
-		RenderSystem.lineWidth(1.0F);
-		RenderSystem.enableBlend();
+		ctx.updatePos(minX, minZ, maxX + chunkSize, maxZ + chunkSize);
+		for (int i = bottom, j; i < top;) {
+			ctx.drawHorizontalBlue(i);
+			ctx.drawHorizontalCyan(j = i + middle);
+			for (int stop = (i += 2) + 12; i <= stop; i += 2) {
+				if (i != j) ctx.drawHorizontalYellow(i);
+			}
+		}
+		ctx.drawHorizontalBlue(top);
 	}
 
+	static class DrawContext {
+		private static final int NONE = ColorHelper.Argb.getArgb(0, 0, 0, 0);
+		private static final int RED = ColorHelper.Argb.getArgb(127, 255, 0, 0);
+		private static final int BLUE = ColorHelper.Argb.getArgb(255, 63, 63, 255);
+		private static final int DARK_CYAN = ColorHelper.Argb.getArgb(255, 0, 155, 155);
+		private static final int YELLOW = ColorHelper.Argb.getArgb(255, 255, 255, 0);
+		private final VertexConsumer vertexConsumer;
+		private final Matrix4f matrix4f;
+		private final double cameraX;
+		private final double cameraY;
+		private final double cameraZ;
+		private final float bottom;
+		private final float top;
+		private float x1 = 0;
+		private float z1 = 0;
+		private float x2 = 0;
+		private float z2 = 0;
+
+		DrawContext(VertexConsumer vertexConsumer, Matrix4f matrix4f, double cameraX, double cameraY, double cameraZ, int bottom, int top) {
+			this.vertexConsumer = vertexConsumer;
+			this.matrix4f = matrix4f;
+			this.cameraX = cameraX;
+			this.cameraY = cameraY;
+			this.cameraZ = cameraZ;
+			this.bottom = (float) (bottom - cameraY);
+			this.top = (float) (top - cameraY);
+		}
+
+		public void updatePos(float x, float z) {
+			this.x1 = (float) (x - cameraX);
+			this.z1 = (float) (z - cameraZ);
+		}
+
+		public void updatePos(float x1, float z1, float x2, float z2) {
+			this.x1 = (float) (x1 - cameraX);
+			this.z1 = (float) (z1 - cameraZ);
+			this.x2 = (float) (x2 - cameraX);
+			this.z2 = (float) (z2 - cameraZ);
+		}
+
+		public void drawVertical(int color, float x, float z) {
+			vertexConsumer.vertex(matrix4f, x, bottom, z).color(NONE).next();
+			vertexConsumer.vertex(matrix4f, x, bottom, z).color(color).next();
+			vertexConsumer.vertex(matrix4f, x, top, z).color(color).next();
+			vertexConsumer.vertex(matrix4f, x, top, z).color(NONE).next();
+		}
+
+		public void drawVerticalRed(int x, int z) {
+			drawVertical(RED, this.x1 + x, this.z1 + z);
+		}
+
+		public void drawVerticalBlue(int x, int z) {
+			drawVertical(BLUE, this.x1 + x, this.z1 + z);
+		}
+
+		public void drawVerticalYellow(int x, int z) {
+			drawVertical(YELLOW, this.x1 + x, this.z1 + z);
+		}
+
+		public void drawVerticalCyan(int x, int z) {
+			drawVertical(DARK_CYAN, this.x1 + x, this.z1 + z);
+		}
+
+		public void drawHorizontal(int color, float y) {
+			vertexConsumer.vertex(matrix4f, x1, y, z1).color(NONE).next();
+			vertexConsumer.vertex(matrix4f, x1, y, z1).color(color).next();
+			vertexConsumer.vertex(matrix4f, x1, y, z2).color(color).next();
+			vertexConsumer.vertex(matrix4f, x2, y, z2).color(color).next();
+			vertexConsumer.vertex(matrix4f, x2, y, z1).color(color).next();
+			vertexConsumer.vertex(matrix4f, x1, y, z1).color(color).next();
+			vertexConsumer.vertex(matrix4f, x1, y, z1).color(NONE).next();
+		}
+
+		public void drawHorizontalBlue(int y) {
+			drawHorizontal(BLUE, (float) (y - cameraY));
+		}
+
+		public void drawHorizontalYellow(int y) {
+			drawHorizontal(YELLOW, (float) (y - cameraY));
+		}
+
+		public void drawHorizontalCyan(int y) {
+			drawHorizontal(DARK_CYAN, (float) (y - cameraY));
+		}
+	}
 }
