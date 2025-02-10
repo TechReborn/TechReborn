4eab039536f67efeb368b6d3d69a87de03de8f04
ZurrTum <zurrtum@foxmail.com>
2025-02-11 01:12:31 +0800

    Fluid render uses water texture

--- src/main/java/techreborn/init/ModFluids.java
+++ src/main/java/techreborn/init/ModFluids.java
@@ -37,41 +37,41 @@ import techreborn.init.TRContent.BlockInfo;
 import java.util.Locale;
 
 public enum ModFluids implements BlockInfo {
-	BERYLLIUM,
-	CALCIUM,
-	CALCIUM_CARBONATE,
-	CARBON,
-	CARBON_FIBER,
-	CHLORITE,
-	COMPRESSED_AIR,
-	DEUTERIUM,
-	DIESEL,
-	ELECTROLYZED_WATER,
-	GLYCERYL,
-	HELIUM,
-	HELIUM3,
-	HELIUMPLASMA,
-	HYDROGEN,
-	LITHIUM,
-	MERCURY,
-	METHANE,
-	NITRO_CARBON,
-	NITRO_DIESEL,
-	NITROCOAL_FUEL,
-	NITROFUEL,
-	NITROGEN,
-	NITROGEN_DIOXIDE,
-	OIL,
-	POTASSIUM,
-	SILICON,
-	SODIUM,
-	SODIUM_SULFIDE,
-	SODIUM_PERSULFATE,
-	SULFUR,
-	SULFURIC_ACID,
-	TRITIUM,
-	WOLFRAMIUM,
-	BIOFUEL;
+	BERYLLIUM(0x364a34),
+	CALCIUM(0x9e6857),
+	CALCIUM_CARBONATE(0x8f401b),
+	CARBON(0x2d302b),
+	CARBON_FIBER(0x3d3838),
+	CHLORITE(0x4ba3a3),
+	COMPRESSED_AIR(0xb3b3b3),
+	DEUTERIUM(0xe5eb38),
+	DIESEL(0xd99938),
+	ELECTROLYZED_WATER(0x1414c9),
+	GLYCERYL(0x1c8a7b),
+	HELIUM(0xffff70),
+	HELIUM3(0xf2f274),
+	HELIUMPLASMA(0xfffa96),
+	HYDROGEN(0x334491),
+	LITHIUM(0x679ecf),
+	MERCURY(0xededed),
+	METHANE(0xc43b89),
+	NITRO_CARBON(0x85302d),
+	NITRO_DIESEL(0xdba842),
+	NITROCOAL_FUEL(0x163b34),
+	NITROFUEL(0xd68c4b),
+	NITROGEN(0x39d4c4),
+	NITROGEN_DIOXIDE(0x7ee0d7),
+	OIL(0x262626),
+	POTASSIUM(0x95b8ba),
+	SILICON(0x56435e),
+	SODIUM(0x374fa6),
+	SODIUM_SULFIDE(0xcf9a1f),
+	SODIUM_PERSULFATE(0x378080),
+	SULFUR(0xbd5a57),
+	SULFURIC_ACID(0xd1d1d1),
+	TRITIUM(0xdb3d3d),
+	WOLFRAMIUM(0x42374f),
+	BIOFUEL(0x1d6920);
 
 	public final String name;
 	private RebornFluid stillFluid;
@@ -81,21 +81,15 @@ public enum ModFluids implements BlockInfo {
 	private RebornBucketItem bucket;
 	private final Identifier identifier;
 
-	ModFluids() {
+	ModFluids(int color) {
 		name = this.toString().toLowerCase(Locale.ROOT);
 		this.identifier = Identifier.of(TechReborn.MOD_ID, name);
 
 		FluidSettings fluidSettings = FluidSettings.create();
 
-		Identifier texture_still = Identifier.of(TechReborn.MOD_ID, "block/fluids/" + name + "_still");
-		Identifier texture_flowing = Identifier.of(TechReborn.MOD_ID, "block/fluids/" + name + "_flowing");
-
-		fluidSettings.setStillTexture(texture_still);
-		fluidSettings.setFlowingTexture(texture_flowing);
-
-		stillFluid = new RebornFluid(true, fluidSettings, () -> block, () -> bucket, () -> flowingFluid, () -> stillFluid) {
+		stillFluid = new RebornFluid(true, fluidSettings, () -> block, () -> bucket, () -> flowingFluid, () -> stillFluid, color) {
 		};
-		flowingFluid = new RebornFluid(false, fluidSettings, () -> block, () -> bucket, () -> flowingFluid, () -> stillFluid) {
+		flowingFluid = new RebornFluid(false, fluidSettings, () -> block, () -> bucket, () -> flowingFluid, () -> stillFluid, color) {
 		};
 
 		block = new RebornFluidBlock(stillFluid, TRBlockSettings.fluid(identifier.getPath()));
