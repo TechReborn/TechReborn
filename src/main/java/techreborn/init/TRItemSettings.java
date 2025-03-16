3eeef41d346e7ba5745f618a7c5c2ab88033b3c7
ZurrTum <zurrtum@foxmail.com>
2025-03-16 10:10:25 +0800

    Hide UNBREAKABLE tooltip

--- src/main/java/techreborn/init/TRItemSettings.java
+++ src/main/java/techreborn/init/TRItemSettings.java
@@ -25,6 +25,7 @@
 package techreborn.init;
 
 import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.TooltipDisplayComponent;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.RegistryKey;
@@ -32,13 +33,21 @@ import net.minecraft.util.Identifier;
 import net.minecraft.util.Unit;
 import techreborn.TechReborn;
 
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class TRItemSettings {
+	public static TooltipDisplayComponent UNBREAKABLE_HIDE = new TooltipDisplayComponent(
+		false, new LinkedHashSet<>(Set.of(DataComponentTypes.UNBREAKABLE))
+	);
+
 	public static Item.Settings item(String name) {
 		return new Item.Settings().registryKey(key(name));
 	}
 
 	public static Item.Settings unbreakable(String name) {
-		return item(name).component(DataComponentTypes.UNBREAKABLE, Unit.INSTANCE);
+		return item(name).component(DataComponentTypes.UNBREAKABLE, Unit.INSTANCE)
+			.component(DataComponentTypes.TOOLTIP_DISPLAY, UNBREAKABLE_HIDE);
 	}
 
 	public static RegistryKey<Item> key(String name) {
