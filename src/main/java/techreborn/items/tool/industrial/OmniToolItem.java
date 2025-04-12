74b13f2956e49a5ea79e9e028009796026162390
ZurrTum <zurrtum@foxmail.com>
2025-04-12 14:05:46 +0800

    Fix OmniTool mining speed. Closes #3413

--- src/main/java/techreborn/items/tool/industrial/OmniToolItem.java
+++ src/main/java/techreborn/items/tool/industrial/OmniToolItem.java
@@ -25,6 +25,8 @@
 package techreborn.items.tool.industrial;
 
 import net.minecraft.block.BlockState;
+import net.minecraft.component.DataComponentTypes;
+import net.minecraft.component.type.ToolComponent;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.*;
@@ -63,7 +65,8 @@ public class OmniToolItem extends MiningToolItem implements RcEnergyItem, IToolH
 		if (getStoredEnergy(stack) >= TechRebornConfig.omniToolCost) {
 			return TRToolMaterials.OMNI_TOOL.speed();
 		}
-		return super.getMiningSpeed(stack, state);
+		ToolComponent toolComponent = stack.get(DataComponentTypes.TOOL);
+		return toolComponent != null ? toolComponent.defaultMiningSpeed() : 1.0F;
 	}
 
 	@Override
