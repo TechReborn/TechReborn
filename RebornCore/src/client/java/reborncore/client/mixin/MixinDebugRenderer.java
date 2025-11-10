b3acc47e61840fc5374e19d3b09e1e2dd132a163
ZurrTum <zurrtum@foxmail.com>
2025-11-11 06:43:48 +0800

    Update client chunk render

--- RebornCore/src/client/java/reborncore/client/mixin/MixinDebugRenderer.java
+++ RebornCore/src/client/java/reborncore/client/mixin/MixinDebugRenderer.java
@@ -24,21 +24,26 @@
 
 package reborncore.client.mixin;
 
-import com.mojang.blaze3d.vertex.PoseStack;
-import net.minecraft.client.renderer.MultiBufferSource;
-import net.minecraft.client.renderer.culling.Frustum;
 import net.minecraft.client.renderer.debug.DebugRenderer;
+import net.minecraft.client.renderer.debug.DebugRenderer.SimpleDebugRenderer;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import reborncore.client.ClientChunkManager;
 
+import java.util.List;
+
 @Mixin(DebugRenderer.class)
 public class MixinDebugRenderer {
+	@Shadow
+	@Final
+	private List<SimpleDebugRenderer> opaqueRenderers;
 
-	@Inject(method = "render", at = @At("RETURN"))
-	public void render(PoseStack matrices, Frustum frustum, MultiBufferSource.BufferSource vertexConsumers, double cameraX, double cameraY, double cameraZ, CallbackInfo ci) {
-		ClientChunkManager.render(matrices, vertexConsumers, cameraX, cameraY, cameraZ);
+	@Inject(method = "refreshRendererList", at = @At("TAIL"))
+	private void register(CallbackInfo ci) {
+		this.opaqueRenderers.add(ClientChunkManager::render);
 	}
 }
