cb25989830b50719b00a0dee1d0ff63aa7857422
Anomz <58313106+An0mz@users.noreply.github.com>
2026-05-03 16:34:48 -0400

    Fix import

--- RebornCore/src/main/java/reborncore/common/recipes/PaddedShapedRecipeJsonBuilder.java
+++ RebornCore/src/main/java/reborncore/common/recipes/PaddedShapedRecipeJsonBuilder.java
@@ -28,7 +28,7 @@ import java.util.Objects;
 import net.minecraft.advancements.AdvancementHolder;
 import net.minecraft.advancements.AdvancementRequirements;
 import net.minecraft.advancements.AdvancementRewards;
-import net.minecraft.advancements.critereon.RecipeUnlockedTrigger;
+import net.minecraft.advancements.criterion.RecipeUnlockedTrigger;
 import net.minecraft.core.HolderGetter;
 import net.minecraft.data.recipes.RecipeBuilder;
 import net.minecraft.data.recipes.RecipeCategory;
--- RebornCore/src/main/java/reborncore/common/util/TradeUtils.java
+++ RebornCore/src/main/java/reborncore/common/util/TradeUtils.java
@@ -27,8 +27,8 @@ package reborncore.common.util;
 import it.unimi.dsi.fastutil.ints.Int2ObjectArrayMap;
 import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.entity.npc.VillagerProfession;
-import net.minecraft.world.entity.npc.VillagerTrades;
+import net.minecraft.world.entity.npc.villager.VillagerProfession;
+import net.minecraft.world.entity.npc.villager.VillagerTrades;
 import net.minecraft.world.level.ItemLike;
 
 public final class TradeUtils {
