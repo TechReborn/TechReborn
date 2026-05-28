89a8fda82032868ceb688f1795c4e86b5e23cf80
ZurrTum <web@zurrtum.com>
2026-05-28 11:47:26 +0800

    Remove ColoredItem colorItem method

--- RebornCore/src/main/java/reborncore/common/util/ColoredItem.java
+++ RebornCore/src/main/java/reborncore/common/util/ColoredItem.java
@@ -35,46 +35,94 @@ import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
 import net.minecraft.world.level.block.ColorCollection;
+import org.jspecify.annotations.Nullable;
 
 public enum ColoredItem {
 
-	BLACK(colorItem(Items.DYE, DyeColor.BLACK), colorItem(Items.WOOL, DyeColor.BLACK), colorItem(Items.CARPET, DyeColor.BLACK), colorItem(Items.BED, DyeColor.BLACK), colorItem(Items.STAINED_GLASS, DyeColor.BLACK), colorItem(Items.STAINED_GLASS_PANE, DyeColor.BLACK), colorItem(Items.DYED_TERRACOTTA, DyeColor.BLACK), colorItem(Items.GLAZED_TERRACOTTA, DyeColor.BLACK), colorItem(Items.CONCRETE_POWDER, DyeColor.BLACK), colorItem(Items.CONCRETE, DyeColor.BLACK), colorItem(Items.DYED_SHULKER_BOX, DyeColor.BLACK), colorItem(Items.BANNER, DyeColor.BLACK), colorItem(Items.DYED_CANDLE, DyeColor.BLACK)),
-	BLUE(colorItem(Items.DYE, DyeColor.BLUE), colorItem(Items.WOOL, DyeColor.BLUE), colorItem(Items.CARPET, DyeColor.BLUE), colorItem(Items.BED, DyeColor.BLUE), colorItem(Items.STAINED_GLASS, DyeColor.BLUE), colorItem(Items.STAINED_GLASS_PANE, DyeColor.BLUE), colorItem(Items.DYED_TERRACOTTA, DyeColor.BLUE), colorItem(Items.GLAZED_TERRACOTTA, DyeColor.BLUE), colorItem(Items.CONCRETE_POWDER, DyeColor.BLUE), colorItem(Items.CONCRETE, DyeColor.BLUE), colorItem(Items.DYED_SHULKER_BOX, DyeColor.BLUE), colorItem(Items.BANNER, DyeColor.BLUE), colorItem(Items.DYED_CANDLE, DyeColor.BLUE)),
-	BROWN(colorItem(Items.DYE, DyeColor.BROWN), colorItem(Items.WOOL, DyeColor.BROWN), colorItem(Items.CARPET, DyeColor.BROWN), colorItem(Items.BED, DyeColor.BROWN), colorItem(Items.STAINED_GLASS, DyeColor.BROWN), colorItem(Items.STAINED_GLASS_PANE, DyeColor.BROWN), colorItem(Items.DYED_TERRACOTTA, DyeColor.BROWN), colorItem(Items.GLAZED_TERRACOTTA, DyeColor.BROWN), colorItem(Items.CONCRETE_POWDER, DyeColor.BROWN), colorItem(Items.CONCRETE, DyeColor.BROWN), colorItem(Items.DYED_SHULKER_BOX, DyeColor.BROWN), colorItem(Items.BANNER, DyeColor.BROWN), colorItem(Items.DYED_CANDLE, DyeColor.BROWN)),
-	CYAN(colorItem(Items.DYE, DyeColor.CYAN), colorItem(Items.WOOL, DyeColor.CYAN), colorItem(Items.CARPET, DyeColor.CYAN), colorItem(Items.BED, DyeColor.CYAN), colorItem(Items.STAINED_GLASS, DyeColor.CYAN), colorItem(Items.STAINED_GLASS_PANE, DyeColor.CYAN), colorItem(Items.DYED_TERRACOTTA, DyeColor.CYAN), colorItem(Items.GLAZED_TERRACOTTA, DyeColor.CYAN), colorItem(Items.CONCRETE_POWDER, DyeColor.CYAN), colorItem(Items.CONCRETE, DyeColor.CYAN), colorItem(Items.DYED_SHULKER_BOX, DyeColor.CYAN), colorItem(Items.BANNER, DyeColor.CYAN), colorItem(Items.DYED_CANDLE, DyeColor.CYAN)),
-	GRAY(colorItem(Items.DYE, DyeColor.GRAY), colorItem(Items.WOOL, DyeColor.GRAY), colorItem(Items.CARPET, DyeColor.GRAY), colorItem(Items.BED, DyeColor.GRAY), colorItem(Items.STAINED_GLASS, DyeColor.GRAY), colorItem(Items.STAINED_GLASS_PANE, DyeColor.GRAY), colorItem(Items.DYED_TERRACOTTA, DyeColor.GRAY), colorItem(Items.GLAZED_TERRACOTTA, DyeColor.GRAY), colorItem(Items.CONCRETE_POWDER, DyeColor.GRAY), colorItem(Items.CONCRETE, DyeColor.GRAY), colorItem(Items.DYED_SHULKER_BOX, DyeColor.GRAY), colorItem(Items.BANNER, DyeColor.GRAY), colorItem(Items.DYED_CANDLE, DyeColor.GRAY)),
-	GREEN(colorItem(Items.DYE, DyeColor.GREEN), colorItem(Items.WOOL, DyeColor.GREEN), colorItem(Items.CARPET, DyeColor.GREEN), colorItem(Items.BED, DyeColor.GREEN), colorItem(Items.STAINED_GLASS, DyeColor.GREEN), colorItem(Items.STAINED_GLASS_PANE, DyeColor.GREEN), colorItem(Items.DYED_TERRACOTTA, DyeColor.GREEN), colorItem(Items.GLAZED_TERRACOTTA, DyeColor.GREEN), colorItem(Items.CONCRETE_POWDER, DyeColor.GREEN), colorItem(Items.CONCRETE, DyeColor.GREEN), colorItem(Items.DYED_SHULKER_BOX, DyeColor.GREEN), colorItem(Items.BANNER, DyeColor.GREEN), colorItem(Items.DYED_CANDLE, DyeColor.GREEN)),
-	LIGHT_BLUE(colorItem(Items.DYE, DyeColor.LIGHT_BLUE), colorItem(Items.WOOL, DyeColor.LIGHT_BLUE), colorItem(Items.CARPET, DyeColor.LIGHT_BLUE), colorItem(Items.BED, DyeColor.LIGHT_BLUE), colorItem(Items.STAINED_GLASS, DyeColor.LIGHT_BLUE), colorItem(Items.STAINED_GLASS_PANE, DyeColor.LIGHT_BLUE), colorItem(Items.DYED_TERRACOTTA, DyeColor.LIGHT_BLUE), colorItem(Items.GLAZED_TERRACOTTA, DyeColor.LIGHT_BLUE), colorItem(Items.CONCRETE_POWDER, DyeColor.LIGHT_BLUE), colorItem(Items.CONCRETE, DyeColor.LIGHT_BLUE), colorItem(Items.DYED_SHULKER_BOX, DyeColor.LIGHT_BLUE), colorItem(Items.BANNER, DyeColor.LIGHT_BLUE), colorItem(Items.DYED_CANDLE, DyeColor.LIGHT_BLUE)),
-	LIGHT_GRAY(colorItem(Items.DYE, DyeColor.LIGHT_GRAY), colorItem(Items.WOOL, DyeColor.LIGHT_GRAY), colorItem(Items.CARPET, DyeColor.LIGHT_GRAY), colorItem(Items.BED, DyeColor.LIGHT_GRAY), colorItem(Items.STAINED_GLASS, DyeColor.LIGHT_GRAY), colorItem(Items.STAINED_GLASS_PANE, DyeColor.LIGHT_GRAY), colorItem(Items.DYED_TERRACOTTA, DyeColor.LIGHT_GRAY), colorItem(Items.GLAZED_TERRACOTTA, DyeColor.LIGHT_GRAY), colorItem(Items.CONCRETE_POWDER, DyeColor.LIGHT_GRAY), colorItem(Items.CONCRETE, DyeColor.LIGHT_GRAY), colorItem(Items.DYED_SHULKER_BOX, DyeColor.LIGHT_GRAY), colorItem(Items.BANNER, DyeColor.LIGHT_GRAY), colorItem(Items.DYED_CANDLE, DyeColor.LIGHT_GRAY)),
-	LIME(colorItem(Items.DYE, DyeColor.LIME), colorItem(Items.WOOL, DyeColor.LIME), colorItem(Items.CARPET, DyeColor.LIME), colorItem(Items.BED, DyeColor.LIME), colorItem(Items.STAINED_GLASS, DyeColor.LIME), colorItem(Items.STAINED_GLASS_PANE, DyeColor.LIME), colorItem(Items.DYED_TERRACOTTA, DyeColor.LIME), colorItem(Items.GLAZED_TERRACOTTA, DyeColor.LIME), colorItem(Items.CONCRETE_POWDER, DyeColor.LIME), colorItem(Items.CONCRETE, DyeColor.LIME), colorItem(Items.DYED_SHULKER_BOX, DyeColor.LIME), colorItem(Items.BANNER, DyeColor.LIME), colorItem(Items.DYED_CANDLE, DyeColor.LIME)),
-	MAGENTA(colorItem(Items.DYE, DyeColor.MAGENTA), colorItem(Items.WOOL, DyeColor.MAGENTA), colorItem(Items.CARPET, DyeColor.MAGENTA), colorItem(Items.BED, DyeColor.MAGENTA), colorItem(Items.STAINED_GLASS, DyeColor.MAGENTA), colorItem(Items.STAINED_GLASS_PANE, DyeColor.MAGENTA), colorItem(Items.DYED_TERRACOTTA, DyeColor.MAGENTA), colorItem(Items.GLAZED_TERRACOTTA, DyeColor.MAGENTA), colorItem(Items.CONCRETE_POWDER, DyeColor.MAGENTA), colorItem(Items.CONCRETE, DyeColor.MAGENTA), colorItem(Items.DYED_SHULKER_BOX, DyeColor.MAGENTA), colorItem(Items.BANNER, DyeColor.MAGENTA), colorItem(Items.DYED_CANDLE, DyeColor.MAGENTA)),
+	BLACK(DyeColor.BLACK, Items.DYE, Items.WOOL, Items.CARPET, Items.BED, Items.STAINED_GLASS, Items.STAINED_GLASS_PANE, Items.DYED_TERRACOTTA, Items.GLAZED_TERRACOTTA, Items.CONCRETE_POWDER, Items.CONCRETE, Items.DYED_SHULKER_BOX, Items.BANNER, Items.DYED_CANDLE),
+	BLUE(DyeColor.BLUE, Items.DYE, Items.WOOL, Items.CARPET, Items.BED, Items.STAINED_GLASS, Items.STAINED_GLASS_PANE, Items.DYED_TERRACOTTA, Items.GLAZED_TERRACOTTA, Items.CONCRETE_POWDER, Items.CONCRETE, Items.DYED_SHULKER_BOX, Items.BANNER, Items.DYED_CANDLE),
+	BROWN(DyeColor.BROWN, Items.DYE, Items.WOOL, Items.CARPET, Items.BED, Items.STAINED_GLASS, Items.STAINED_GLASS_PANE, Items.DYED_TERRACOTTA, Items.GLAZED_TERRACOTTA, Items.CONCRETE_POWDER, Items.CONCRETE, Items.DYED_SHULKER_BOX, Items.BANNER, Items.DYED_CANDLE),
+	CYAN(DyeColor.CYAN, Items.DYE, Items.WOOL, Items.CARPET, Items.BED, Items.STAINED_GLASS, Items.STAINED_GLASS_PANE, Items.DYED_TERRACOTTA, Items.GLAZED_TERRACOTTA, Items.CONCRETE_POWDER, Items.CONCRETE, Items.DYED_SHULKER_BOX, Items.BANNER, Items.DYED_CANDLE),
+	GRAY(DyeColor.GRAY, Items.DYE, Items.WOOL, Items.CARPET, Items.BED, Items.STAINED_GLASS, Items.STAINED_GLASS_PANE, Items.DYED_TERRACOTTA, Items.GLAZED_TERRACOTTA, Items.CONCRETE_POWDER, Items.CONCRETE, Items.DYED_SHULKER_BOX, Items.BANNER, Items.DYED_CANDLE),
+	GREEN(DyeColor.GREEN, Items.DYE, Items.WOOL, Items.CARPET, Items.BED, Items.STAINED_GLASS, Items.STAINED_GLASS_PANE, Items.DYED_TERRACOTTA, Items.GLAZED_TERRACOTTA, Items.CONCRETE_POWDER, Items.CONCRETE, Items.DYED_SHULKER_BOX, Items.BANNER, Items.DYED_CANDLE),
+	LIGHT_BLUE(DyeColor.LIGHT_BLUE, Items.DYE, Items.WOOL, Items.CARPET, Items.BED, Items.STAINED_GLASS, Items.STAINED_GLASS_PANE, Items.DYED_TERRACOTTA, Items.GLAZED_TERRACOTTA, Items.CONCRETE_POWDER, Items.CONCRETE, Items.DYED_SHULKER_BOX, Items.BANNER, Items.DYED_CANDLE),
+	LIGHT_GRAY(DyeColor.LIGHT_GRAY, Items.DYE, Items.WOOL, Items.CARPET, Items.BED, Items.STAINED_GLASS, Items.STAINED_GLASS_PANE, Items.DYED_TERRACOTTA, Items.GLAZED_TERRACOTTA, Items.CONCRETE_POWDER, Items.CONCRETE, Items.DYED_SHULKER_BOX, Items.BANNER, Items.DYED_CANDLE),
+	LIME(DyeColor.LIME, Items.DYE, Items.WOOL, Items.CARPET, Items.BED, Items.STAINED_GLASS, Items.STAINED_GLASS_PANE, Items.DYED_TERRACOTTA, Items.GLAZED_TERRACOTTA, Items.CONCRETE_POWDER, Items.CONCRETE, Items.DYED_SHULKER_BOX, Items.BANNER, Items.DYED_CANDLE),
+	MAGENTA(DyeColor.MAGENTA, Items.DYE, Items.WOOL, Items.CARPET, Items.BED, Items.STAINED_GLASS, Items.STAINED_GLASS_PANE, Items.DYED_TERRACOTTA, Items.GLAZED_TERRACOTTA, Items.CONCRETE_POWDER, Items.CONCRETE, Items.DYED_SHULKER_BOX, Items.BANNER, Items.DYED_CANDLE),
 	NEUTRAL(null, null, null, null, Items.GLASS, Items.GLASS_PANE, Items.TERRACOTTA, null, null, null, Items.SHULKER_BOX, null, Items.CANDLE),
-	ORANGE(colorItem(Items.DYE, DyeColor.ORANGE), colorItem(Items.WOOL, DyeColor.ORANGE), colorItem(Items.CARPET, DyeColor.ORANGE), colorItem(Items.BED, DyeColor.ORANGE), colorItem(Items.STAINED_GLASS, DyeColor.ORANGE), colorItem(Items.STAINED_GLASS_PANE, DyeColor.ORANGE), colorItem(Items.DYED_TERRACOTTA, DyeColor.ORANGE), colorItem(Items.GLAZED_TERRACOTTA, DyeColor.ORANGE), colorItem(Items.CONCRETE_POWDER, DyeColor.ORANGE), colorItem(Items.CONCRETE, DyeColor.ORANGE), colorItem(Items.DYED_SHULKER_BOX, DyeColor.ORANGE), colorItem(Items.BANNER, DyeColor.ORANGE), colorItem(Items.DYED_CANDLE, DyeColor.ORANGE)),
-	PINK(colorItem(Items.DYE, DyeColor.PINK), colorItem(Items.WOOL, DyeColor.PINK), colorItem(Items.CARPET, DyeColor.PINK), colorItem(Items.BED, DyeColor.PINK), colorItem(Items.STAINED_GLASS, DyeColor.PINK), colorItem(Items.STAINED_GLASS_PANE, DyeColor.PINK), colorItem(Items.DYED_TERRACOTTA, DyeColor.PINK), colorItem(Items.GLAZED_TERRACOTTA, DyeColor.PINK), colorItem(Items.CONCRETE_POWDER, DyeColor.PINK), colorItem(Items.CONCRETE, DyeColor.PINK), colorItem(Items.DYED_SHULKER_BOX, DyeColor.PINK), colorItem(Items.BANNER, DyeColor.PINK), colorItem(Items.DYED_CANDLE, DyeColor.PINK)),
-	PURPLE(colorItem(Items.DYE, DyeColor.PURPLE), colorItem(Items.WOOL, DyeColor.PURPLE), colorItem(Items.CARPET, DyeColor.PURPLE), colorItem(Items.BED, DyeColor.PURPLE), colorItem(Items.STAINED_GLASS, DyeColor.PURPLE), colorItem(Items.STAINED_GLASS_PANE, DyeColor.PURPLE), colorItem(Items.DYED_TERRACOTTA, DyeColor.PURPLE), colorItem(Items.GLAZED_TERRACOTTA, DyeColor.PURPLE), colorItem(Items.CONCRETE_POWDER, DyeColor.PURPLE), colorItem(Items.CONCRETE, DyeColor.PURPLE), colorItem(Items.DYED_SHULKER_BOX, DyeColor.PURPLE), colorItem(Items.BANNER, DyeColor.PURPLE), colorItem(Items.DYED_CANDLE, DyeColor.PURPLE)),
-	RED(colorItem(Items.DYE, DyeColor.RED), colorItem(Items.WOOL, DyeColor.RED), colorItem(Items.CARPET, DyeColor.RED), colorItem(Items.BED, DyeColor.RED), colorItem(Items.STAINED_GLASS, DyeColor.RED), colorItem(Items.STAINED_GLASS_PANE, DyeColor.RED), colorItem(Items.DYED_TERRACOTTA, DyeColor.RED), colorItem(Items.GLAZED_TERRACOTTA, DyeColor.RED), colorItem(Items.CONCRETE_POWDER, DyeColor.RED), colorItem(Items.CONCRETE, DyeColor.RED), colorItem(Items.DYED_SHULKER_BOX, DyeColor.RED), colorItem(Items.BANNER, DyeColor.RED), colorItem(Items.DYED_CANDLE, DyeColor.RED)),
-	WHITE(colorItem(Items.DYE, DyeColor.WHITE), colorItem(Items.WOOL, DyeColor.WHITE), colorItem(Items.CARPET, DyeColor.WHITE), colorItem(Items.BED, DyeColor.WHITE), colorItem(Items.STAINED_GLASS, DyeColor.WHITE), colorItem(Items.STAINED_GLASS_PANE, DyeColor.WHITE), colorItem(Items.DYED_TERRACOTTA, DyeColor.WHITE), colorItem(Items.GLAZED_TERRACOTTA, DyeColor.WHITE), colorItem(Items.CONCRETE_POWDER, DyeColor.WHITE), colorItem(Items.CONCRETE, DyeColor.WHITE), colorItem(Items.DYED_SHULKER_BOX, DyeColor.WHITE), colorItem(Items.BANNER, DyeColor.WHITE), colorItem(Items.DYED_CANDLE, DyeColor.WHITE)),
-	YELLOW(colorItem(Items.DYE, DyeColor.YELLOW), colorItem(Items.WOOL, DyeColor.YELLOW), colorItem(Items.CARPET, DyeColor.YELLOW), colorItem(Items.BED, DyeColor.YELLOW), colorItem(Items.STAINED_GLASS, DyeColor.YELLOW), colorItem(Items.STAINED_GLASS_PANE, DyeColor.YELLOW), colorItem(Items.DYED_TERRACOTTA, DyeColor.YELLOW), colorItem(Items.GLAZED_TERRACOTTA, DyeColor.YELLOW), colorItem(Items.CONCRETE_POWDER, DyeColor.YELLOW), colorItem(Items.CONCRETE, DyeColor.YELLOW), colorItem(Items.DYED_SHULKER_BOX, DyeColor.YELLOW), colorItem(Items.BANNER, DyeColor.YELLOW), colorItem(Items.DYED_CANDLE, DyeColor.YELLOW));
+	ORANGE(DyeColor.ORANGE, Items.DYE, Items.WOOL, Items.CARPET, Items.BED, Items.STAINED_GLASS, Items.STAINED_GLASS_PANE, Items.DYED_TERRACOTTA, Items.GLAZED_TERRACOTTA, Items.CONCRETE_POWDER, Items.CONCRETE, Items.DYED_SHULKER_BOX, Items.BANNER, Items.DYED_CANDLE),
+	PINK(DyeColor.PINK, Items.DYE, Items.WOOL, Items.CARPET, Items.BED, Items.STAINED_GLASS, Items.STAINED_GLASS_PANE, Items.DYED_TERRACOTTA, Items.GLAZED_TERRACOTTA, Items.CONCRETE_POWDER, Items.CONCRETE, Items.DYED_SHULKER_BOX, Items.BANNER, Items.DYED_CANDLE),
+	PURPLE(DyeColor.PURPLE, Items.DYE, Items.WOOL, Items.CARPET, Items.BED, Items.STAINED_GLASS, Items.STAINED_GLASS_PANE, Items.DYED_TERRACOTTA, Items.GLAZED_TERRACOTTA, Items.CONCRETE_POWDER, Items.CONCRETE, Items.DYED_SHULKER_BOX, Items.BANNER, Items.DYED_CANDLE),
+	RED(DyeColor.RED, Items.DYE, Items.WOOL, Items.CARPET, Items.BED, Items.STAINED_GLASS, Items.STAINED_GLASS_PANE, Items.DYED_TERRACOTTA, Items.GLAZED_TERRACOTTA, Items.CONCRETE_POWDER, Items.CONCRETE, Items.DYED_SHULKER_BOX, Items.BANNER, Items.DYED_CANDLE),
+	WHITE(DyeColor.WHITE, Items.DYE, Items.WOOL, Items.CARPET, Items.BED, Items.STAINED_GLASS, Items.STAINED_GLASS_PANE, Items.DYED_TERRACOTTA, Items.GLAZED_TERRACOTTA, Items.CONCRETE_POWDER, Items.CONCRETE, Items.DYED_SHULKER_BOX, Items.BANNER, Items.DYED_CANDLE),
+	YELLOW(DyeColor.YELLOW, Items.DYE, Items.WOOL, Items.CARPET, Items.BED, Items.STAINED_GLASS, Items.STAINED_GLASS_PANE, Items.DYED_TERRACOTTA, Items.GLAZED_TERRACOTTA, Items.CONCRETE_POWDER, Items.CONCRETE, Items.DYED_SHULKER_BOX, Items.BANNER, Items.DYED_CANDLE);
 
-	private Item dye;
-	private Item wool;
-	private Item carpet;
-	private Item bed;
-	private Item glass;
-	private Item glassPane;
-	private Item terracotta;
-	private Item glazedTerracotta;
-	private Item concretePowder;
-	private Item concrete;
-	private Item shulkerBox;
-	private Item banner;
-	private Item candle;
+	private final @Nullable Item dye;
+	private final @Nullable Item wool;
+	private final @Nullable Item carpet;
+	private final @Nullable Item bed;
+	private final @Nullable Item glass;
+	private final @Nullable Item glassPane;
+	private final @Nullable Item terracotta;
+	private final @Nullable Item glazedTerracotta;
+	private final @Nullable Item concretePowder;
+	private final @Nullable Item concrete;
+	private final @Nullable Item shulkerBox;
+	private final @Nullable Item banner;
+	private final @Nullable Item candle;
 
-	private static Map<Pair<ColoredItem, ColoredItem>, ColoredItem> vanillaMixingMap = null;
-	private static Map<Pair<ColoredItem, ColoredItem>, ColoredItem> extendedMixingMap = null;
-	private static Map<Pair<ColoredItem, ColoredItem>, ColoredItem> extendedMixingMapNoNeutral = null;
+	private static @Nullable Map<Pair<ColoredItem, ColoredItem>, ColoredItem> vanillaMixingMap = null;
+	private static @Nullable Map<Pair<ColoredItem, ColoredItem>, ColoredItem> extendedMixingMap = null;
+	private static @Nullable Map<Pair<ColoredItem, ColoredItem>, ColoredItem> extendedMixingMapNoNeutral = null;
 
-	ColoredItem(Item dye, Item wool, Item carpet, Item bed, Item glass, Item glassPane, Item terracotta, Item glazedTerracotta, Item concretePowder, Item concrete, Item shulkerBox, Item banner, Item candle) {
+	ColoredItem(
+		DyeColor color,
+		ColorCollection<Item> dye,
+		ColorCollection<Item> wool,
+		ColorCollection<Item> carpet,
+		ColorCollection<Item> bed,
+		ColorCollection<Item> glass,
+		ColorCollection<Item> glassPane,
+		ColorCollection<Item> terracotta,
+		ColorCollection<Item> glazedTerracotta,
+		ColorCollection<Item> concretePowder,
+		ColorCollection<Item> concrete,
+		ColorCollection<Item> shulkerBox,
+		ColorCollection<Item> banner,
+		ColorCollection<Item> candle
+	) {
+		this(
+			dye.pick(color),
+			wool.pick(color),
+			carpet.pick(color),
+			bed.pick(color),
+			glass.pick(color),
+			glassPane.pick(color),
+			terracotta.pick(color),
+			glazedTerracotta.pick(color),
+			concretePowder.pick(color),
+			concrete.pick(color),
+			shulkerBox.pick(color),
+			banner.pick(color),
+			candle.pick(color)
+		);
+	}
+
+	ColoredItem(
+		@Nullable Item dye,
+		@Nullable Item wool,
+		@Nullable Item carpet,
+		@Nullable Item bed,
+		@Nullable Item glass,
+		@Nullable Item glassPane,
+		@Nullable Item terracotta,
+		@Nullable Item glazedTerracotta,
+		@Nullable Item concretePowder,
+		@Nullable Item concrete,
+		@Nullable Item shulkerBox,
+		@Nullable Item banner,
+		@Nullable Item candle
+	) {
 		this.dye = dye;
 		this.wool = wool;
 		this.carpet = carpet;
@@ -88,10 +136,6 @@ public enum ColoredItem {
 		this.shulkerBox = shulkerBox;
 		this.banner = banner;
 		this.candle = candle;
-	}
-
-	private static Item colorItem(ColorCollection<Item> items, DyeColor color) {
-		return items.pick(color);
 	}
 
 	private static void putSym(ImmutableMap.Builder<Pair<ColoredItem, ColoredItem>, ColoredItem> builder, ColoredItem item1, ColoredItem item2, ColoredItem result) {
